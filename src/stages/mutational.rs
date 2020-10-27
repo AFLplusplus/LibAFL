a63ecf3c1b665d3a9cf7c81259b7a089aa5a6717
Dominik Maier <domenukk@gmail.com>
2020-10-27 15:12:33 +0100

    mutators not done yet

--- src/stages/mutational.rs
+++ src/stages/mutational.rs
@@ -1,15 +1,16 @@
 use std::Vec;
 use crate::mutators::Mutator;
 use crate::inputs::Input;
-use c
 
-struct MutationalStage {
+pub struct MutationalStage {
     mutators: Vec<Box<dyn Mutator>>;
 }
 
 impl Stage for MutationalStage {
 
     fn Perform(&mut self, input: &Input, entry: &mut Entry) -> Result<(), AflError> {
+        // TODO: Implement me
+        Err(AflError::Unknown);
     }
 
 }
