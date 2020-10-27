ce9413bfa47a17d8aba96b89307abc1d5449b35b
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-10-27 20:45:49 +0100

    in memory executor first impl

--- src/executors/mod.rs
+++ src/executors/mod.rs
@@ -13,7 +13,7 @@ pub trait Executor {
 
     fn run_target(&mut self) -> Result<ExitKind, AflError>;
 
-    fn place_input(&mut self, entry: Box<dyn Input>) -> Result<(), AflError>;
+    fn place_input(&mut self, input: Box<dyn Input>) -> Result<(), AflError>;
 
 }
 
@@ -22,5 +22,31 @@ pub struct ExecutorBase {
 
     observers: Vec<Box<dyn Observer>>,
     cur_input: Box<dyn Input>
+
+}
+
+type HarnessFunction = fn(&dyn Executor, &[u8]) -> ExitKind;
+
+pub struct InMemoryExecutor {
+
+    base: ExecutorBase,
+    harness: HarnessFunction,
+
+}
+
+impl Executor for InMemoryExecutor {
+
+    fn run_target(&mut self) -> Result<ExitKind, AflError> {
+        let bytes = self.base.cur_input.serialize();
+        return match bytes {
+            Ok(b) => Ok((self.harness)(self, b)),
+            Err(e) => Err(e)
+        }
+    }
+
+    fn place_input(&mut self, input: Box<dyn Input>) -> Result<(), AflError> {
+        self.base.cur_input = input;
+        Ok(())
+    }
 
 }
