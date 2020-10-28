ac4e71390ae4b98eafd310b7f51fb31e28b24a35
Dominik Maier <domenukk@gmail.com>
2020-10-29 00:07:50 +0100

    started observers

--- src/feedbacks/mod.rs
+++ src/feedbacks/mod.rs
@@ -1,1 +1,8 @@
-pub trait Feedback {}
+use crate::corpus::Testcase;
+use crate::executors::Executor;
+
+pub trait Feedback {
+
+    fn is_interesting(&mut self, executor: &dyn Executor, entry: &dyn Testcase) -> f64;
+
+}
--- src/observers/mod.rs
+++ src/observers/mod.rs
@@ -1,4 +1,5 @@
 use crate::AflError;
+use std::cell::RefCell;
 
 pub trait Observer {
     fn flush(&mut self) -> Result<(), AflError> {
@@ -9,5 +10,43 @@ pub trait Observer {
 
     fn post_exec(&mut self) -> Result<(), AflError> {
         Ok(())
+    }
+}
+
+pub trait MapObserver<MapT>: Observer {
+
+    // TODO: Rust
+    fn get_map(&self) -> &RefCell<Vec<MapT>>;
+    //fn get_map_mut(&mut self) -> &mut Vec<MapT>;
+
+}
+
+pub struct U8MapObserver {
+    
+    map: RefCell<Vec<u8>>,
+
+}
+
+impl Observer for U8MapObserver {
+    fn reset(&mut self) -> Result<(), AflError> {
+
+        // TODO: Clear
+        Err(AflError::Unknown)
+
+    }
+}
+
+impl MapObserver<u8> for U8MapObserver {
+
+    fn get_map(&self) -> &RefCell<Vec<u8>> {
+        // TODO: Rust this better
+        return &self.map;
+    }
+
+}
+
+impl U8MapObserver {
+    pub fn new(map: RefCell<Vec<u8>>) -> Self {
+        U8MapObserver{map: map}
     }
 }
