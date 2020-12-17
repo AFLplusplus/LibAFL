8272e640be0f2c42b647202ba17fea7a2e022a80
Andrea Fioraldi <andreafioraldi@gmail.com>
2020-12-17 14:27:11 +0100

    qemufuzzer skeleton

---
+++ fuzzers/qemufuzzer/test/test.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int target_func(const uint8_t *buf, size_t size) {
+
+  /*printf("BUF (%ld): ", size);
+  for (int i = 0; i < size; i++) {
+      printf("%02X", buf[i]);
+  }
+  printf("\n");*/
+  
+  if (size == 0) return 0;
+  
+  switch (buf[0]) {
+
+    case 1:
+      if (buf[1] == 0x44) {
+        //__builtin_trap();
+        return 8;
+      }
+
+      break;
+    case 0xff:
+      if (buf[2] == 0xff) {
+        if (buf[1] == 0x44) {
+          //*(char *)(0xdeadbeef) = 1;
+          return 9;
+        }
+      }
+
+      break;
+    default:
+      break;
+
+  }
+
+  return 1;
+
+}
+
+int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
+  return target_func(Data, Size);
+}
