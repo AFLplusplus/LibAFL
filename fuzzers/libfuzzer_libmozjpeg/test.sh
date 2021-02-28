955a62550c3dad6b2cc0f8c680d1f9e0826795ad
Marcin Kozlowski <marcinguy@gmail.com>
2021-02-28 21:04:31 +0100

    Taskset removal (wrong invocation, without -c)

--- fuzzers/libfuzzer_libmozjpeg/test.sh
+++ fuzzers/libfuzzer_libmozjpeg/test.sh
@@ -6,7 +6,7 @@ cargo build --example libfuzzer_libmozjpeg --release || exit 1
 cp ../../target/release/examples/libfuzzer_libmozjpeg ./.libfuzzer_test.elf
 
 # The broker
-RUST_BACKTRACE=full taskset 0 ./.libfuzzer_test.elf &
+RUST_BACKTRACE=full ./.libfuzzer_test.elf &
 # Give the broker time to spawn
 sleep 2
 echo "Spawning client"
