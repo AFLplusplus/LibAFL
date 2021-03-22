837bbf12e18575283ba9d8ee6bf39be9059bb50b
Dominik Maier <domenukk@gmail.com>
2021-03-22 19:56:20 +0100

    removed warnings from build.rs

--- fuzzers/libfuzzer_windows/build.rs
+++ fuzzers/libfuzzer_windows/build.rs
@@ -3,14 +3,11 @@
 use std::{
     env,
     path::Path,
-    process::{exit, Command},
 };
 
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
-    let cwd = env::current_dir().unwrap().to_string_lossy().to_string();
     let out_dir = out_dir.to_string_lossy().to_string();
-    let out_dir_path = Path::new(&out_dir);
 
     println!("cargo:rerun-if-changed=../libfuzzer_runtime/rt.c",);
     println!("cargo:rerun-if-changed=harness.cc");
@@ -18,10 +15,10 @@ fn main() {
     // Enforce clang for its -fsanitize-coverage support.
     std::env::set_var("CC", "clang");
     std::env::set_var("CXX", "clang++");
-    let ldflags = match env::var("LDFLAGS") {
+    /*let ldflags = match env::var("LDFLAGS") {
         Ok(val) => val,
         Err(_) => "".to_string(),
-    };
+    };*/
 
     cc::Build::new()
         .file("../libfuzzer_runtime/rt.c")
