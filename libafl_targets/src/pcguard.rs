b4e6115d4f05b8b6af61e9bf55d07552f7d2c405
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-03-29 15:57:27 +0200

    fixes for pcguard and value profile

--- libafl_targets/src/pcguard.rs
+++ libafl_targets/src/pcguard.rs
@@ -4,10 +4,10 @@ compile_error!(
 );
 
 // TODO compile time flag
-pub const MAP_SIZE: usize = 65536;
+pub const EDGES_MAP_SIZE: usize = 65536;
 
-pub static mut EDGES_MAP: [u8; MAP_SIZE] = [0; MAP_SIZE];
-//pub static mut CMP_MAP: [u8; MAP_SIZE] = [0; MAP_SIZE];
+pub static mut EDGES_MAP: [u8; EDGES_MAP_SIZE] = [0; EDGES_MAP_SIZE];
+//pub static mut CMP_MAP: [u8; EDGES_MAP_SIZE] = [0; EDGES_MAP_SIZE];
 pub static mut MAX_EDGES_NUM: usize = 0;
 
 #[no_mangle]
@@ -32,7 +32,7 @@ pub unsafe extern "C" fn __sanitizer_cov_trace_pc_guard_init(mut start: *mut u32
 
     while start < stop {
         MAX_EDGES_NUM += 1;
-        *start = (MAX_EDGES_NUM & (MAP_SIZE - 1)) as u32;
+        *start = (MAX_EDGES_NUM & (EDGES_MAP_SIZE - 1)) as u32;
         start = start.offset(1);
     }
 }
