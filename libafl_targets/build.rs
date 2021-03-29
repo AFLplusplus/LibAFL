bd9aa89753d5312c6dc983fb633274ddef3b09de
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-03-29 16:07:56 +0200

    libfuzzer_libmozjpeg readme

--- libafl_targets/build.rs
+++ libafl_targets/build.rs
@@ -6,8 +6,8 @@ use std::path::Path;
 fn main() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = out_dir.to_string_lossy().to_string();
-    let src_dir = Path::new("src");
     //let out_dir_path = Path::new(&out_dir);
+    let _src_dir = Path::new("src");
 
     //std::env::set_var("CC", "clang");
     //std::env::set_var("CXX", "clang++");
@@ -17,7 +17,7 @@ fn main() {
         println!("cargo:rerun-if-changed=src/libfuzzer_compatibility.c");
 
         cc::Build::new()
-            .file(src_dir.join("libfuzzer_compatibility.c"))
+            .file(_src_dir.join("libfuzzer_compatibility.c"))
             .compile("libfuzzer_compatibility");
     }
     
@@ -26,7 +26,7 @@ fn main() {
         println!("cargo:rerun-if-changed=src/value_profile.c");
 
         cc::Build::new()
-            .file(src_dir.join("value_profile.c"))
+            .file(_src_dir.join("value_profile.c"))
             .compile("value_profile");
     }
 
