eb2cec5a7cece323a6d86f7863df33085556f820
tokatoka <tokazerkje@outlook.com>
2021-04-27 00:11:14 +0900

    comment benchmarks code out

--- libafl/src/events/compress.rs
+++ libafl/src/events/compress.rs
@@ -19,7 +19,7 @@ impl GzipCompressor {
 impl GzipCompressor {
     pub fn compress(&self, buf: &[u8]) -> Option<Vec<u8>> {
         if buf.len() > self.threshold {
-            let t1 = crate::utils::current_time();
+            //let t1 = crate::utils::current_time();
             //compress if the buffer is large enough
             let compressed = buf
                 .into_iter()
@@ -27,46 +27,54 @@ impl GzipCompressor {
                 .encode(&mut GZipEncoder::new(), Action::Finish)
                 .collect::<Result<Vec<_>, _>>()
                 .unwrap();
-            let t2 = crate::utils::current_time();
+            //let t2 = crate::utils::current_time();
+            /*
             println!(
                 "fn compress with compression {} nano sec",
                 (t2 - t1).as_nanos()
             );
             println!("memory saved {} bytes", buf.len() - compressed.len());
+            */
             Some(compressed)
         } else {
+            /*
             let t1 = crate::utils::current_time();
             let t2 = crate::utils::current_time();
             println!(
                 "fn compress without compression {} nano sec",
                 (t2 - t1).as_nanos()
             );
+            */
             None
         }
     }
 
     pub fn decompress(&self, _tag: Tag, flag: Flag, buf: &[u8]) -> Option<Vec<u8>> {
         if flag & LLMP_FLAG_COMPRESSED == LLMP_FLAG_COMPRESSED {
-            let t1 = crate::utils::current_time();
+            //let t1 = crate::utils::current_time();
             let decompressed: Vec<u8> = buf
                 .into_iter()
                 .cloned()
                 .decode(&mut GZipDecoder::new())
                 .collect::<Result<Vec<_>, _>>()
                 .unwrap();
-            let t2 = crate::utils::current_time();
+            //let t2 = crate::utils::current_time();
+            /*
             println!(
                 "fn decompress with decompression {} nano sec",
                 (t2 - t1).as_nanos()
             );
+            */
             Some(decompressed)
         } else {
+            /*
             let t1 = crate::utils::current_time();
             let t2 = crate::utils::current_time();
             println!(
                 "fn decompress without decompression {} nano sec",
                 (t2 - t1).as_nanos()
             );
+            */
             None
         }
     }
