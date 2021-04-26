5b756005a160ec84cabf8b86f7b464efc14fc8d9
s1341 <github@shmarya.net>
2021-04-26 15:44:49 +0300

    use __builtin_thread_pointer instead of custom asm

--- libafl_frida/build.rs
+++ libafl_frida/build.rs
@@ -1,6 +1,5 @@
 // build.rs
 
 fn main() {
-    #[cfg(target_arch = "aarch64")]
     cc::Build::new().file("src/gettls.c").compile("libgettls.a");
 }
--- libafl_frida/src/gettls.c
+++ libafl_frida/src/gettls.c
@@ -1,5 +1,3 @@
 void * get_tls_ptr() {
-        void * address = 0;
-        __asm("mrs %[result], tpidr_el0": [result] "=r" (address));
-        return address;
+        return __builtin_thread_pointer();
 }
