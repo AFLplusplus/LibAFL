f5bdec95032fd131108aa71f252bb9442d0294cf
tokatoka <tokazerkje@outlook.com>
2021-04-29 09:48:25 +0900

    fix doc

--- libafl/src/bolts/compress.rs
+++ libafl/src/bolts/compress.rs
@@ -1,4 +1,4 @@
-//! Compression of events passed between a broker and clients
+//! Compression of events passed between a broker and clients.
 //! Currently we use the gzip compression algorithm for its fast decompression performance.
 
 #[cfg(feature = "llmp_compress")]
@@ -16,14 +16,15 @@ pub struct GzipCompressor {
 }
 
 impl GzipCompressor {
+    /// If the buffer is larger than the threshold value, we compress the buffer.
     pub fn new(threshold: usize) -> Self {
         GzipCompressor { threshold }
     }
 }
 
-/// Compression
-/// The buffer is compressed with gzip algo
 impl GzipCompressor {
+    /// Compression.
+    /// The buffer is compressed with the gzip algo
     pub fn compress(&self, buf: &[u8]) -> Result<Option<Vec<u8>>, Error> {
         if buf.len() > self.threshold {
             //compress if the buffer is large enough
@@ -38,8 +39,8 @@ impl GzipCompressor {
         }
     }
 
-    /// Decompression
-    /// The buffer is decompressed, flag is used to indicate if it's compressed or not
+    /// Decompression.
+    /// Flag is used to indicate if it's compressed or not
     pub fn decompress(&self, flags: Flag, buf: &[u8]) -> Result<Option<Vec<u8>>, Error> {
         if flags & LLMP_FLAG_COMPRESSED == LLMP_FLAG_COMPRESSED {
             let decompressed: Vec<u8> = buf
