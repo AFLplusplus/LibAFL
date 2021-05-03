aaab1514b249b0f15efc78cb8fca25928590fb93
s1341 <github@shmarya.net>
2021-05-03 10:26:06 +0300

    fmt

--- libafl_frida/src/helper.rs
+++ libafl_frida/src/helper.rs
@@ -9,7 +9,10 @@ use libafl::utils::find_mapping_for_path;
 use libafl_targets::drcov::{DrCovBasicBlock, DrCovWriter};
 
 #[cfg(target_arch = "aarch64")]
-use capstone::arch::{arm64::{Arm64OperandType, Arm64Extender, Arm64Shift}, ArchOperand::Arm64Operand};
+use capstone::arch::{
+    arm64::{Arm64Extender, Arm64OperandType, Arm64Shift},
+    ArchOperand::Arm64Operand,
+};
 use capstone::{
     arch::{self, BuildsCapstone},
     Capstone, Insn,
@@ -89,10 +92,7 @@ impl<'a> FridaHelper<'a> for FridaInstrumentationHelper<'a> {
             let mut hasher = AHasher::new_with_keys(0, 0);
             hasher.write(input.target_bytes().as_slice());
 
-            let filename = format!(
-                "./coverage/{:016x}.drcov",
-                hasher.finish(),
-            );
+            let filename = format!("./coverage/{:016x}.drcov", hasher.finish(),);
             DrCovWriter::new(&filename, &self.ranges, &mut self.drcov_basic_blocks).write();
         }
 
@@ -223,10 +223,14 @@ impl<'a> FridaInstrumentationHelper<'a> {
         if options.stalker_enabled() {
             for (id, module_name) in modules_to_instrument.iter().enumerate() {
                 let (lib_start, lib_end) = find_mapping_for_path(module_name.to_str().unwrap());
-                println!("including range {:x}-{:x} for {:?}", lib_start, lib_end, module_name);
-                helper
-                    .ranges
-                    .insert(lib_start..lib_end, (id as u16, module_name.to_str().unwrap()));
+                println!(
+                    "including range {:x}-{:x} for {:?}",
+                    lib_start, lib_end, module_name
+                );
+                helper.ranges.insert(
+                    lib_start..lib_end,
+                    (id as u16, module_name.to_str().unwrap()),
+                );
             }
 
             if helper.options.drcov_enabled() {
@@ -419,14 +423,18 @@ impl<'a> FridaInstrumentationHelper<'a> {
 
                 if extender_encoding != -1 && shift_amount < 0b1000 {
                     // emit add extended register: https://developer.arm.com/documentation/ddi0602/latest/Base-Instructions/ADD--extended-register---Add--extended-register--
-                    writer.put_bytes(&(0x8b210000 | ((extender_encoding as u32) << 13) | (shift_amount << 10)).to_le_bytes());
+                    writer.put_bytes(
+                        &(0x8b210000 | ((extender_encoding as u32) << 13) | (shift_amount << 10))
+                            .to_le_bytes(),
+                    );
                 } else if shift_encoding != -1 {
-                    writer.put_bytes(&(0x8b010000 | ((shift_encoding as u32) << 22) | (shift_amount << 10)).to_le_bytes());
+                    writer.put_bytes(
+                        &(0x8b010000 | ((shift_encoding as u32) << 22) | (shift_amount << 10))
+                            .to_le_bytes(),
+                    );
                 } else {
                     panic!("extender: {:?}, shift: {:?}", extender, shift);
                 }
-
-
             };
         }
 
@@ -500,8 +508,8 @@ impl<'a> FridaInstrumentationHelper<'a> {
             3 | 6 | 12 | 24 | 32 | 48 | 64 => {
                 let msr_nvcz_x0: u32 = 0xd51b4200;
                 writer.put_bytes(&msr_nvcz_x0.to_le_bytes());
-            },
-            _ => ()
+            }
+            _ => (),
         }
 
         // Restore x0, x1
@@ -586,7 +594,17 @@ impl<'a> FridaInstrumentationHelper<'a> {
         &self,
         _address: u64,
         instr: &Insn,
-    ) -> Result<(capstone::RegId, capstone::RegId, i32, u32, Arm64Shift, Arm64Extender), ()> {
+    ) -> Result<
+        (
+            capstone::RegId,
+            capstone::RegId,
+            i32,
+            u32,
+            Arm64Shift,
+            Arm64Extender,
+        ),
+        (),
+    > {
         // We have to ignore these instructions. Simulating them with their side effects is
         // complex, to say the least.
         match instr.mnemonic().unwrap() {
