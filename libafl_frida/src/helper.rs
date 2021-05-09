2c0548a757844e7e7ff93cbe79528c80922dc73e
Omree <you@example.com>
2021-05-09 19:01:34 +0300

    add target arch aarch64 for is_interesting_cmplog_instruction

--- libafl_frida/src/helper.rs
+++ libafl_frida/src/helper.rs
@@ -1030,6 +1030,7 @@ impl<'a> FridaInstrumentationHelper<'a> {
         Err(())
     }
 
+    #[cfg(target_arch = "aarch64")]
     #[inline]
     fn is_interesting_cmplog_instruction(
         &self,
