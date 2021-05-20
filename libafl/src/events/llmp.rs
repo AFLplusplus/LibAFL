aa14c903dba0dbf0da33d1e5a5329a64a748ae18
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-05-20 12:01:32 +0200

    fix update stats display

--- libafl/src/events/llmp.rs
+++ libafl/src/events/llmp.rs
@@ -181,17 +181,7 @@ where
                 // TODO: The stats buffer should be added on client add.
                 let client = stats.client_stats_mut_for(sender_id);
                 client.update_executions(*executions as u64, *time);
-                stats.display(event.name().to_string(), sender_id);
-                Ok(BrokerEventResult::Handled)
-            }
-            Event::UpdateUserStats {
-                name,
-                value,
-                phantom: _,
-            } => {
-                let client = stats.client_stats_mut_for(sender_id);
-                client.update_user_stats(name.clone(), value.clone());
-                stats.display(event.name().to_string(), sender_id);
+                stats.display(event.name().to_string() + " #" + &sender_id.to_string());
                 Ok(BrokerEventResult::Handled)
             }
             #[cfg(feature = "introspection")]
