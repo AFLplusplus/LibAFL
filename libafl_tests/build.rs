8d0e64bd6c89285480b17611c09e83ec796e4e3c
tokatoka <tokazerkje@outlook.com>
2021-05-25 18:57:00 +0900

    don't build libafl_tests on windows

--- libafl_tests/build.rs
+++ libafl_tests/build.rs
@@ -1,42 +1,50 @@
 use std::env;
 use std::path::Path;
-use std::process::Command;
+use std::process::{Command, exit};
 
 const AFL_URL: &str = "https://github.com/AFLplusplus/AFLplusplus";
 
 fn main() {
-    //let out_dir = env::var_os("OUT_DIR").unwrap().to_string_lossy().to_string();
-    let cwd = env::current_dir().unwrap().to_string_lossy().to_string();
 
-    let afl = format!("{}/AFLplusplus", &cwd);
-    let afl_gcc = format!("{}/AFLplusplus/afl-cc", &cwd);
 
-    let afl_path = Path::new(&afl);
-    let afl_gcc_path = Path::new(&afl_gcc);
+    if cfg!(feature = "forkserver_test"){
+        if cfg!(windows) {
+            println!("cargo:warning=No support for windows yet.");
+            exit(0);
+        }
 
-    if !afl_path.is_dir() {
-        println!("cargo:warning=AFL++ not found, downloading...");
-        Command::new("git")
-            .arg("clone")
-            .arg(AFL_URL)
+        let cwd = env::current_dir().unwrap().to_string_lossy().to_string();
+
+        let afl = format!("{}/AFLplusplus", &cwd);
+        let afl_gcc = format!("{}/AFLplusplus/afl-cc", &cwd);
+
+        let afl_path = Path::new(&afl);
+        let afl_gcc_path = Path::new(&afl_gcc);
+
+        if !afl_path.is_dir() {
+            println!("cargo:warning=AFL++ not found, downloading...");
+            Command::new("git")
+                .arg("clone")
+                .arg(AFL_URL)
+                .status()
+                .unwrap();
+        }
+
+        if !afl_gcc_path.is_file() {
+            Command::new("make")
+                .arg("all")
+                .current_dir(&afl_path)
+                .status()
+                .unwrap();
+        }
+
+        Command::new(afl_gcc_path)
+            .args(&["src/forkserver_test.c", "-o"])
+            .arg(&format!("{}/forkserver_test.o", "src"))
             .status()
             .unwrap();
+
+            println!("cargo:rerun-if-changed=build.rs");
+            println!("cargo:rerun-if-changed=src/");
     }
-
-    if !afl_gcc_path.is_file() {
-        Command::new("make")
-            .arg("all")
-            .current_dir(&afl_path)
-            .status()
-            .unwrap();
-    }
-
-    Command::new(afl_gcc_path)
-        .args(&["src/forkserver_test.c", "-o"])
-        .arg(&format!("{}/forkserver_test.o", "src"))
-        .status()
-        .unwrap();
-
-    println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src/");
 }
