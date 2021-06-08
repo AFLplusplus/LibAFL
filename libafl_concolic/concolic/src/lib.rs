43066ffa644c99d3b1c0699b26d878f22928bc0c
Julius Hohnerlein <julihoh@users.noreply.github.com>
2021-06-08 15:59:53 +0200

    implement tracing runtime to generate message file

---
+++ libafl_concolic/concolic/src/lib.rs
@@ -0,0 +1,621 @@
+use std::{
+    io::{self, Cursor, Read, Write},
+    num::NonZeroUsize,
+};
+
+pub use bincode::Result;
+use bincode::{DefaultOptions, Options};
+use serde::{Deserialize, Serialize};
+
+pub type SymExprRef = NonZeroUsize;
+
+fn serialization_options() -> DefaultOptions {
+    DefaultOptions::new()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Message {
+    GetInputByte {
+        offset: usize,
+    },
+
+    BuildInteger {
+        value: u64,
+        bits: u8,
+    },
+    BuildInteger128 {
+        high: u64,
+        low: u64,
+    },
+    BuildFloat {
+        value: f64,
+        is_double: bool,
+    },
+    BuildNullPointer,
+    BuildTrue,
+    BuildFalse,
+    BuildBool {
+        value: bool,
+    },
+
+    BuildNeg {
+        op: SymExprRef,
+    },
+    BuildAdd {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSub {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildMul {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedDiv {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSignedDiv {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedRem {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSignedRem {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildShiftLeft {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildLogicalShiftRight {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildArithmeticShiftRight {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildSignedLessThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSignedLessEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSignedGreaterThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildSignedGreaterEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedLessThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedLessEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedGreaterThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildUnsignedGreaterEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildNot {
+        op: SymExprRef,
+    },
+    BuildEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildNotEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildBoolAnd {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildBoolOr {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildBoolXor {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildAnd {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildOr {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildXor {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildFloatOrdered {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedGreaterThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedGreaterEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedLessThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedLessEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatOrderedNotEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildFloatUnordered {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedGreaterThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedGreaterEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedLessThan {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedLessEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatUnorderedNotEqual {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildFloatAbs {
+        op: SymExprRef,
+    },
+    BuildFloatAdd {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatSub {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatMul {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatDiv {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    BuildFloatRem {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+
+    BuildSext {
+        op: SymExprRef,
+        bits: u8,
+    },
+    BuildZext {
+        op: SymExprRef,
+        bits: u8,
+    },
+    BuildTrunc {
+        op: SymExprRef,
+        bits: u8,
+    },
+    BuildIntToFloat {
+        op: SymExprRef,
+        is_double: bool,
+        is_signed: bool,
+    },
+    BuildFloatToFloat {
+        op: SymExprRef,
+        to_double: bool,
+    },
+    BuildBitsToFloat {
+        op: SymExprRef,
+        to_double: bool,
+    },
+    BuildFloatToBits {
+        op: SymExprRef,
+    },
+    BuildFloatToSignedInteger {
+        op: SymExprRef,
+        bits: u8,
+    },
+    BuildFloatToUnsignedInteger {
+        op: SymExprRef,
+        bits: u8,
+    },
+    BuildBoolToBits {
+        op: SymExprRef,
+        bits: u8,
+    },
+
+    ConcatHelper {
+        a: SymExprRef,
+        b: SymExprRef,
+    },
+    ExtractHelper {
+        op: SymExprRef,
+        first_bit: usize,
+        last_bit: usize,
+    },
+    BuildExtract {
+        op: SymExprRef,
+        offset: u64,
+        length: u64,
+        little_endian: bool,
+    },
+    BuildBswap {
+        op: SymExprRef,
+    },
+
+    PushPathConstraint {
+        constraint: SymExprRef,
+        taken: bool,
+        site_id: usize,
+    },
+}
+
+pub struct MessageFileReader<R: Read> {
+    reader: R,
+    deserializer_config: bincode::DefaultOptions,
+    current_id: usize,
+}
+
+impl<R: Read> MessageFileReader<R> {
+    pub fn next_message(&mut self) -> Option<bincode::Result<(SymExprRef, Message)>> {
+        match self.deserializer_config.deserialize_from(&mut self.reader) {
+            Ok(mut message) => {
+                let message_id = self.transform_message(&mut message);
+                Some(Ok((message_id, message)))
+            }
+            Err(e) => match *e {
+                bincode::ErrorKind::Io(ref io_err) => match io_err.kind() {
+                    io::ErrorKind::UnexpectedEof => None,
+                    _ => Some(Err(e)),
+                },
+                _ => Some(Err(e)),
+            },
+        }
+    }
+
+    fn make_absolute(&self, expr: SymExprRef) -> SymExprRef {
+        NonZeroUsize::new(self.current_id - expr.get()).unwrap()
+    }
+
+    fn transform_message(&mut self, message: &mut Message) -> SymExprRef {
+        use Message::*;
+        let ret = self.current_id;
+        match message {
+            GetInputByte { .. }
+            | BuildInteger { .. }
+            | BuildInteger128 { .. }
+            | BuildFloat { .. }
+            | BuildNullPointer
+            | BuildTrue
+            | BuildFalse
+            | BuildBool { .. } => {
+                self.current_id += 1;
+            }
+            BuildNeg { op }
+            | BuildFloatAbs { op }
+            | BuildNot { op }
+            | BuildSext { op, .. }
+            | BuildZext { op, .. }
+            | BuildTrunc { op, .. }
+            | BuildIntToFloat { op, .. }
+            | BuildFloatToFloat { op, .. }
+            | BuildBitsToFloat { op, .. }
+            | BuildFloatToBits { op }
+            | BuildFloatToSignedInteger { op, .. }
+            | BuildFloatToUnsignedInteger { op, .. }
+            | BuildBoolToBits { op, .. }
+            | ExtractHelper { op, .. }
+            | BuildExtract { op, .. }
+            | BuildBswap { op } => {
+                *op = self.make_absolute(*op);
+                self.current_id += 1;
+            }
+            BuildAdd { a, b }
+            | BuildSub { a, b }
+            | BuildMul { a, b }
+            | BuildUnsignedDiv { a, b }
+            | BuildSignedDiv { a, b }
+            | BuildUnsignedRem { a, b }
+            | BuildSignedRem { a, b }
+            | BuildShiftLeft { a, b }
+            | BuildLogicalShiftRight { a, b }
+            | BuildArithmeticShiftRight { a, b }
+            | BuildSignedLessThan { a, b }
+            | BuildSignedLessEqual { a, b }
+            | BuildSignedGreaterThan { a, b }
+            | BuildSignedGreaterEqual { a, b }
+            | BuildUnsignedLessThan { a, b }
+            | BuildUnsignedLessEqual { a, b }
+            | BuildUnsignedGreaterThan { a, b }
+            | BuildUnsignedGreaterEqual { a, b }
+            | BuildEqual { a, b }
+            | BuildNotEqual { a, b }
+            | BuildBoolAnd { a, b }
+            | BuildBoolOr { a, b }
+            | BuildBoolXor { a, b }
+            | BuildAnd { a, b }
+            | BuildOr { a, b }
+            | BuildXor { a, b }
+            | BuildFloatOrdered { a, b }
+            | BuildFloatOrderedGreaterThan { a, b }
+            | BuildFloatOrderedGreaterEqual { a, b }
+            | BuildFloatOrderedLessThan { a, b }
+            | BuildFloatOrderedLessEqual { a, b }
+            | BuildFloatOrderedEqual { a, b }
+            | BuildFloatOrderedNotEqual { a, b }
+            | BuildFloatUnordered { a, b }
+            | BuildFloatUnorderedGreaterThan { a, b }
+            | BuildFloatUnorderedGreaterEqual { a, b }
+            | BuildFloatUnorderedLessThan { a, b }
+            | BuildFloatUnorderedLessEqual { a, b }
+            | BuildFloatUnorderedEqual { a, b }
+            | BuildFloatUnorderedNotEqual { a, b }
+            | BuildFloatAdd { a, b }
+            | BuildFloatSub { a, b }
+            | BuildFloatMul { a, b }
+            | BuildFloatDiv { a, b }
+            | BuildFloatRem { a, b }
+            | ConcatHelper { a, b } => {
+                *a = self.make_absolute(*a);
+                *b = self.make_absolute(*b);
+                self.current_id += 1;
+            }
+            PushPathConstraint { constraint: op, .. } => {
+                *op = self.make_absolute(*op);
+            }
+        }
+        NonZeroUsize::new(ret).unwrap()
+    }
+}
+
+impl<'buffer> MessageFileReader<Cursor<&'buffer [u8]>> {
+    pub fn new_from_buffer(buffer: &'buffer [u8]) -> Self {
+        let reader = Cursor::new(buffer);
+        Self {
+            reader,
+            deserializer_config: serialization_options(),
+            current_id: 1,
+        }
+    }
+}
+
+pub struct MessageFileWriter<W: Write> {
+    id_counter: usize,
+    writer: W,
+    serialization_options: DefaultOptions,
+}
+
+impl<W: Write> MessageFileWriter<W> {
+    pub fn new_from_writer(writer: W) -> Self {
+        Self {
+            writer,
+            id_counter: 1,
+            serialization_options: serialization_options(),
+        }
+    }
+
+    fn make_relative(&self, expr: SymExprRef) -> SymExprRef {
+        NonZeroUsize::new(self.id_counter - expr.get()).unwrap()
+    }
+
+    pub fn write_message(&mut self, mut message: Message) -> SymExprRef {
+        use Message::*;
+        let current_id = self.id_counter;
+        match &mut message {
+            GetInputByte { .. }
+            | BuildInteger { .. }
+            | BuildInteger128 { .. }
+            | BuildFloat { .. }
+            | BuildNullPointer
+            | BuildTrue
+            | BuildFalse
+            | BuildBool { .. } => {
+                self.id_counter += 1;
+            }
+            BuildNeg { op }
+            | BuildFloatAbs { op }
+            | BuildNot { op }
+            | BuildSext { op, .. }
+            | BuildZext { op, .. }
+            | BuildTrunc { op, .. }
+            | BuildIntToFloat { op, .. }
+            | BuildFloatToFloat { op, .. }
+            | BuildBitsToFloat { op, .. }
+            | BuildFloatToBits { op }
+            | BuildFloatToSignedInteger { op, .. }
+            | BuildFloatToUnsignedInteger { op, .. }
+            | BuildBoolToBits { op, .. }
+            | ExtractHelper { op, .. }
+            | BuildExtract { op, .. }
+            | BuildBswap { op } => {
+                *op = self.make_relative(*op);
+                self.id_counter += 1;
+            }
+            BuildAdd { a, b }
+            | BuildSub { a, b }
+            | BuildMul { a, b }
+            | BuildUnsignedDiv { a, b }
+            | BuildSignedDiv { a, b }
+            | BuildUnsignedRem { a, b }
+            | BuildSignedRem { a, b }
+            | BuildShiftLeft { a, b }
+            | BuildLogicalShiftRight { a, b }
+            | BuildArithmeticShiftRight { a, b }
+            | BuildSignedLessThan { a, b }
+            | BuildSignedLessEqual { a, b }
+            | BuildSignedGreaterThan { a, b }
+            | BuildSignedGreaterEqual { a, b }
+            | BuildUnsignedLessThan { a, b }
+            | BuildUnsignedLessEqual { a, b }
+            | BuildUnsignedGreaterThan { a, b }
+            | BuildUnsignedGreaterEqual { a, b }
+            | BuildEqual { a, b }
+            | BuildNotEqual { a, b }
+            | BuildBoolAnd { a, b }
+            | BuildBoolOr { a, b }
+            | BuildBoolXor { a, b }
+            | BuildAnd { a, b }
+            | BuildOr { a, b }
+            | BuildXor { a, b }
+            | BuildFloatOrdered { a, b }
+            | BuildFloatOrderedGreaterThan { a, b }
+            | BuildFloatOrderedGreaterEqual { a, b }
+            | BuildFloatOrderedLessThan { a, b }
+            | BuildFloatOrderedLessEqual { a, b }
+            | BuildFloatOrderedEqual { a, b }
+            | BuildFloatOrderedNotEqual { a, b }
+            | BuildFloatUnordered { a, b }
+            | BuildFloatUnorderedGreaterThan { a, b }
+            | BuildFloatUnorderedGreaterEqual { a, b }
+            | BuildFloatUnorderedLessThan { a, b }
+            | BuildFloatUnorderedLessEqual { a, b }
+            | BuildFloatUnorderedEqual { a, b }
+            | BuildFloatUnorderedNotEqual { a, b }
+            | BuildFloatAdd { a, b }
+            | BuildFloatSub { a, b }
+            | BuildFloatMul { a, b }
+            | BuildFloatDiv { a, b }
+            | BuildFloatRem { a, b }
+            | ConcatHelper { a, b } => {
+                *a = self.make_relative(*a);
+                *b = self.make_relative(*b);
+                self.id_counter += 1;
+            }
+            PushPathConstraint { constraint: op, .. } => {
+                *op = self.make_relative(*op);
+            }
+        }
+        self.serialization_options
+            .serialize_into(&mut self.writer, &message)
+            .expect("unable to serialize message");
+        NonZeroUsize::new(current_id).unwrap()
+    }
+}
+
+impl<T: ShMem> MessageFileWriter<ShmemCursor<T>> {
+    pub fn new_from_shmem(shmem: T) -> Self {
+        Self::new_from_writer(ShmemCursor::from_shmem(shmem))
+    }
+}
+
+impl MessageFileWriter<ShmemCursor<<StdShMemProvider as ShMemProvider>::Mem>> {
+    pub fn new_from_stdshmem_env(env_name: impl AsRef<str>) -> Self {
+        Self::new_from_shmem(
+            StdShMemProvider::new()
+                .expect("unable to initialize StdShMemProvider")
+                .existing_from_env(env_name.as_ref())
+                .expect("unable to get shared memory from env"),
+        )
+    }
+}
+
+use libafl::bolts::shmem::{ShMem, ShMemProvider, StdShMemProvider};
+
+pub type StdShMemMessageFileWriter = MessageFileWriter<ShmemCursor<<StdShMemProvider as ShMemProvider>::Mem>>;
+
+/// A cursor around [`ShMem`] that immitates [`std::io::Cursor`]. Notably, this implements [`Write`] for [`ShMem`].
+pub struct ShmemCursor<T: ShMem> {
+    inner: T,
+    pos: usize,
+}
+
+impl<T: ShMem> ShmemCursor<T> {
+    pub fn from_shmem(shmem: T) -> Self {
+        Self {
+            inner: shmem,
+            pos: 0,
+        }
+    }
+
+    fn slice_mut(&mut self) -> &mut [u8] {
+        &mut (self.inner.map_mut()[self.pos..])
+    }
+}
+
+impl<T: ShMem> std::io::Write for ShmemCursor<T> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        match self.slice_mut().write(buf) {
+            Ok(w) => {
+                self.pos += w;
+                Ok(w)
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn write_vectored(&mut self, bufs: &[io::IoSlice<'_>]) -> io::Result<usize> {
+        match self.slice_mut().write_vectored(bufs) {
+            Ok(w) => {
+                self.pos += w;
+                Ok(w)
+            }
+            Err(e) => Err(e),
+        }
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+        match self.slice_mut().write_all(buf) {
+            Ok(w) => {
+                self.pos += buf.len();
+                Ok(w)
+            }
+            Err(e) => Err(e),
+        }
+    }
+}
--- libafl_concolic/runtime/src/lib.rs
+++ libafl_concolic/runtime/src/lib.rs
@@ -1,11 +1,25 @@
 use ctor::{ctor, dtor};
 use std::env;
+use std::io::Cursor;
 
-struct State {}
+use concolic::{Message, MessageFileWriter, SymExprRef, StdShMemMessageFileWriter};
+
+struct State {
+    writer: StdShMemMessageFileWriter,
+}
 
 impl State {
     fn new() -> Self {
-        Self {}
+        let writer = MessageFileWriter::new_from_stdshmem_env("SHARED_MEMORY_MESSAGES");
+        Self { writer }
+    }
+
+    fn log_message(&mut self, mut message: Message) -> SymExprRef {
+        self.writer.write_message(message)
+    }
+
+    fn unwrap(&self, expr: Option<SymExprRef>) -> SymExprRef {
+        expr.unwrap()
     }
 }
 
@@ -30,11 +44,161 @@ fn with_state<R>(cb: impl FnOnce(&mut State) -> R) -> R {
 #[allow(clippy::missing_safety_doc)]
 #[no_mangle]
 pub unsafe extern "C" fn _sym_push_path_constraint(
-    constraint: *const std::ffi::c_void,
+    constraint: Option<SymExprRef>,
     taken: bool,
     site_id: usize,
 ) {
     with_state(|s| {
-        println!("just testing {}", site_id + 3);
+        s.log_message(Message::PushPathConstraint {
+            constraint: constraint.unwrap(),
+            taken,
+            site_id,
+        });
     })
 }
+
+macro_rules! expression_builder {
+    ($c_name:ident ( $($param_name:ident : $param_type:ty $( => $param_expr:tt)? ),+ ) => $message:ident) => {
+        #[allow(clippy::missing_safety_doc)]
+        #[no_mangle]
+        pub unsafe extern "C" fn $c_name( $( $param_name : $param_type, )+ ) -> SymExprRef {
+            with_state(|s| {
+                s.log_message(Message::$message { $($param_name $(: s.$param_expr($param_name))? ,)+ })
+            })
+        }
+    };
+    ($c_name:ident ( ) => $message:ident) => {
+        #[allow(clippy::missing_safety_doc)]
+        #[no_mangle]
+        pub unsafe extern "C" fn $c_name( ) -> SymExprRef {
+            with_state(|s| {
+                s.log_message(Message::$message)
+            })
+        }
+    };
+}
+
+expression_builder!(_sym_get_input_byte(offset: usize) => GetInputByte);
+
+expression_builder!(_sym_build_integer(value: u64, bits: u8) => BuildInteger);
+expression_builder!(_sym_build_integer128(high: u64, low: u64) => BuildInteger128);
+expression_builder!(_sym_build_float(value: f64, is_double: bool) => BuildFloat);
+expression_builder!(_sym_build_null_pointer() => BuildNullPointer);
+expression_builder!(_sym_build_true() => BuildTrue);
+expression_builder!(_sym_build_false() => BuildFalse);
+expression_builder!(_sym_build_bool(value: bool) => BuildBool);
+
+macro_rules! unary_expression_builder {
+    ($c_name:ident, $message:ident) => {
+        expression_builder!($c_name(op: Option<SymExprRef> => unwrap) => $message);
+    };
+}
+
+macro_rules! binary_expression_builder {
+    ($c_name:ident, $message:ident) => {
+        expression_builder!($c_name(a: Option<SymExprRef> => unwrap, b: Option<SymExprRef> => unwrap) => $message);
+    };
+}
+
+unary_expression_builder!(_sym_build_neg, BuildNeg);
+
+binary_expression_builder!(_sym_build_add, BuildAdd);
+binary_expression_builder!(_sym_build_sub, BuildSub);
+binary_expression_builder!(_sym_build_mul, BuildMul);
+binary_expression_builder!(_sym_build_unsigned_div, BuildUnsignedDiv);
+binary_expression_builder!(_sym_build_signed_div, BuildSignedDiv);
+binary_expression_builder!(_sym_build_unsigned_rem, BuildUnsignedRem);
+binary_expression_builder!(_sym_build_signed_rem, BuildSignedRem);
+binary_expression_builder!(_sym_build_shift_left, BuildShiftLeft);
+binary_expression_builder!(_sym_build_logical_shift_right, BuildLogicalShiftRight);
+binary_expression_builder!(_sym_build_arithmetic_shift_right, BuildArithmeticShiftRight);
+
+binary_expression_builder!(_sym_build_signed_less_than, BuildSignedLessThan);
+binary_expression_builder!(_sym_build_signed_less_equal, BuildSignedLessEqual);
+binary_expression_builder!(_sym_build_signed_greater_than, BuildSignedGreaterThan);
+binary_expression_builder!(_sym_build_signed_greater_equal, BuildSignedGreaterEqual);
+binary_expression_builder!(_sym_build_unsigned_less_than, BuildUnsignedLessThan);
+binary_expression_builder!(_sym_build_unsigned_less_equal, BuildUnsignedLessEqual);
+binary_expression_builder!(_sym_build_unsigned_greater_than, BuildUnsignedGreaterThan);
+binary_expression_builder!(_sym_build_unsigned_greater_equal, BuildUnsignedGreaterEqual);
+
+binary_expression_builder!(_sym_build_and, BuildAnd);
+binary_expression_builder!(_sym_build_or, BuildOr);
+binary_expression_builder!(_sym_build_xor, BuildXor);
+
+binary_expression_builder!(_sym_build_float_ordered, BuildFloatOrdered);
+binary_expression_builder!(
+    _sym_build_float_ordered_greater_than,
+    BuildFloatOrderedGreaterThan
+);
+binary_expression_builder!(
+    _sym_build_float_ordered_greater_equal,
+    BuildFloatOrderedGreaterEqual
+);
+binary_expression_builder!(
+    _sym_build_float_ordered_less_than,
+    BuildFloatOrderedLessThan
+);
+binary_expression_builder!(
+    _sym_build_float_ordered_less_equal,
+    BuildFloatOrderedLessEqual
+);
+binary_expression_builder!(_sym_build_float_ordered_equal, BuildFloatOrderedEqual);
+binary_expression_builder!(
+    _sym_build_float_ordered_not_equal,
+    BuildFloatOrderedNotEqual
+);
+
+binary_expression_builder!(_sym_build_float_unordered, BuildFloatUnordered);
+binary_expression_builder!(
+    _sym_build_float_unordered_greater_than,
+    BuildFloatUnorderedGreaterThan
+);
+binary_expression_builder!(
+    _sym_build_float_unordered_greater_equal,
+    BuildFloatUnorderedGreaterEqual
+);
+binary_expression_builder!(
+    _sym_build_float_unordered_less_than,
+    BuildFloatUnorderedLessThan
+);
+binary_expression_builder!(
+    _sym_build_float_unordered_less_equal,
+    BuildFloatUnorderedLessEqual
+);
+binary_expression_builder!(_sym_build_float_unordered_equal, BuildFloatUnorderedEqual);
+binary_expression_builder!(
+    _sym_build_float_unordered_not_equal,
+    BuildFloatUnorderedNotEqual
+);
+
+binary_expression_builder!(_sym_build_fp_add, BuildFloatAdd);
+binary_expression_builder!(_sym_build_fp_sub, BuildFloatSub);
+binary_expression_builder!(_sym_build_fp_mul, BuildFloatMul);
+binary_expression_builder!(_sym_build_fp_div, BuildFloatDiv);
+binary_expression_builder!(_sym_build_fp_rem, BuildFloatRem);
+
+unary_expression_builder!(_sym_build_fp_abs, BuildFloatAbs);
+
+unary_expression_builder!(_sym_build_not, BuildNot);
+binary_expression_builder!(_sym_build_equal, BuildEqual);
+binary_expression_builder!(_sym_build_not_equal, BuildNotEqual);
+binary_expression_builder!(_sym_build_bool_and, BuildBoolAnd);
+binary_expression_builder!(_sym_build_bool_or, BuildBoolOr);
+binary_expression_builder!(_sym_build_bool_xor, BuildBoolXor);
+
+expression_builder!(_sym_build_sext(op: Option<SymExprRef> => unwrap, bits: u8) => BuildSext);
+expression_builder!(_sym_build_zext(op: Option<SymExprRef> => unwrap, bits: u8) => BuildZext);
+expression_builder!(_sym_build_trunc(op: Option<SymExprRef> => unwrap, bits: u8) => BuildTrunc);
+expression_builder!(_sym_build_int_to_float(op: Option<SymExprRef> => unwrap, is_double: bool, is_signed: bool) => BuildIntToFloat);
+expression_builder!(_sym_build_float_to_float(op: Option<SymExprRef> => unwrap, to_double: bool) => BuildFloatToFloat);
+expression_builder!(_sym_build_bits_to_float(op: Option<SymExprRef> => unwrap, to_double: bool) => BuildBitsToFloat);
+expression_builder!(_sym_build_float_to_bits(op: Option<SymExprRef> => unwrap) => BuildFloatToBits);
+expression_builder!(_sym_build_float_to_signed_integer(op: Option<SymExprRef> => unwrap, bits: u8) => BuildFloatToSignedInteger);
+expression_builder!(_sym_build_float_to_unsigned_integer(op: Option<SymExprRef> => unwrap, bits: u8) => BuildFloatToUnsignedInteger);
+expression_builder!(_sym_build_bool_to_bits(op: Option<SymExprRef> => unwrap, bits: u8) => BuildBoolToBits);
+
+binary_expression_builder!(_sym_concat_helper, ConcatHelper);
+expression_builder!(_sym_extract_helper(op: Option<SymExprRef> => unwrap, first_bit:usize, last_bit:usize) => ExtractHelper);
+expression_builder!(_sym_build_extract(op: Option<SymExprRef> => unwrap, offset:u64, length:u64, little_endian:bool) => BuildExtract);
+unary_expression_builder!(_sym_build_bswap, BuildBswap);
