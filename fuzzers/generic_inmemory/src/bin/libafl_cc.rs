875876ff038d1b948f7874aa4cea50b7b23a505b
Dominik Maier <domenukk@gmail.com>
2021-06-14 17:59:28 +0200

    merged main

--- fuzzers/generic_inmemory/src/bin/libafl_cc.rs
+++ fuzzers/generic_inmemory/src/bin/libafl_cc.rs
@@ -10,17 +10,13 @@ fn main() {
         let mut cc = ClangWrapper::new("clang", "clang++");
         cc.from_args(&args)
             .unwrap()
-            .add_link_arg("-Wl,--whole-archive".into())
+            .add_arg("-fsanitize-coverage=trace-pc-guard,trace-cmp".into())
             .unwrap()
             .add_link_arg(
                 dir.join(format!("{}generic_inmemory.{}", LIB_PREFIX, LIB_EXT))
                     .display()
                     .to_string(),
             )
-            .unwrap()
-            .add_link_arg("-Wl,-no-whole-archive".into())
-            .unwrap()
-            .add_arg("-fsanitize-coverage=trace-pc-guard,trace-cmp".into())
             .unwrap();
         // Libraries needed by libafl on Windows
         #[cfg(windows)]
--- fuzzers/generic_inmemory/src/lib.rs
+++ fuzzers/generic_inmemory/src/lib.rs
@@ -37,12 +37,25 @@ use libafl_targets::{
     MAX_EDGES_NUM,
 };
 
+<<<<<<< HEAD
+=======
+extern "C" {
+    // void __sanitizer_cov_trace_cmp4(uint32_t arg1, uint32_t arg2)
+    fn __sanitizer_cov_trace_cmp4(a: u32, b: u32);
+}
+
+>>>>>>> 81f7397 (generic inmemory fuzzer)
 /// The main fn, `no_mangle` as it is a C main
 #[no_mangle]
 pub fn main() {
     // Registry the metadata types used in this fuzzer
     // Needed only on no_std
     //RegistryBuilder::register::<Tokens>();
+<<<<<<< HEAD
+=======
+    
+    unsafe { __sanitizer_cov_trace_cmp4(0,0) };
+>>>>>>> 81f7397 (generic inmemory fuzzer)
 
     let workdir = env::current_dir().unwrap();
 
