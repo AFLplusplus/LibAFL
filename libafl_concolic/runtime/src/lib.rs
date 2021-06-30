8207fc2fac7665d2c079fdaa408ae640a354838f
Julius Hohnerlein <julihoh@users.noreply.github.com>
2021-06-30 13:19:50 +0000

    ensure runtime can handle 0-expressions

--- libafl_concolic/runtime/src/lib.rs
+++ libafl_concolic/runtime/src/lib.rs
@@ -32,12 +32,6 @@ impl<F: ExpressionFilter> State<F> {
         }
     }
 
-    /// This is here to have a version of the normal [`Option::unwrap`] that is compatible with the `expression_builder`
-    /// macro below.
-    fn unwrap(&self, expr: Option<SymExprRef>) -> SymExprRef {
-        expr.unwrap()
-    }
-
     /// This is called at the end of the process, giving us the opprtunity to signal the end of the trace.
     fn end(mut self) {
         self.log_message(Message::End);
@@ -83,13 +77,15 @@ pub unsafe extern "C" fn _sym_push_path_constraint(
     taken: bool,
     site_id: usize,
 ) {
-    with_state(|s| {
-        s.log_message(Message::PushPathConstraint {
-            constraint: constraint.unwrap(),
-            taken,
-            site_id,
-        });
-    })
+    if let Some(constraint) = constraint {
+        with_state(|s| {
+            s.log_message(Message::PushPathConstraint {
+                constraint,
+                taken,
+                site_id,
+            });
+        })
+    }
 }
 
 // Call stack tracing
@@ -114,12 +110,12 @@ pub unsafe extern "C" fn _sym_notify_basic_block(location_id: usize) {
 /// A macro to generate the boilerplate for declaring a runtime function for SymCC that simply logs the function call
 /// according to [`concolic::Message`].
 macro_rules! expression_builder {
-    ($c_name:ident ( $($param_name:ident : $param_type:ty $( => $param_expr:tt)? ),+ ) => $message:ident) => {
+    ($c_name:ident ( $($param_name:ident : $param_type:ty $(=> $param_name2:ident ?)? ),+ ) => $message:ident) => {
         #[allow(clippy::missing_safety_doc)]
         #[no_mangle]
         pub unsafe extern "C" fn $c_name( $( $param_name : $param_type, )+ ) -> Option<SymExprRef> {
             with_state(|s| {
-                s.log_message(Message::$message { $($param_name $(: s.$param_expr($param_name))? ,)+ })
+                Some(s.log_message(Message::$message { $($param_name $(: $param_name2?)? ,)+ })?)
             })
         }
     };
@@ -128,7 +124,7 @@ macro_rules! expression_builder {
         #[no_mangle]
         pub unsafe extern "C" fn $c_name( ) -> Option<SymExprRef> {
             with_state(|s| {
-                s.log_message(Message::$message)
+                Some(s.log_message(Message::$message)?)
             })
         }
     };
@@ -146,13 +142,13 @@ expression_builder!(_sym_build_bool(value: bool) => BuildBool);
 
 macro_rules! unary_expression_builder {
     ($c_name:ident, $message:ident) => {
-        expression_builder!($c_name(op: Option<SymExprRef> => unwrap) => $message);
+        expression_builder!($c_name(op: Option<SymExprRef> => op?) => $message);
     };
 }
 
 macro_rules! binary_expression_builder {
     ($c_name:ident, $message:ident) => {
-        expression_builder!($c_name(a: Option<SymExprRef> => unwrap, b: Option<SymExprRef> => unwrap) => $message);
+        expression_builder!($c_name(a: Option<SymExprRef> => a?, b: Option<SymExprRef> => b?) => $message);
     };
 }
 
@@ -243,18 +239,18 @@ binary_expression_builder!(_sym_build_bool_and, BuildBoolAnd);
 binary_expression_builder!(_sym_build_bool_or, BuildBoolOr);
 binary_expression_builder!(_sym_build_bool_xor, BuildBoolXor);
 
-expression_builder!(_sym_build_sext(op: Option<SymExprRef> => unwrap, bits: u8) => BuildSext);
-expression_builder!(_sym_build_zext(op: Option<SymExprRef> => unwrap, bits: u8) => BuildZext);
-expression_builder!(_sym_build_trunc(op: Option<SymExprRef> => unwrap, bits: u8) => BuildTrunc);
-expression_builder!(_sym_build_int_to_float(op: Option<SymExprRef> => unwrap, is_double: bool, is_signed: bool) => BuildIntToFloat);
-expression_builder!(_sym_build_float_to_float(op: Option<SymExprRef> => unwrap, to_double: bool) => BuildFloatToFloat);
-expression_builder!(_sym_build_bits_to_float(op: Option<SymExprRef> => unwrap, to_double: bool) => BuildBitsToFloat);
-expression_builder!(_sym_build_float_to_bits(op: Option<SymExprRef> => unwrap) => BuildFloatToBits);
-expression_builder!(_sym_build_float_to_signed_integer(op: Option<SymExprRef> => unwrap, bits: u8) => BuildFloatToSignedInteger);
-expression_builder!(_sym_build_float_to_unsigned_integer(op: Option<SymExprRef> => unwrap, bits: u8) => BuildFloatToUnsignedInteger);
-expression_builder!(_sym_build_bool_to_bits(op: Option<SymExprRef> => unwrap, bits: u8) => BuildBoolToBits);
+expression_builder!(_sym_build_sext(op: Option<SymExprRef> => op?, bits: u8) => BuildSext);
+expression_builder!(_sym_build_zext(op: Option<SymExprRef> => op?, bits: u8) => BuildZext);
+expression_builder!(_sym_build_trunc(op: Option<SymExprRef> => op?, bits: u8) => BuildTrunc);
+expression_builder!(_sym_build_int_to_float(op: Option<SymExprRef> => op?, is_double: bool, is_signed: bool) => BuildIntToFloat);
+expression_builder!(_sym_build_float_to_float(op: Option<SymExprRef> => op?, to_double: bool) => BuildFloatToFloat);
+expression_builder!(_sym_build_bits_to_float(op: Option<SymExprRef> => op?, to_double: bool) => BuildBitsToFloat);
+expression_builder!(_sym_build_float_to_bits(op: Option<SymExprRef> => op?) => BuildFloatToBits);
+expression_builder!(_sym_build_float_to_signed_integer(op: Option<SymExprRef> => op?, bits: u8) => BuildFloatToSignedInteger);
+expression_builder!(_sym_build_float_to_unsigned_integer(op: Option<SymExprRef> => op?, bits: u8) => BuildFloatToUnsignedInteger);
+expression_builder!(_sym_build_bool_to_bits(op: Option<SymExprRef> => op?, bits: u8) => BuildBoolToBits);
 
 binary_expression_builder!(_sym_concat_helper, ConcatHelper);
-expression_builder!(_sym_extract_helper(op: Option<SymExprRef> => unwrap, first_bit:usize, last_bit:usize) => ExtractHelper);
-expression_builder!(_sym_build_extract(op: Option<SymExprRef> => unwrap, offset:u64, length:u64, little_endian:bool) => BuildExtract);
+expression_builder!(_sym_extract_helper(op: Option<SymExprRef> => op?, first_bit:usize, last_bit:usize) => ExtractHelper);
+expression_builder!(_sym_build_extract(op: Option<SymExprRef> => op?, offset:u64, length:u64, little_endian:bool) => BuildExtract);
 unary_expression_builder!(_sym_build_bswap, BuildBswap);
