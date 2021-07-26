6877be05d6f42ee987567e8c3a9dffc6cfcf4b71
tokatoka <tokazerkje@outlook.com>
2021-07-26 11:59:46 +0900

    no volatile rw

--- libafl_targets/src/sancov_pcguard.rs
+++ libafl_targets/src/sancov_pcguard.rs
@@ -19,12 +19,12 @@ pub unsafe extern "C" fn __sanitizer_cov_trace_pc_guard(guard: *mut u32) {
     let pos = *guard as usize;
     #[cfg(feature = "sancov_pcguard_edges")]
     {
-        (EDGES_MAP_PTR as *mut u8).add(pos).write_volatile(1);
+        (EDGES_MAP_PTR as *mut u8).add(pos).write(1);
     }
     #[cfg(feature = "sancov_pcguard_hitcounts")]
     {
-        let val = (EDGES_MAP_PTR as *mut u8).add(pos).read_volatile();
-        (EDGES_MAP_PTR as *mut u8).add(pos).write_volatile(val + 1);
+        let val = (EDGES_MAP_PTR as *mut u8).add(pos).read();
+        (EDGES_MAP_PTR as *mut u8).add(pos).write(val + 1);
     }
 }
 
