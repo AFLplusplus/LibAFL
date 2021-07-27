4245151b23c5938a10038af310d6b76ed583237d
tokatoka <tokazerkje@outlook.com>
2021-07-27 22:39:28 +0900

    mutable pointer

--- libafl_targets/src/coverage.rs
+++ libafl_targets/src/coverage.rs
@@ -1,11 +1,12 @@
 //! Coverage maps as static mut array
 
 use crate::EDGES_MAP_SIZE;
+use core::ptr;
 
 /// The map for edges.
 pub static mut EDGES_MAP: [u8; EDGES_MAP_SIZE] = [0; EDGES_MAP_SIZE];
 /// The pointer to the map.
-pub static mut EDGES_MAP_PTR: *const u8 = unsafe { EDGES_MAP.as_ptr() };
+pub static mut EDGES_MAP_PTR: *mut u8 = ptr::null_mut();
 
 /// The max count of edges tracked.
 pub static mut MAX_EDGES_NUM: usize = 0;
