2cb5502faeb4a18961d8dfd2e254eb08b4b85de6
Dominik Maier <domenukk@gmail.com>
2021-08-06 01:47:07 +0200

    not a doc comment

--- fuzzers/libfuzzer_stb_image_concolic/fuzzer/src/main.rs
+++ fuzzers/libfuzzer_stb_image_concolic/fuzzer/src/main.rs
@@ -66,7 +66,7 @@ pub fn main() {
 
 /// The actual fuzzer
 fn fuzz(corpus_dirs: &[PathBuf], objective_dir: PathBuf, broker_port: u16) -> Result<(), Error> {
-    /// Needed for MacOS and Android to get sharedmaps working.
+    // Needed for MacOS and Android to get sharedmaps working.
     let _service = StdShMemService::start().expect("Error starting ShMem Service");
 
     // 'While the stats are state, they are usually used in the broker - which is likely never restarted
