86aad549e3b37d6aeff99f389072a059dcf68bb9
Julius Hohnerlein <julihoh@users.noreply.github.com>
2021-08-06 10:11:03 +0000

    fix compilation of runtime of concolic example fuzzer

--- fuzzers/libfuzzer_stb_image_concolic/runtime/src/lib.rs
+++ fuzzers/libfuzzer_stb_image_concolic/runtime/src/lib.rs
@@ -1,14 +1,14 @@
+//! This is a basic SymCC runtime.
+//! It traces the execution to the shared memory region that should be passed through the environment by the fuzzer process. 
+//! Additionally, it concretizes all floating point operations for simplicity.
+//! Refer to the `symcc_runtime` crate documentation for building your own runtime.
+
 use symcc_runtime::{
     export_runtime,
     filter::NoFloat,
     tracing::{self, StdShMemMessageFileWriter},
     Runtime,
 };
-
-//! This is a basic SymCC runtime.
-//! It traces the execution to the shared memory region that should be passed through the environment by the fuzzer process. 
-//! Additionally, it concretizes all floating point operations for simplicity.
-//! Refer to the `symcc_runtime` crate documentation for building your own runtime.
 
 export_runtime!(
     NoFloat => NoFloat;
