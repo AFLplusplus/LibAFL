ebea1e684b60a9548d67fe35ec15801f6929e0d6
Dominik Maier <domenukk@gmail.com>
2021-08-08 02:53:51 +0200

    fmt

--- fuzzers/libfuzzer_reachability/src/lib.rs
+++ fuzzers/libfuzzer_reachability/src/lib.rs
@@ -47,7 +47,6 @@ pub fn libafl_main() {
 
 /// The actual fuzzer
 fn fuzz(corpus_dirs: &[PathBuf], objective_dir: PathBuf, broker_port: u16) -> Result<(), Error> {
-
     // 'While the stats are state, they are usually used in the broker - which is likely never restarted
     let stats = SimpleStats::new(|s| println!("{}", s));
 
