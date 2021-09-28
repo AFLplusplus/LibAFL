c51645e3d31f08776efa9c69bdd906b019587eaa
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-09-28 17:33:55 +0200

    __ubsan_default_options

--- libafl_targets/src/common.c
+++ libafl_targets/src/common.c
@@ -8,3 +8,13 @@ EXT_FUNC_IMPL(__asan_default_options, const char*, (), false) {
          "handle_sigbus=0:handle_abort=0:"
          "handle_sigfpe=0:handle_sigill=0";
 }
+
+EXT_FUNC_IMPL(__ubsan_default_options, const char*, (), false) {
+  return "abort_on_error=1:"
+         "allocator_release_to_os_interval_ms=500:"
+         "handle_abort=0:handle_segv=0:"
+         "handle_sigbus=0:handle_sigfpe=0:"
+         "handle_sigill=0:print_stacktrace=0:"
+         "symbolize=0:symbolize_inline_frames=0";
+}
+
