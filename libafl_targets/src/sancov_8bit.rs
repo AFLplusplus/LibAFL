7cf78e861ec7adf1d970dd43e363061927e4f7fe
Andrea Fioraldi <andreafioraldi@gmail.com>
2021-11-04 09:41:58 +0100

    sancov_8bit targets feature

--- libafl_targets/src/sancov_8bit.rs
+++ libafl_targets/src/sancov_8bit.rs
@@ -2,7 +2,7 @@
 use alloc::vec::Vec;
 use core::slice::from_raw_parts_mut;
 
-pub static mut COUNTERS_MAPS: Vec<&'mut static [u8]> = Vec::new();
+pub static mut COUNTERS_MAPS: Vec<&'static mut [u8]> = Vec::new();
 
 /// Initialize the sancov `8-bit-counters` - usually called by `llvm`.
 ///
@@ -10,8 +10,5 @@ pub static mut COUNTERS_MAPS: Vec<&'mut static [u8]> = Vec::new();
 /// Set up our coverage maps.
 #[no_mangle]
 pub fn __sanitizer_cov_8bit_counters_init(start: *mut u8, stop: *mut u8) {
-    unsafe {
-        COUNTERS_MAPS.push(from_raw_parts_mut(start, stop.offset_from(start)))
-    }
+    unsafe { COUNTERS_MAPS.push(from_raw_parts_mut(start, stop.offset_from(start) as usize)) }
 }
-
