d399210d257ecef1639bf3e5defac7fa0a2a96b3
Dominik Maier <domenukk@gmail.com>
2021-11-11 02:34:23 +0100

    fmt all fuzzers script

---
+++ scripts/fmt_all_fuzzres.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd "$SCRIPT_DIR/.."
+
+# TODO: This should be rewritten in rust, a Makefile, or some platform-independent language
+
+cd fuzzers
+
+for fuzzer in *;
+do
+    cd $fuzzer
+    echo "[*] Running fmt for $fuzzer"
+    cargo fmt --all
+    cd ..
+done
