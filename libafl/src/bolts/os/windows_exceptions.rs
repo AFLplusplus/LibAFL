4d082cd4c1c628dec01fecbfaea831217309fc20
tokatoka <tokazerkje@outlook.com>
2021-12-05 08:18:48 +0900

    fix

--- libafl/src/bolts/os/windows_exceptions.rs
+++ libafl/src/bolts/os/windows_exceptions.rs
@@ -1,7 +1,7 @@
 //! Exception handling for Windows
 
 pub use windows::Win32::System::Diagnostics::Debug::{
-    SetUnhandledExceptionFilter, EXCEPTION_POINTERS, AddVectoredExceptionHandler,
+    AddVectoredExceptionHandler, EXCEPTION_POINTERS,
 };
 
 pub use windows::Win32::Foundation::NTSTATUS;
@@ -362,9 +362,10 @@ pub unsafe fn setup_exception_handler<T: 'static + Handler>(handler: &mut T) -> 
     if catch_assertions {
         signal(SIGABRT, handle_signal);
     }
-    if let prev = AddVectoredExceptionHandler(1, Some(core::mem::transmute(
-        handle_exception as *const c_void,
-    ))) {
+    if let prev = AddVectoredExceptionHandler(
+        1,
+        Some(core::mem::transmute(handle_exception as *const c_void)),
+    ) {
         PREVIOUS_HANDLER = Some(core::mem::transmute(prev as *const c_void));
     }
     Ok(())
