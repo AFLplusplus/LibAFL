efe05d0b0cc91e04e2a3952aa0ff71ee96d3a663
Dominik Maier <domenukk@gmail.com>
2022-01-31 02:51:08 +0100

    Err instead of empty tokens

--- libafl/src/mutators/token_mutations.rs
+++ libafl/src/mutators/token_mutations.rs
@@ -66,7 +66,7 @@ impl Tokens {
     pub unsafe fn from_ptrs(token_start: *const u8, token_stop: *const u8) -> Result<Self, Error> {
         let ret = Self::default();
         if token_start == null() || token_stop == null() {
-            return Ok(ret);
+            return Err(Err::IllegalArgument("token_start or token_stop is null. If you are using autotokens() you likely did not build your target with the \"AutoTokens\"-pass"));
         }
         if token_stop <= token_start {
             return Err(Error::IllegalArgument(format!(
