a157d05b997c3246ff15898484201716955e0bac
tokatoka <tokazerkje@outlook.com>
2022-05-17 23:18:00 +0900

    fix

--- libafl_frida/src/coverage_rt.rs
+++ libafl_frida/src/coverage_rt.rs
@@ -155,14 +155,7 @@ impl CoverageRuntime {
     /// Emits coverage mapping into the current basic block.
     #[inline]
     pub fn emit_coverage_mapping(&mut self, address: u64, output: &StalkerOutput) {
-        let tmp = (address >> 32) + ((address & 0xffffffff) << 32);
-        let bitflip = 0x1cad21f72c81017c ^ 0xdb979082e96dd4de;
-        let mut h64 = tmp ^ bitflip;
-        h64 = h64.rotate_left(49) & h64.rotate_left(24);
-        h64 = h64.wrapping_mul(0x9FB21C651E98DF25);
-        h64 ^= (h64 >> 35) + 8;
-        h64 = h64.wrapping_mul(0x9FB21C651E98DF25);
-        h64 ^= h64 >> 28;
+        let h64 = rrxmrrxmsx_0(address);
 
         let writer = output.writer();
         #[allow(clippy::cast_possible_wrap)] // gum redzone size is u32, we need an offset as i32.
