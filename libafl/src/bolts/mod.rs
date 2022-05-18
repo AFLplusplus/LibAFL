d1a78892b55df958c5ff5cb7f0d9ece08afb508b
tokatoka <tokazerkje@outlook.com>
2022-05-18 18:17:03 +0900

    fix

--- libafl/src/bolts/mod.rs
+++ libafl/src/bolts/mod.rs
@@ -110,7 +110,7 @@ pub fn current_time() -> time::Duration {
     time::Duration::from_millis(millis)
 }
 
-/// Given a u64 number, return a random number using this mixing function
+/// Given a u64 number, return a hashed number using this mixing function
 /// This function is used to hash an address into a more random number (used in libafl_frida).
 /// Mixing function: <http://mostlymangling.blogspot.com/2018/07/on-mixing-functions-in-fast-splittable.html>
 #[inline]
