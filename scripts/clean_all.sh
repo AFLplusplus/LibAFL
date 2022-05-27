194e0d0e0fbe931d7c1960c95c54cdc8471c5c98
Dominik Maier <dmnk@google.com>
2022-05-27 08:25:21 +0200

    shellcheck

--- scripts/clean_all.sh
+++ scripts/clean_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd "$SCRIPT_DIR/.."
+cd "$SCRIPT_DIR/.." || exit 1
 
 # TODO: This should be rewritten in rust, a Makefile, or some platform-independent language
 
@@ -12,10 +12,10 @@ cargo clean
 fuzzers=$(find ./fuzzers -maxdepth 1 -type d)
 backtrace_fuzzers=$(find ./fuzzers/backtrace_baby_fuzzers -maxdepth 1 -type d)
 
-for fuzzer in $(echo $fuzzers $backtrace_fuzzers);
+for fuzzer in $(echo "$fuzzers" "$backtrace_fuzzers");
 do
-    pushd $fuzzer
+    pushd "$fuzzer" || exit 1
     echo "[*] Running clean for $fuzzer"
     cargo clean
-    popd
+    popd || exit 1
 done
