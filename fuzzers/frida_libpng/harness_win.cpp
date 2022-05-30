8417af98154126fc8c2ed4b5f16936855c3147d2
Dongjia Zhang <tokazerkje@outlook.com>
2022-05-30 17:52:54 +0900

    Update harness_win.cpp

--- fuzzers/frida_libpng/harness_win.cpp
+++ fuzzers/frida_libpng/harness_win.cpp
@@ -7,7 +7,11 @@ extern "C" __declspec(dllexport) size_t
     LLVMFuzzerTestOneInput(const char *data, unsigned int len) {
   if (data[0] == 'b') {
     if (data[1] == 'a') {
-      if (data[2] == 'd') {}
+      if (data[2] == 'd') {
+        // STATUS_ACCESS_VIOLATION
+        int *p = 0x0;
+        *p = 0;
+      }
     }
   }
   return 0;
