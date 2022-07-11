c4da0a7fd47230435040439756b1cef8acf854a8
syheliel <syheliel@gmail.com>
2022-07-11 20:27:13 +0800

    add `test_nyxhelper`

--- libafl_nyx/src/lib.rs
+++ libafl_nyx/src/lib.rs
@@ -1,2 +1,3 @@
 pub mod executor;
 pub mod nyx_bridge;
+pub mod test;
--- libafl_nyx/src/test.rs
+++ libafl_nyx/src/test.rs
@@ -1,7 +1,14 @@
+use std::{ffi::OsString, path::PathBuf, str::FromStr};
+
 use libafl::{bolts::shmem::StdShMemProvider, monitors::MultiMonitor};
 
+use crate::executor::NyxHelper;
+
 #[test]
-fn test_main() {
-    let mon = MultiMonitor::new(|s| println!("{:?}", s));
-    let shmem_provider = StdShMemProvider::new();
+fn test_nyxhelper() {
+    let share_dir = PathBuf::from_str("/tmp/nyx_libxml2/").unwrap();
+    let cpu_id = 0;
+    let enable_snap_mode = true;
+    let nyx_type = crate::executor::NyxProcessType::ALONE;
+    let helper = NyxHelper::new(share_dir, cpu_id, enable_snap_mode, nyx_type);
 }
