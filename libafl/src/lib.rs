d5bd9527bcabe2f006c2ddbe31ded0444be1c6ae
toka <tokazerkje@outlook.com>
2022-08-01 13:02:55 +0200

    fix

--- libafl/src/lib.rs
+++ libafl/src/lib.rs
@@ -25,7 +25,8 @@ Welcome to `LibAFL`
     clippy::missing_panics_doc,
     clippy::missing_docs_in_private_items,
     clippy::module_name_repetitions,
-    clippy::unreadable_literal
+    clippy::unreadable_literal,
+    clippy::assertions_on_result_states,
 )]
 #![cfg_attr(debug_assertions, warn(
     missing_debug_implementations,
