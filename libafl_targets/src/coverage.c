b30549e198f568e2a4377a750e6bc3053823a512
toka <tokazerkje@outlook.com>
2022-08-12 18:46:52 +0200

    fmt

--- libafl_targets/src/coverage.c
+++ libafl_targets/src/coverage.c
@@ -9,10 +9,10 @@ typedef uint32_t prev_loc_t;
 #define CTX_MAX_K 32U
 
 extern uint8_t __afl_area_ptr_local[EDGES_MAP_SIZE];
-uint8_t* __afl_area_ptr = __afl_area_ptr_local;
+uint8_t       *__afl_area_ptr = __afl_area_ptr_local;
 
 extern uint32_t __afl_acc_memop_ptr_local[ACCOUNTING_MAP_SIZE];
-uint32_t* __afl_acc_memop_ptr = __afl_acc_memop_ptr_local;
+uint32_t       *__afl_acc_memop_ptr = __afl_acc_memop_ptr_local;
 
 // Weak symbols, LLVM Passes overwrites them if we really use it
 #if defined(__linux__)
@@ -20,14 +20,15 @@ extern EXT_VAR(__start_libafl_token, uint8_t);
 extern EXT_VAR(__stop_libafl_token, uint8_t);
 
 #elif defined(__APPLE__)
-extern uint8_t __start_libafl_token __asm("section$start$__DATA$__libafl_token");
+extern uint8_t __start_libafl_token __asm(
+    "section$start$__DATA$__libafl_token");
 extern uint8_t __stop_libafl_token __asm("section$end$__DATA$__libafl_token");
 #endif
 
-#if defined (__linux__) || defined(__APPLE__)
+#if defined(__linux__) || defined(__APPLE__)
 // Expose the start of libafl_token section as C symbols
-uint8_t* __token_start = &__start_libafl_token;
-uint8_t* __token_stop = &__stop_libafl_token;
+uint8_t *__token_start = &__start_libafl_token;
+uint8_t *__token_stop = &__stop_libafl_token;
 
 #endif
 
