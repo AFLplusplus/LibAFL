ea3e1632b17ab19d750e654cf0f85f367f930e87
syheliel <syheliel@gmail.com>
2022-08-17 14:09:46 +0800

    add check for setup script

--- fuzzers/nyx_libxml2_standalone/setup_libxml2.sh
+++ fuzzers/nyx_libxml2_standalone/setup_libxml2.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 cargo build --release
-export CC=`pwd`/target/release/libafl_cc
-export CXX=`pwd`/target/release/libafl_cxx
+PWD=$(pwd)
+export CC="$PWD/target/release/libafl_cc"
+export CXX="$PWD/target/release/libafl_cxx"
 curl -C - https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.14/libxml2-v2.9.14.tar.gz --output libxml2-v2.9.14.tar.gz
-tar -xvf ./libxml2-v2.9.14.tar.gz  --transform s/libxml2-v2.9.14/libxml2/
+tar -xvf ./libxml2-v2.9.14.tar.gz  --transform s/libxml2-v2.9.14/libxml2/ || exit
 cd ./libxml2/ || exit
-./autogen.sh --enable-shared=no
-make
+./autogen.sh --enable-shared=no || exit
+make -j || exit
 cd - || exit
 python3 "../../libafl_nyx/packer/packer/nyx_packer.py" \
     ./libxml2/xmllint \
@@ -16,9 +17,9 @@ python3 "../../libafl_nyx/packer/packer/nyx_packer.py" \
     -args "/tmp/input" \
     -file "/tmp/input" \
     --fast_reload_mode \
-    --purge
+    --purge || exit
 
-python3 ../../libafl_nyx/packer/packer/nyx_config_gen.py /tmp/nyx_libxml2/ Kernel
+python3 ../../libafl_nyx/packer/packer/nyx_config_gen.py /tmp/nyx_libxml2/ Kernel || exit
 sudo modprobe -r kvm-intel # or kvm-amd for AMD 
 sudo modprobe -r kvm
 sudo modprobe kvm enable_vmware_backdoor=y
