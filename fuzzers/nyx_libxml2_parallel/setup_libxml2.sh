cc26c73d08d0fb9f4c5a1cff0c62e6a6aadc3a16
syheliel <syheliel@gmail.com>
2022-08-24 12:08:20 +0800

    fix CI

--- fuzzers/nyx_libxml2_parallel/setup_libxml2.sh
+++ fuzzers/nyx_libxml2_parallel/setup_libxml2.sh
@@ -7,7 +7,7 @@
 export CC=afl-clang-fast
 export CXX=afl-clang-fast++
 curl -C - https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.9.14/libxml2-v2.9.14.tar.gz --output libxml2-v2.9.14.tar.gz
-tar -xvf ./libxml2-v2.9.14.tar.gz  --transform s/libxml2-v2.9.14/libxml2/ || exit
+tar -xf ./libxml2-v2.9.14.tar.gz  --transform s/libxml2-v2.9.14/libxml2/ || exit
 cd ./libxml2/ || exit
 ./autogen.sh --enable-shared=no || exit
 make -j || exit
