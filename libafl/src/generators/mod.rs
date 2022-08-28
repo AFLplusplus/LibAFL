141ab0b0b59308b36c3b7432ca0c96748f79821e
Dominik Maier <dmnk@google.com>
2022-08-28 09:35:17 +0200

    Skippable stage, generator wrapper for Grimoire

--- libafl/src/generators/mod.rs
+++ libafl/src/generators/mod.rs
@@ -5,7 +5,7 @@ use core::{cmp::min, marker::PhantomData};
 
 use crate::{
     bolts::rands::Rand,
-    inputs::{bytes::BytesInput, Input},
+    inputs::{bytes::BytesInput, GeneralizedInput, Input},
     state::HasRand,
     Error,
 };
@@ -31,6 +31,52 @@ where
 
     /// Generate a new dummy input
     fn generate_dummy(&self, state: &mut S) -> I;
+}
+
+/// A Generator that produces [`GeneralizedInput`]s from a wrapped [`BytesInput`] generator
+#[derive(Clone, Debug)]
+pub struct GeneralizedInputBytesGenerator<G, S> {
+    bytes_generator: G,
+    phantom: PhantomData<S>,
+}
+
+impl<G, S> GeneralizedInputBytesGenerator<G, S>
+where
+    S: HasRand,
+    G: Generator<BytesInput, S>,
+{
+    /// Creates a new [`GeneralizedInputBytesGenerator`] by wrapping a bytes generator.
+    pub fn new(bytes_generator: G) -> Self {
+        Self {
+            bytes_generator,
+            phantom: PhantomData,
+        }
+    }
+}
+
+impl<G, S> From<G> for GeneralizedInputBytesGenerator<G, S>
+where
+    S: HasRand,
+    G: Generator<BytesInput, S>,
+{
+    fn from(bytes_generator: G) -> Self {
+        Self::new(bytes_generator)
+    }
+}
+
+impl<G, S> Generator<GeneralizedInput, S> for GeneralizedInputBytesGenerator<G, S>
+where
+    S: HasRand,
+    G: Generator<BytesInput, S>,
+{
+    fn generate(&mut self, state: &mut S) -> Result<GeneralizedInput, Error> {
+        let bytes_input = self.bytes_generator.generate(state)?;
+        Ok(GeneralizedInput::new(bytes_input.bytes))
+    }
+
+    fn generate_dummy(&self, state: &mut S) -> GeneralizedInput {
+        GeneralizedInput::new(self.bytes_generator.generate_dummy(state).bytes)
+    }
 }
 
 #[derive(Clone, Debug)]
