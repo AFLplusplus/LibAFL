67c94b59485737cd57a044002824f72a659987c1
Dominik Maier <dmnk@google.com>
2022-09-06 00:13:57 +0200

    cleanup

--- libafl/src/corpus/testcase.rs
+++ libafl/src/corpus/testcase.rs
@@ -9,7 +9,7 @@ use serde::{Deserialize, Serialize};
 use crate::{
     bolts::{serdeany::SerdeAnyMap, HasLen},
     inputs::Input,
-    prelude::HasMetadata,
+    state::HasMetadata,
     Error,
 };
 
