53c6de46a4e34ec57ac3168d0bd1c9f5cec2d59a
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-10-19 10:23:14 +0200

    Fix pcrel to 1

--- libafl_qemu/build_linux.rs
+++ libafl_qemu/build_linux.rs
@@ -3,7 +3,7 @@ use which::which;
 
 const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-const QEMU_REVISION: &str = "c3a14af2d3b561794c85f4aeaa70e9a988e44f70";
+const QEMU_REVISION: &str = "35d36bf8fa2d483965a57ee0c7d7a997e8798273";
 
 fn build_dep_check(tools: &[&str]) {
     for tool in tools {
