cf9c4188c081e1e4a4020d0e50e8dab1fe22fb5a
Patrick Gersch <gersch.patrick@gmail.com>
2022-10-25 14:16:11 +0200

    Disabling qemu dependecies for qemu fullsystem (#737)
    * Disabling qemu dependecies by default
    
    * Adding full emulation_mode support
    
    * Removing usermode from libafl_qemu default features
    
    * Fixing refactoring
    
    * Fixing typo in systemmode
    
    * Fixing clippy:needless-borrow
    
    * Mark libafl_load/save_qemu_snapshot as unused + cpu_reset
    
    * Fixing clippy::needless-borrow
    
    * Fixing needless-borrow yet again
    
    * reset_cpu -> cpu_reset
    
    * Fixing github workflow yet again
    
    * Fixing clippy::uninlined-format-args
    
    * Adding current libafl_qemu_bridge
    
    Co-authored-by: Andrea Fioraldi <andreafioraldi@gmail.com>

--- libafl_qemu/build_linux.rs
+++ libafl_qemu/build_linux.rs
@@ -3,7 +3,7 @@ use which::which;
 
 const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-const QEMU_REVISION: &str = "35d36bf8fa2d483965a57ee0c7d7a997e8798273";
+const QEMU_REVISION: &str = "ddb71cf43844f8848ae655ca696bdfc3fb7839f1";
 
 fn build_dep_check(tools: &[&str]) {
     for tool in tools {
@@ -26,10 +26,25 @@ macro_rules! assert_unique_feature {
 
 #[allow(clippy::too_many_lines)]
 pub fn build() {
+    // Make sure that exactly one qemu mode is set
+    assert_unique_feature!("usermode", "systemmode");
+    let emulation_mode = if cfg!(feature = "usermode") {
+        "usermode".to_string()
+    }else if cfg!(feature = "systemmode") {
+        "systemmode".to_string()
+    }else{
+        env::var("EMULATION_MODE").unwrap_or_else(|_| {
+            println!(
+                "cargo:warning=No emulation mode feature enabled or EMULATION_MODE env specified for libafl_qemu, supported: usermode, systemmmode - defaulting to usermode"
+            );
+            "usermode".to_string()
+        })
+    };
+    println!("cargo:rustc-cfg=emulation_mode=\"{emulation_mode}\"");
+
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/asan-giovese.c");
     println!("cargo:rerun-if-changed=src/asan-giovese.h");
-    #[cfg(feature = "usermode")]
     println!("cargo:rerun-if-env-changed=CROSS_CC");
 
     // Make sure we have at most one architecutre feature set
@@ -60,7 +75,6 @@ pub fn build() {
 
     let jobs = env::var("NUM_JOBS");
 
-    #[cfg(feature = "usermode")]
     let cross_cc = env::var("CROSS_CC").unwrap_or_else(|_| {
         println!("cargo:warning=CROSS_CC is not set, default to cc (things can go wrong if the selected cpu target ({cpu_target}) is not the host arch ({}))", env::consts::ARCH);
         "cc".to_owned()
@@ -153,20 +167,20 @@ pub fn build() {
         qemu_path
     };
 
-    #[cfg(feature = "usermode")]
-    let target_suffix = "linux-user";
-    #[cfg(not(feature = "usermode"))]
-    let target_suffix = "softmmu";
+    let build_dir = qemu_path.join("build");
 
-    let build_dir = out_dir_path.join("build");
-    if !build_dir.is_dir() {
-        fs::create_dir_all(&build_dir).unwrap();
-    }
 
-    #[cfg(feature = "usermode")]
-    let output_lib = build_dir.join(&format!("libqemu-{cpu_target}.so"));
-    #[cfg(not(feature = "usermode"))]
-    let output_lib = build_dir.join(&format!("libqemu-system-{}.so", cpu_target));
+    let target_suffix = if emulation_mode == "usermode" {
+        "linux-user".to_string()
+    }else{
+        "softmmu".to_string()
+    };
+
+    let output_lib = if emulation_mode == "usermode" {
+        build_dir.join(&format!("libqemu-{cpu_target}.so"))
+    }else{
+        build_dir.join(&format!("libqemu-system-{cpu_target}.so"))
+    };
 
     println!("cargo:rerun-if-changed={}", output_lib.to_string_lossy());
 
@@ -177,25 +191,134 @@ pub fn build() {
                 .arg("distclean")
                 .status(),
         );*/
-        let configure = qemu_path.join("configure");
-
-        #[cfg(feature = "usermode")]
-        Command::new(configure)
-            .current_dir(&build_dir)
-            //.arg("--as-static-lib")
-            .arg("--as-shared-lib")
-            .arg(&format!("--target-list={cpu_target}-{target_suffix}"))
-            .args(["--disable-blobs", "--disable-bsd-user", "--disable-fdt"])
-            .status()
-            .expect("Configure failed");
-        #[cfg(not(feature = "usermode"))]
-        Command::new(configure)
-            .current_dir(&build_dir)
-            //.arg("--as-static-lib")
-            .arg("--as-shared-lib")
-            .arg(&format!("--target-list={}-{}", cpu_target, target_suffix))
-            .status()
-            .expect("Configure failed");
+        if emulation_mode == "usermode" {
+            Command::new("./configure")
+                .current_dir(&qemu_path)
+                //.arg("--as-static-lib")
+                .arg("--as-shared-lib")
+                .arg(&format!("--target-list={cpu_target}-{target_suffix}"))
+                .args(["--disable-blobs", "--disable-bsd-user", "--disable-fdt", "--disable-system"])
+                .status()
+                .expect("Configure failed");
+        }else{
+            Command::new("./configure")
+                .current_dir(&qemu_path)
+                //.arg("--as-static-lib")
+                .arg("--as-shared-lib")
+                .arg(&format!("--target-list={cpu_target}-{target_suffix}"))
+                .arg("--enable-slirp=internal")
+                .arg("--enable-fdt=internal")
+                .arg("--audio-drv-list=")
+                .arg("--disable-alsa")
+                .arg("--disable-attr")
+                .arg("--disable-auth-pam")
+                .arg("--disable-dbus-display")
+                .arg("--disable-blobs")
+                .arg("--disable-bochs")
+                .arg("--disable-bpf")
+                .arg("--disable-brlapi")
+                .arg("--disable-bsd-user")
+                .arg("--disable-bzip2")
+                .arg("--disable-cap-ng")
+                .arg("--disable-canokey")
+                .arg("--disable-cloop")
+                .arg("--disable-cocoa")
+                .arg("--disable-coreaudio")
+                .arg("--disable-curl")
+                .arg("--disable-curses")
+                .arg("--disable-dmg")
+                .arg("--disable-docs")
+                .arg("--disable-dsound")
+                .arg("--disable-fuse")
+                .arg("--disable-fuse-lseek")
+                .arg("--disable-gcrypt")
+                .arg("--disable-gettext")
+                .arg("--disable-gio")
+                .arg("--disable-glusterfs")
+                .arg("--disable-gnutls")
+                .arg("--disable-gtk")
+                .arg("--disable-guest-agent")
+                .arg("--disable-guest-agent-msi")
+                .arg("--disable-hax")
+                .arg("--disable-hvf")
+                .arg("--disable-iconv")
+                .arg("--disable-jack")
+                .arg("--disable-keyring")
+                .arg("--disable-kvm")
+                .arg("--disable-libdaxctl")
+                .arg("--disable-libiscsi")
+                .arg("--disable-libnfs")
+                .arg("--disable-libpmem")
+                .arg("--disable-libssh")
+                .arg("--disable-libudev")
+                .arg("--disable-libusb")
+                .arg("--disable-linux-aio")
+                .arg("--disable-linux-io-uring")
+                .arg("--disable-linux-user")
+                .arg("--disable-live-block-migration")
+                .arg("--disable-lzfse")
+                .arg("--disable-lzo")
+                .arg("--disable-l2tpv3")
+                .arg("--disable-malloc-trim")
+                .arg("--disable-mpath")
+                .arg("--disable-multiprocess")
+                .arg("--disable-netmap")
+                .arg("--disable-nettle")
+                .arg("--disable-numa")
+                .arg("--disable-nvmm")
+                .arg("--disable-opengl")
+                .arg("--disable-oss")
+                .arg("--disable-pa")
+                .arg("--disable-parallels")
+                .arg("--disable-plugins")
+                .arg("--disable-png")
+                .arg("--disable-pvrdma")
+                .arg("--disable-qcow1")
+                .arg("--disable-qed")
+                .arg("--disable-qga-vss")
+                .arg("--disable-rbd")
+                .arg("--disable-rdma")
+                .arg("--disable-replication")
+                .arg("--disable-sdl")
+                .arg("--disable-sdl-image")
+                .arg("--disable-seccomp")
+                .arg("--disable-selinux")
+                .arg("--disable-slirp-smbd")
+                .arg("--disable-smartcard")
+                .arg("--disable-snappy")
+                .arg("--disable-sparse")
+                .arg("--disable-spice")
+                .arg("--disable-spice-protocol")
+                .arg("--disable-tools")
+                .arg("--disable-tpm")
+                .arg("--disable-usb-redir")
+                .arg("--disable-user")
+                .arg("--disable-u2f")
+                .arg("--disable-vde")
+                .arg("--disable-vdi")
+                .arg("--disable-vduse-blk-export")
+                .arg("--disable-vhost-crypto")
+                .arg("--disable-vhost-kernel")
+                .arg("--disable-vhost-net")
+                .arg("--disable-vhost-user-blk-server")
+                .arg("--disable-vhost-vdpa")
+                .arg("--disable-virglrenderer")
+                .arg("--disable-virtfs")
+                .arg("--disable-virtiofsd")
+                .arg("--disable-vmnet")
+                .arg("--disable-vnc")
+                .arg("--disable-vnc-jpeg")
+                .arg("--disable-vnc-sasl")
+                .arg("--disable-vte")
+                .arg("--disable-vvfat")
+                .arg("--disable-whpx")
+                .arg("--disable-xen")
+                .arg("--disable-xen-pci-passthrough")
+                .arg("--disable-xkbcommon")
+                .arg("--disable-zstd")
+                .status()
+                .expect("Configure failed");
+        }
         if let Ok(j) = jobs {
             Command::new("make")
                 .current_dir(&build_dir)
@@ -233,84 +356,86 @@ pub fn build() {
         }
     }
 
-    #[cfg(feature = "usermode")]
-    Command::new("ld")
-        .current_dir(out_dir_path)
-        .arg("-o")
-        .arg("libqemu-partially-linked.o")
-        .arg("-r")
-        .args(objects)
-        .arg("--start-group")
-        .arg("--whole-archive")
-        .arg(format!("{}/libhwcore.fa", build_dir.display()))
-        .arg(format!("{}/libqom.fa", build_dir.display()))
-        .arg(format!("{}/libevent-loop-base.a", build_dir.display()))
-        .arg("--no-whole-archive")
-        .arg(format!("{}/libqemuutil.a", build_dir.display()))
-        .arg(format!("{}/libhwcore.fa", build_dir.display()))
-        .arg(format!("{}/libqom.fa", build_dir.display()))
-        .arg(format!(
-            "--dynamic-list={}/plugins/qemu-plugins.symbols",
-            qemu_path.display()
-        ))
-        .status()
-        .expect("Partial linked failure");
-
-    #[cfg(not(feature = "usermode"))]
-    Command::new("ld")
-        .current_dir(&out_dir_path)
-        .arg("-o")
-        .arg("libqemu-partially-linked.o")
-        .arg("-r")
-        .args(objects)
-        .arg("--start-group")
-        .arg("--whole-archive")
-        .arg(format!("{}/libhwcore.fa", build_dir.display()))
-        .arg(format!("{}/libqom.fa", build_dir.display()))
-        .arg(format!("{}/libevent-loop-base.a", build_dir.display()))
-        .arg(format!("{}/libio.fa", build_dir.display()))
-        .arg(format!("{}/libcrypto.fa", build_dir.display()))
-        .arg(format!("{}/libauthz.fa", build_dir.display()))
-        .arg(format!("{}/libblockdev.fa", build_dir.display()))
-        .arg(format!("{}/libblock.fa", build_dir.display()))
-        .arg(format!("{}/libchardev.fa", build_dir.display()))
-        .arg(format!("{}/libqmp.fa", build_dir.display()))
-        .arg("--no-whole-archive")
-        .arg(format!("{}/libqemuutil.a", build_dir.display()))
-        .arg(format!(
-            "{}/subprojects/libvhost-user/libvhost-user-glib.a",
-            build_dir.display()
-        ))
-        .arg(format!(
-            "{}/subprojects/libvhost-user/libvhost-user.a",
-            build_dir.display()
-        ))
-        .arg(format!(
-            "{}/subprojects/libvduse/libvduse.a",
-            build_dir.display()
-        ))
-        .arg(format!("{}/libfdt.a", build_dir.display()))
-        .arg(format!("{}/libslirp.a", build_dir.display()))
-        .arg(format!("{}/libmigration.fa", build_dir.display()))
-        .arg(format!("{}/libhwcore.fa", build_dir.display()))
-        .arg(format!("{}/libqom.fa", build_dir.display()))
-        .arg(format!("{}/libio.fa", build_dir.display()))
-        .arg(format!("{}/libcrypto.fa", build_dir.display()))
-        .arg(format!("{}/libauthz.fa", build_dir.display()))
-        .arg(format!("{}/libblockdev.fa", build_dir.display()))
-        .arg(format!("{}/libblock.fa", build_dir.display()))
-        .arg(format!("{}/libchardev.fa", build_dir.display()))
-        .arg(format!("{}/libqmp.fa", build_dir.display()))
-        .arg(format!(
-            "--dynamic-list={}/plugins/qemu-plugins.symbols",
-            qemu_path.display()
-        ))
-        .status()
-        .expect("Partial linked failure");
+    if emulation_mode == "usermode" {
+        Command::new("ld")
+            .current_dir(out_dir_path)
+            .arg("-o")
+            .arg("libqemu-partially-linked.o")
+            .arg("-r")
+            .args(objects)
+            .arg("--start-group")
+            .arg("--whole-archive")
+            .arg(format!("{}/libhwcore.fa", build_dir.display()))
+            .arg(format!("{}/libqom.fa", build_dir.display()))
+            .arg(format!("{}/libevent-loop-base.a", build_dir.display()))
+            .arg("--no-whole-archive")
+            .arg(format!("{}/libqemuutil.a", build_dir.display()))
+            .arg(format!("{}/libhwcore.fa", build_dir.display()))
+            .arg(format!("{}/libqom.fa", build_dir.display()))
+            .arg(format!(
+                "--dynamic-list={}/plugins/qemu-plugins.symbols",
+                qemu_path.display()
+            ))
+            .arg("--end-group")
+            .status()
+            .expect("Partial linked failure");
+    }else{
+        Command::new("ld")
+            .current_dir(out_dir_path)
+            .arg("-o")
+            .arg("libqemu-partially-linked.o")
+            .arg("-r")
+            .args(objects)
+            .arg("--start-group")
+            .arg("--whole-archive")
+            .arg(format!("{}/libhwcore.fa", build_dir.display()))
+            .arg(format!("{}/libqom.fa", build_dir.display()))
+            .arg(format!("{}/libevent-loop-base.a", build_dir.display()))
+            .arg(format!("{}/libio.fa", build_dir.display()))
+            .arg(format!("{}/libcrypto.fa", build_dir.display()))
+            .arg(format!("{}/libauthz.fa", build_dir.display()))
+            .arg(format!("{}/libblockdev.fa", build_dir.display()))
+            .arg(format!("{}/libblock.fa", build_dir.display()))
+            .arg(format!("{}/libchardev.fa", build_dir.display()))
+            .arg(format!("{}/libqmp.fa", build_dir.display()))
+            .arg("--no-whole-archive")
+            .arg(format!("{}/libqemuutil.a", build_dir.display()))
+            .arg(format!(
+                "{}/subprojects/libvhost-user/libvhost-user-glib.a",
+                build_dir.display()
+            ))
+            .arg(format!(
+                "{}/subprojects/libvhost-user/libvhost-user.a",
+                build_dir.display()
+            ))
+            .arg(format!(
+                "{}/subprojects/libvduse/libvduse.a",
+                build_dir.display()
+            ))
+            .arg(format!("{}/libfdt.a", build_dir.display()))
+            .arg(format!("{}/libslirp.a", build_dir.display()))
+            .arg(format!("{}/libmigration.fa", build_dir.display()))
+            .arg(format!("{}/libhwcore.fa", build_dir.display()))
+            .arg(format!("{}/libqom.fa", build_dir.display()))
+            .arg(format!("{}/libio.fa", build_dir.display()))
+            .arg(format!("{}/libcrypto.fa", build_dir.display()))
+            .arg(format!("{}/libauthz.fa", build_dir.display()))
+            .arg(format!("{}/libblockdev.fa", build_dir.display()))
+            .arg(format!("{}/libblock.fa", build_dir.display()))
+            .arg(format!("{}/libchardev.fa", build_dir.display()))
+            .arg(format!("{}/libqmp.fa", build_dir.display()))
+            .arg(format!(
+                "--dynamic-list={}/plugins/qemu-plugins.symbols",
+                qemu_path.display()
+            ))
+            .arg("--end-group")
+            .status()
+            .expect("Partial linked failure");
+    }
 
     Command::new("ar")
         .current_dir(out_dir_path)
-        .arg("crus")
+        .arg("crs")
         .arg("libqemu-partially-linked.a")
         .arg("libqemu-partially-linked.o")
         .status()
@@ -318,26 +443,13 @@ pub fn build() {
 
     println!("cargo:rustc-link-search=native={out_dir}");
     println!("cargo:rustc-link-lib=static=qemu-partially-linked");
+    println!("cargo:rustc-link-lib=rt");
+    println!("cargo:rustc-link-lib=gmodule-2.0");
+    println!("cargo:rustc-link-lib=glib-2.0");
+    println!("cargo:rustc-link-lib=stdc++");
+    println!("cargo:rustc-link-lib=z");
 
-    #[cfg(not(feature = "usermode"))]
-    {
-        println!("cargo:rustc-link-lib=png");
-        println!("cargo:rustc-link-lib=z");
-        println!("cargo:rustc-link-lib=gio-2.0");
-        println!("cargo:rustc-link-lib=gobject-2.0");
-        println!("cargo:rustc-link-lib=ncursesw");
-        println!("cargo:rustc-link-lib=tinfo");
-        println!("cargo:rustc-link-lib=gtk-3");
-        println!("cargo:rustc-link-lib=gdk-3");
-        println!("cargo:rustc-link-lib=pangocairo-1.0");
-        println!("cargo:rustc-link-lib=pango-1.0");
-        println!("cargo:rustc-link-lib=harfbuzz");
-        println!("cargo:rustc-link-lib=atk-1.0");
-        println!("cargo:rustc-link-lib=cairo-gobject");
-        println!("cargo:rustc-link-lib=cairo");
-        println!("cargo:rustc-link-lib=gdk_pixbuf-2.0");
-        println!("cargo:rustc-link-lib=X11");
-        println!("cargo:rustc-link-lib=epoxy");
+    if emulation_mode == "systemmode" {
         println!("cargo:rustc-link-lib=pixman-1");
 
         fs::create_dir_all(target_dir.join("pc-bios")).unwrap();
@@ -351,13 +463,6 @@ pub fn build() {
             }
         }
     }
-
-    println!("cargo:rustc-link-lib=rt");
-    println!("cargo:rustc-link-lib=gmodule-2.0");
-    println!("cargo:rustc-link-lib=glib-2.0");
-    println!("cargo:rustc-link-lib=stdc++");
-
-    println!("cargo:rustc-link-lib=z");
 
     /* #[cfg(not(feature = "python"))]
     {
@@ -376,8 +481,7 @@ pub fn build() {
         println!("cargo:rustc-env=LD_LIBRARY_PATH={}", target_dir.display());
     } */
 
-    #[cfg(feature = "usermode")]
-    {
+    if emulation_mode == "usermode" {
         let qasan_dir = Path::new("libqasan");
         let qasan_dir = fs::canonicalize(qasan_dir).unwrap();
 
