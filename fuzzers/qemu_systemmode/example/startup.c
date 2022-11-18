85b1d4d2ad70f8acdcce11f16efbed414acbf355
Alwin Berger <alwin.berger@tu-dortmund.de>
2022-11-18 14:06:53 +0100

    fuzzers/qemu_systemmode: simplify example

--- fuzzers/qemu_systemmode/example/startup.c
+++ fuzzers/qemu_systemmode/example/startup.c
@@ -40,10 +40,11 @@ void Reset_Handler( void )
     __asm volatile ( "mov sp, r0" );
 
     /* copy .data section from flash to RAM */
-    for( uint32_t * src = &_sidata, * dest = &_sdata; dest < &_edata; )
-    {
-        *dest++ = *src++;
-    }
+    // Not needed for this example, see linker script
+    // for( uint32_t * src = &_sidata, * dest = &_sdata; dest < &_edata; )
+    // {
+    //     *dest++ = *src++;
+    // }
 
     /* zero out .bss section */
     for( uint32_t * dest = &_sbss; dest < &_ebss; )
