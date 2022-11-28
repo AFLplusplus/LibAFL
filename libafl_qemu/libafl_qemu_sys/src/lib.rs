aa7121e3450dcc3a890a4c82c60b2003f033f435
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-11-28 10:14:50 +0100

    allow

--- libafl_qemu/libafl_qemu_sys/src/lib.rs
+++ libafl_qemu/libafl_qemu_sys/src/lib.rs
@@ -2,5 +2,7 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(improper_ctypes)]
+#![allow(unused_mut)]
+#![allow(clippy::all)]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
