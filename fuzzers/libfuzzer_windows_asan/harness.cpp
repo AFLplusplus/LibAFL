50c12d6e49c174d517fbe452a5afd6817ddb63a3
Max Ammann <maximilian.ammann@trailofbits.com>
2022-12-09 14:21:36 +0100

    Fix cpp format

--- fuzzers/libfuzzer_windows_asan/harness.cpp
+++ fuzzers/libfuzzer_windows_asan/harness.cpp
@@ -3,14 +3,14 @@
 #include <string>
 
 void asan_crash() {
-     int* array = new int[100];
-     delete[] array;
-     array[5] += 1; 
-     fprintf(stdout, "%d\n", array[5]);
+  int *array = new int[100];
+  delete[] array;
+  array[5] += 1;
+  fprintf(stdout, "%d\n", array[5]);
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-  //abort();
+  // abort();
   asan_crash();
   return 0;
 }
