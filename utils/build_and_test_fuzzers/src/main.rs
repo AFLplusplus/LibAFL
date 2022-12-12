724da0b13e2579479c12cecd52600989f49f225d
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-12-12 17:35:49 +0100

    diffing fuzzers ci only

--- utils/build_and_test_fuzzers/src/main.rs
+++ utils/build_and_test_fuzzers/src/main.rs
@@ -7,5 +7,9 @@ fn main() {
     let all_crates = find_all_crates();
     extend_diffing_crates_with_deps(&mut diff_crates, &all_crates);
 
-    println!("{:?}", diff_crates);
+    for file in diff_crates {
+        if file.starts_with("./fuzzers") || file.starts_with("fuzzers") {
+            print!("{} ", file.parent().unwrap().display());
+        }
+    }
 }
