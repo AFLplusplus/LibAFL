bd35f88d03c30315418f89f7700fa4e9257ebd90
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-12-13 21:18:48 +0100

    better main.rs

--- utils/build_and_test_fuzzers/src/main.rs
+++ utils/build_and_test_fuzzers/src/main.rs
@@ -16,9 +16,12 @@ fn main() {
     let all_crates = find_all_crates();
     extend_diffing_crates_with_deps(&mut diff_crates, &all_crates);
 
+    let mut fuzzers = vec![];
     for file in diff_crates {
         if file.starts_with("./fuzzers") || file.starts_with("fuzzers") {
-            print!("{} ", file.parent().unwrap().display());
+            fuzzers.push(file.parent().unwrap().display().to_string());
         }
     }
+
+    println!("{}", fuzzers.join("\n"));
 }
