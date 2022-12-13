162de0ceaf6147fe166b9a0dddac5820056ac61b
Andrea Fioraldi <andreafioraldi@gmail.com>
2022-12-13 21:19:44 +0100

    Fix CI diffing (#944)
    * fix build_and_test_fuzzers to diff from origin/main
    
    * fix fuzzbench_forkserver

--- utils/build_and_test_fuzzers/src/main.rs
+++ utils/build_and_test_fuzzers/src/main.rs
@@ -5,16 +5,23 @@ pub use diffing::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let commit = if args.len() > 1 { &args[1] } else { "HEAD^" };
+    let commit = if args.len() > 1 {
+        &args[1]
+    } else {
+        "origin/main"
+    };
 
     let files = get_diffing_files(commit);
     let mut diff_crates = get_diffing_crates(&files);
     let all_crates = find_all_crates();
     extend_diffing_crates_with_deps(&mut diff_crates, &all_crates);
 
+    let mut fuzzers = vec![];
     for file in diff_crates {
         if file.starts_with("./fuzzers") || file.starts_with("fuzzers") {
-            print!("{} ", file.parent().unwrap().display());
+            fuzzers.push(file.parent().unwrap().display().to_string());
         }
     }
+
+    println!("{}", fuzzers.join("\n"));
 }
