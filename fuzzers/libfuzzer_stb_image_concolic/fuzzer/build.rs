a6c789785a57b48b29e069c19334c221378710a5
tokatoka <tokazerkje@outlook.com>
2022-12-20 21:50:02 +0900

    a

--- fuzzers/libfuzzer_stb_image_concolic/fuzzer/build.rs
+++ fuzzers/libfuzzer_stb_image_concolic/fuzzer/build.rs
@@ -68,7 +68,7 @@ fn main() {
     )
     .unwrap();
 
-    println!("?: {}", runtime_dir.join("target").join("release").join("libSymRuntime.so").exists())
+    println!("?: {}", runtime_dir.join("target").join("release").join("libSymRuntime.so").exists());
 
     if !runtime_dir.join("libSymRuntime.so").exists() {
         println!("cargo:warning=Runtime not found. Build it first.");
