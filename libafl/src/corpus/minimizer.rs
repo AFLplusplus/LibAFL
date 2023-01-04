6293270f9e601edb41245b9754aab317f4d87542
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-01-04 11:32:13 +0100

    cmin

--- libafl/src/corpus/minimizer.rs
+++ libafl/src/corpus/minimizer.rs
@@ -111,7 +111,7 @@ where
         let mut seed_exprs = HashMap::new();
         let mut cov_map = HashMap::new();
 
-        for idx in 0..state.corpus().count() {
+        for idx in state.corpus().ids() {
             let (weight, input) = {
                 let mut testcase = state.corpus().get(idx)?.borrow_mut();
                 let weight = TS::compute(&mut *testcase, state)?
