769b17338255c80142dc42c87705bef735a2a696
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-01-16 16:51:07 +0100

    upd

--- libafl_qemu/libafl_qemu_build/src/build.rs
+++ libafl_qemu/libafl_qemu_build/src/build.rs
@@ -8,7 +8,7 @@ use which::which;
 
 const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-const QEMU_REVISION: &str = "241ec5977a41151093d6d47738d1a7f291b9e91f";
+const QEMU_REVISION: &str = "f49d184a90e766d4d4f409160acb6b6c645156cf";
 
 fn build_dep_check(tools: &[&str]) {
     for tool in tools {
