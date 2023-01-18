a30c04d252a9a0d9bfe977ded93647ebde1d09da
Addison Crump <addison.crump@cispa.de>
2023-01-18 12:02:46 +0100

    use from_name again

--- libafl_libfuzzer/libafl_libfuzzer_runtime/src/fuzz.rs
+++ libafl_libfuzzer/libafl_libfuzzer_runtime/src/fuzz.rs
@@ -50,7 +50,7 @@ pub fn fuzz(
 
         match Launcher::builder()
             .shmem_provider(shmem_provider)
-            .configuration(EventConfig::AlwaysUnique)
+            .configuration(EventConfig::from_name(options.fuzzer_name()))
             .monitor(monitor)
             .run_client(&mut run_client)
             .cores(&cores)
