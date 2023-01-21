ec8f41a9cee4cf97acff4e396854f9ca7a6bee2e
tokatoka <tokazerkje@outlook.com>
2023-01-21 20:47:17 +0900

    remove

--- libafl_tinyinst/src/executor.rs
+++ libafl_tinyinst/src/executor.rs
@@ -236,8 +236,8 @@ where
                 // setup shared memory
                 let mut shmem = provider.new_shmem(MAX_FILE + SHMEM_FUZZ_HDR_SIZE)?;
                 let shmem_id = shmem.id();
-                println!("{:#?}", shmem.id());
-                shmem.write_to_env("__TINY_SHM_FUZZ_ID")?;
+                // println!("{:#?}", shmem.id());
+                // shmem.write_to_env("__TINY_SHM_FUZZ_ID")?;
 
                 let size_in_bytes = (MAX_FILE + SHMEM_FUZZ_HDR_SIZE).to_ne_bytes();
                 shmem.as_mut_slice()[..4].clone_from_slice(&size_in_bytes[..4]);
