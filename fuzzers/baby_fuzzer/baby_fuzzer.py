4ad5150a065567b8598c300c251dab2696f624f2
Dominik Maier <dmnk@google.com>
2023-01-26 00:48:38 +0000

    Fixing python example

--- fuzzers/baby_fuzzer/baby_fuzzer.py
+++ fuzzers/baby_fuzzer/baby_fuzzer.py
@@ -4,29 +4,27 @@ from pylibafl import libafl
 
 def map_observer_wrapper(map_observer):
     if type(map_observer).__name__ == "OwnedMapObserverI32":
-        return libafl.MapObserverI32.new_from_owned(map_observer)
+        return libafl.MapObserverI32.new_owned(map_observer)
 
 def executor_wrapper(executor):
     if type(executor).__name__ == "OwnedInProcessExecutorI32":
         return libafl.ExecutorI32.new_from_inprocess(executor)
 
 def monitor_wrapper(monitor):
-    if type(monitor).__name__ == "SimpleMonitor":
-        return libafl.Monitor.new_from_simple(monitor)
+    return monitor.as_monitor()
 
 def event_manager_wrapper(event_manager):
-    if type(event_manager).__name__ == "SimpleEventManager":
-        return libafl.EventManagerI32.new_from_simple(event_manager)
+    return event_manager.as_manager()
 
 def corpus_wrapper(corpus):
     if type(corpus).__name__ == "InMemoryCorpus":
-        return libafl.Corpus.new_from_in_memory(corpus)
+        return libafl.Corpus.new_in_memory(corpus)
     if type(corpus).__name__ == "OnDiskCorpus":
-        return libafl.Corpus.new_from_on_disk(corpus)
+        return libafl.Corpus.new_on_disk(corpus)
 
 def rand_wrapper(rand):
     if type(rand).__name__ == "StdRand":
-        return libafl.Rand.new_from_std(rand)
+        return libafl.Rand.new_std(rand)
 
 def stage_wrapper(stage):
     if type(stage).__name__ == "StdScheduledHavocMutationsStageI32":
@@ -40,24 +38,24 @@ def harness(inp):
 
 map_observer = libafl.OwnedMapObserverI32("signals", [0] * 16)
 
-feedback_state = libafl.MapFeedbackStateI32.with_observer(map_observer_wrapper(map_observer))
+feedback = libafl.MaxMapFeedbackI32(map_observer_wrapper(map_observer))
+objective = libafl.CrashFeedback()
 
-feedback = libafl.MaxMapFeedbackI32(feedback_state, map_observer_wrapper(map_observer))
-
-state = libafl.StdStateI32(
+state = libafl.StdState(
     rand_wrapper(libafl.StdRand.with_current_nanos()), 
     corpus_wrapper(libafl.InMemoryCorpus()), 
     corpus_wrapper(libafl.OnDiskCorpus("./crashes")), 
-    feedback_state
+    feedback.as_feedback(),
+    objective.as_feedback(),
 )
 
-monitor = libafl.SimpleMonitor()
+monitor = libafl.SimpleMonitor(lambda x: print(x))
 
 mgr = libafl.SimpleEventManager(monitor_wrapper(monitor))
 
-fuzzer = libafl.StdFuzzerI32(feedback)
+fuzzer = libafl.StdFuzzer(feedback.as_feedback(), objective.as_feedback())
 
-executor = libafl.OwnedInProcessExecutorI32(harness, map_observer_wrapper(map_observer), fuzzer, state, event_manager_wrapper(mgr))
+executor = libafl.InProcessExecutor(harness, map_observer_wrapper(map_observer), fuzzer, state, event_manager_wrapper(mgr))
 
 generator = libafl.RandPrintablesGeneratorI32(32)
 
