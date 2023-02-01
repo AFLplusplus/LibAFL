677b0f19e8a02d52db2bfcf6e7ed208510dfe450
tokatoka <tokazerkje@outlook.com>
2023-02-01 21:42:11 +0900

    type_replace

---
+++ libafl/src/stages/colorization.rs
@@ -0,0 +1,210 @@
+//! The colorization stage from colorization() in afl++
+
+use alloc::{
+    string::{String, ToString},
+    vec::Vec,
+};
+use core::{fmt::Debug, marker::PhantomData, ops::Range};
+
+use crate::{
+    bolts::{rands::Rand, tuples::MatchName},
+    corpus::{Corpus, CorpusId},
+    executors::{Executor, HasObservers},
+    fuzzer::Evaluator,
+    inputs::HasBytesVec,
+    observers::MapObserver,
+    stages::Stage,
+    state::{HasClientPerfMonitor, HasCorpus, HasMetadata, HasRand, UsesState},
+    Error,
+};
+
+/// The mutational stage using power schedules
+#[derive(Clone, Debug)]
+pub struct ColorizationStage<E, EM, O, Z> {
+    map_observer_name: String,
+    #[allow(clippy::type_complexity)]
+    phantom: PhantomData<(E, EM, O, Z)>,
+}
+
+impl<E, EM, O, Z> UsesState for ColorizationStage<E, EM, O, Z>
+where
+    E: UsesState,
+{
+    type State = E::State;
+}
+
+impl<E, EM, O, Z> Stage<E, EM, Z> for ColorizationStage<E, EM, O, Z>
+where
+    E: Executor<EM, Z> + HasObservers,
+    EM: UsesState<State = E::State>,
+    O: MapObserver,
+    E::State: HasClientPerfMonitor + HasCorpus + HasMetadata + HasRand,
+    Z: Evaluator<E, EM, State = E::State>,
+    E::Input: HasBytesVec,
+{
+    #[inline]
+    #[allow(clippy::let_and_return)]
+    fn perform(
+        &mut self,
+        fuzzer: &mut Z,
+        executor: &mut E,
+        state: &mut E::State,
+        manager: &mut EM,
+        corpus_idx: CorpusId,
+    ) -> Result<(), Error> {
+        let orig = state
+            .corpus()
+            .get(corpus_idx)?
+            .borrow_mut()
+            .load_input()
+            .unwrap()
+            .clone();
+        let mut changed = orig.clone();
+
+        // First, run orig_input once and get the original hash
+        let (_, corpus_idx) = fuzzer.evaluate_input(state, executor, manager, orig)?;
+
+        let observer = executor
+            .observers()
+            .match_name::<O>(&self.map_observer_name)
+            .ok_or_else(|| Error::key_not_found("MapObserver not found".to_string()))?;
+
+        let mut orig_hash = observer.hash() as usize;
+
+        let changed_bytes = changed.bytes_mut();
+        let input_len = changed_bytes.len();
+        let ranges: Vec<Range<usize>> = vec![0..input_len - 1];
+
+        // Now replace with random values (This is type_replace)
+        self.type_replace(changed_bytes, state);
+
+        // while ((rng = pop_biggest_range(&ranges)) != NULL &&
+        // afl->stage_cur < afl->stage_max) {
+        for idx in 0..input_len * 2 {}
+
+        Ok(())
+    }
+}
+
+impl<E, EM, O, Z> ColorizationStage<E, EM, O, Z>
+where
+    E: Executor<EM, Z> + HasObservers,
+    EM: UsesState<State = E::State>,
+    O: MapObserver,
+    E::State: HasClientPerfMonitor + HasCorpus + HasMetadata + HasRand,
+    Z: Evaluator<E, EM, State = E::State>,
+{
+    /// Creates a new [`PowerMutationalStage`]
+    pub fn new(map_observer_name: &O) -> Self {
+        Self::transforming(map_observer_name)
+    }
+
+    pub fn type_replace(&self, bytes: &mut [u8], state: &mut E::State) {
+        let len = bytes.len();
+        for idx in 0..len {
+            let c = match bytes[idx] {
+                0x41..=0x46 => {
+                    // 'A' + 1 + rand('F' - 'A')
+                    0x41 + 1 + state.rand_mut().below(5) as u8
+                }
+                0x61..=0x66 => {
+                    // 'a' + 1 + rand('f' - 'a')
+                    0x61 + 1 + state.rand_mut().below(5) as u8
+                }
+                0x30 => {
+                    // '0' -> '1'
+                    0x31
+                }
+                0x31 => {
+                    // '1' -> '0'
+                    0x30
+                }
+                0x32..=0x39 => {
+                    // '2' + 1 + rand('9' - '2')
+                    0x32 + 1 + state.rand_mut().below(7) as u8
+                }
+                0x47..=0x5a => {
+                    // 'G' + 1 + rand('Z' - 'G')
+                    0x47 + 1 + state.rand_mut().below(19) as u8
+                }
+                0x67..=0x7a => {
+                    // 'g' + 1 + rand('z' - 'g')
+                    0x67 + 1 + state.rand_mut().below(19) as u8
+                }
+                0x21..=0x2a => {
+                    // '!' + 1 + rand('*' - '!');
+                    0x21 + 1 + state.rand_mut().below(9) as u8
+                }
+                0x2c..=0x2e => {
+                    // ',' + 1 + rand('.' - ',')
+                    0x2c + 1 + state.rand_mut().below(2) as u8
+                }
+                0x3a..=0x40 => {
+                    // ':' + 1 + rand('@' - ':')
+                    0x3a + 1 + state.rand_mut().below(6) as u8
+                }
+                0x5b..=0x60 => {
+                    // '[' + 1 + rand('`' - '[')
+                    0x5b + 1 + state.rand_mut().below(5) as u8
+                }
+                0x7b..=0x7e => {
+                    // '{' + 1 + rand('~' - '{')
+                    0x7b + 1 + state.rand_mut().below(3) as u8
+                }
+                0x2b => {
+                    // '+' -> '/'
+                    0x2f
+                }
+                0x2f => {
+                    // '/' -> '+'
+                    0x2b
+                }
+                0x20 => {
+                    // ' ' -> '\t'
+                    0x9
+                }
+                0x9 => {
+                    // '\t' -> ' '
+                    0x20
+                }
+                0xd => {
+                    // '\r' -> '\n'
+                    0xa
+                }
+                0xa => {
+                    // '\n' -> '\r'
+                    0xd
+                }
+                0x0 => 0x1,
+                0x1 => 0x0,
+                0xff => 0x0,
+                _ => {
+                    if bytes[idx] < 32 {
+                        bytes[idx] ^ 0x1f
+                    } else {
+                        bytes[idx] ^ 0x7f
+                    }
+                }
+            };
+
+            bytes[idx] = c;
+        }
+    }
+}
+
+impl<E, EM, O, Z> ColorizationStage<E, EM, O, Z>
+where
+    E: Executor<EM, Z> + HasObservers,
+    EM: UsesState<State = E::State>,
+    O: MapObserver,
+    E::State: HasClientPerfMonitor + HasCorpus + HasMetadata + HasRand,
+    Z: Evaluator<E, EM, State = E::State>,
+{
+    /// Creates a new transforming [`PowerMutationalStage`]
+    pub fn transforming(map_observer_name: &O) -> Self {
+        Self {
+            map_observer_name: map_observer_name.name().to_string(),
+            phantom: PhantomData,
+        }
+    }
+}
