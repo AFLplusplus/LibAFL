310838a54b571757c00398ca1e6c30f99f7f5e0f
tokatoka <tokazerkje@outlook.com>
2023-02-02 13:14:32 +0900

    heap

--- libafl/src/stages/colorization.rs
+++ libafl/src/stages/colorization.rs
@@ -1,10 +1,43 @@
 //! The colorization stage from colorization() in afl++
 
 use alloc::{
+    collections::binary_heap::BinaryHeap,
     string::{String, ToString},
     vec::Vec,
 };
-use core::{fmt::Debug, marker::PhantomData, ops::Range};
+use core::{cmp::Ordering, fmt::Debug, marker::PhantomData, ops::Range};
+
+// Bigger range is better
+#[derive(Debug, PartialEq, Eq)]
+struct Bigger(Range<usize>);
+
+impl PartialOrd for Bigger {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        self.0.len().partial_cmp(&other.0.len())
+    }
+}
+
+impl Ord for Bigger {
+    fn cmp(&self, other: &Bigger) -> Ordering {
+        self.0.len().cmp(&other.0.len())
+    }
+}
+
+// Earlier range is better
+#[derive(Debug, PartialEq, Eq)]
+struct Earlier(Range<usize>);
+
+impl PartialOrd for Earlier {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        other.0.start.partial_cmp(&self.0.start)
+    }
+}
+
+impl Ord for Earlier {
+    fn cmp(&self, other: &Self) -> Ordering {
+        other.0.start.cmp(&self.0.start)
+    }
+}
 
 use crate::{
     bolts::{rands::Rand, tuples::MatchName},
@@ -77,7 +110,16 @@ where
 
         let changed_bytes = changed.bytes_mut();
         let input_len = changed_bytes.len();
-        let mut ranges: Vec<Range<usize>> = vec![0..input_len - 1];
+
+        // Binary heap, pop is logN, insert is logN
+        // We will separate this range into smaller ranges.
+        // Keep it sorted, we want biggest ones to come first
+        let mut ranges = BinaryHeap::new();
+        ranges.push(Bigger(0..input_len));
+
+        // This heap contains the smaller ranges. Changes inside them does not affect the coverage.
+        // Keep it sorted, we want the earliest ones to come first
+        let mut ok_ranges = BinaryHeap::new();
 
         // Now replace with random values (This is type_replace)
         self.type_replace(changed_bytes, state);
@@ -90,8 +132,9 @@ where
         // What we do is now to separate the input into smaller regions
         // And in each small regions make sure changing those bytes in the regions does not affect the coverage
         for _ in 0..input_len * 2 {
-            if let Some(r) = self.pop_biggest_range(&mut ranges) {
+            if let Some(b) = ranges.pop() {
                 // Separate the ranges
+                let r = b.0;
                 let range_start = r.start;
                 let range_end = r.end;
                 let copy_len = r.len();
@@ -128,17 +171,38 @@ where
 
                     // Add smaller range
                     if copy_len > 1 {
-                        ranges.push(range_start..(range_start - 1 + copy_len / 2));
-                        ranges.push((range_start + copy_len / 2)..range_end);
+                        ranges.push(Bigger(range_start..(range_start - 1 + copy_len / 2)));
+                        ranges.push(Bigger((range_start + copy_len / 2)..range_end));
                     }
+                } else {
+                    // The change in this range is safe!
+                    ok_ranges.push(Earlier(range_start..range_end));
                 }
             } else {
                 break;
             }
         }
 
-        // Now ranges is a list of smaller range
+        // Now ok_ranges is a list of smaller range
         // Each of them should be stored into a metadata and we'll use them later in afl++ redqueen
+
+        // let's consolidate ranges in ok_ranges
+        let mut res: Vec<Range<usize>> = Vec::new();
+        for item in ok_ranges.into_sorted_vec() {
+            match res.last_mut() {
+                Some(l) => {
+                    // Try consolidate
+                    if l.end == item.0.start {
+                        l.end = item.0.start;
+                    } else {
+                        res.push(item.0)
+                    }
+                }
+                None => {
+                    res.push(item.0);
+                }
+            }
+        }
 
         Ok(())
     }
@@ -155,25 +219,6 @@ where
     /// Creates a new [`PowerMutationalStage`]
     pub fn new(map_observer_name: &O) -> Self {
         Self::transforming(map_observer_name)
-    }
-
-    /// Return the biggest range
-    pub fn pop_biggest_range(&self, ranges: &mut Vec<Range<usize>>) -> Option<Range<usize>> {
-        if ranges.is_empty() {
-            return None;
-        } else {
-            let mut max_len = 0;
-            let mut ret_idx = 0;
-            for idx in 0..ranges.len() {
-                let len = ranges[idx].len();
-                if len > max_len {
-                    max_len = len;
-                    ret_idx = idx;
-                }
-            }
-
-            Some(ranges.remove(ret_idx))
-        }
     }
 
     /// Replace bytes with random values but following certain rules
