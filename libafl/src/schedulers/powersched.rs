311bd4e4c90214a2bd697bc56e12a5376a02af13
Addison Crump <addison.crump@cispa.de>
2023-02-14 16:30:20 +0100

    additional powersched differences

--- libafl/src/schedulers/powersched.rs
+++ libafl/src/schedulers/powersched.rs
@@ -226,8 +226,6 @@ where
 
         // Use these to adjust `SchedulerMetadata`
         let (prev_total_time, prev_cycles) = prev_meta.cycle_and_time();
-        let prev_bitmap_size = prev_meta.bitmap_size();
-        let prev_bitmap_size_log = libm::log2(prev_bitmap_size as f64);
 
         let psmeta = state
             .metadata_mut()
@@ -237,15 +235,51 @@ where
         // We won't add new one because it'll get added when it gets executed in calirbation next time.
         psmeta.set_exec_time(psmeta.exec_time() - prev_total_time);
         psmeta.set_cycles(psmeta.cycles() - (prev_cycles as u64));
-        psmeta.set_bitmap_size(psmeta.bitmap_size() - prev_bitmap_size);
-        psmeta.set_bitmap_size_log(psmeta.bitmap_size_log() - libm::log2(prev_bitmap_size_log));
-        psmeta.set_bitmap_entries(psmeta.bitmap_entries() - 1);
+        // we cannot make determinations about the previous bitmap size, since we do not have the
+        // ability to recompute this from the full corpus without significant slowdowns
+        // for now, we assume that any replace/removal *does not* affect the filled bitmaps
 
         state
             .corpus()
             .get(idx)?
             .borrow_mut()
             .add_metadata(SchedulerTestcaseMetaData::new(prev_depth));
+        Ok(())
+    }
+
+    fn on_remove(
+        &self,
+        state: &mut Self::State,
+        _idx: CorpusId,
+        prev: &Option<Testcase<<Self::State as UsesInput>::Input>>,
+    ) -> Result<(), Error> {
+        let prev = prev.as_ref().ok_or_else(|| {
+            Error::illegal_argument(
+                "Power schedulers must be aware of the removed corpus entry for reweighting.",
+            )
+        })?;
+
+        let prev_meta = prev
+            .metadata()
+            .get::<SchedulerTestcaseMetaData>()
+            .ok_or_else(|| {
+                Error::key_not_found("SchedulerTestcaseMetaData not found".to_string())
+            })?;
+
+        // Use these to adjust `SchedulerMetadata`
+        let (prev_total_time, prev_cycles) = prev_meta.cycle_and_time();
+
+        let psmeta = state
+            .metadata_mut()
+            .get_mut::<SchedulerMetadata>()
+            .ok_or_else(|| Error::key_not_found("SchedulerMetadata not found".to_string()))?;
+
+        psmeta.set_exec_time(psmeta.exec_time() - prev_total_time);
+        psmeta.set_cycles(psmeta.cycles() - (prev_cycles as u64));
+        // we cannot make determinations about the previous bitmap size, since we do not have the
+        // ability to recompute this from the full corpus without significant slowdowns
+        // for now, we assume that any replace/removal *does not* affect the filled bitmaps
+
         Ok(())
     }
 
