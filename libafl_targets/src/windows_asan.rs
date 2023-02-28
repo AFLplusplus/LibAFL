381977cde1daecb9ec8c1491daffe2cf32d1eb34
Dominik Maier <dmnk@google.com>
2023-02-28 15:01:28 +0000

    win:

--- libafl_targets/src/windows_asan.rs
+++ libafl_targets/src/windows_asan.rs
@@ -4,7 +4,7 @@ use libafl::{
     events::{EventFirer, EventRestarter},
     executors::{inprocess::windows_asan_handler::asan_death_handler, Executor, HasObservers},
     feedbacks::Feedback,
-    state::{HasClientPerfMonitor, HasSolutions},
+    state::{HasClientPerfMonitor, HasFuzzedCorpusId, HasSolutions},
     HasObjective,
 };
 
@@ -16,19 +16,19 @@ extern "C" {
 }
 
 /// # Safety
-/// Setup asan callback on windows
+/// Setup ASan callback on windows
 // See https://github.com/AFLplusplus/LibAFL/issues/769
-// This is needed to intercept asan error exit
-// When we use AddressSanitizer on windows, the crash handler is not called when ASAN detects an error
-// This is because, on linux, ASAN runtime raises SIGABRT so we can rely on the signal handler
+// This is needed to intercept ASan error exit
+// When we use AddressSanitizer on windows, the crash handler is not called when ASan detects an error
+// This is because, on linux, ASan runtime raises SIGABRT so we can rely on the signal handler
 // but on windows it simply calls TerminateProcess.
-// so we need to call the api by asan to register the callback when asan is about to finish the process.
+// so we need to call the api by ASan to register the callback when ASan is about to finish the process.
 pub unsafe fn setup_asan_callback<E, EM, OF, Z>(_executor: &E, _event_mgr: &EM, _fuzzer: &Z)
 where
     E: Executor<EM, Z> + HasObservers,
     EM: EventFirer<State = E::State> + EventRestarter<State = E::State>,
     OF: Feedback<E::State>,
-    E::State: HasSolutions + HasClientPerfMonitor,
+    E::State: HasSolutions + HasClientPerfMonitor + HasFuzzedCorpusId,
     Z: HasObjective<Objective = OF, State = E::State>,
 {
     __sanitizer_set_death_callback(asan_death_handler::<E, EM, OF, Z>);
