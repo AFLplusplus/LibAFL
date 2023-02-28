d7a850fd7d0f6f25e219a563ce1369419d8cdc83
tokatoka <tokazerkje@outlook.com>
2023-02-28 19:13:38 +0900

    remove

--- libafl/src/bolts/mod.rs
+++ libafl/src/bolts/mod.rs
@@ -250,12 +250,6 @@ impl SimpleLogger {
     }
 }
 
-/// Default stderr logger
-pub static LIBAFL_STDERR_LOGGER: SimpleLogger = SimpleLogger::stderr();
-
-/// Default stdout logger
-pub static LIBAFL_STDOUT_LOGGER: SimpleLogger = SimpleLogger::stdout();
-
 #[cfg(feature = "std")]
 impl log::Log for SimpleLogger {
     #[inline]
