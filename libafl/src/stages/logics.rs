6ef205d684da555d18710ad9aaaa824e41842073
tokatoka <tokazerkje@outlook.com>
2023-03-15 18:50:49 +0900

    while logic

--- libafl/src/stages/logics.rs
+++ libafl/src/stages/logics.rs
@@ -9,6 +9,74 @@ use crate::{
     Error,
 };
 
+#[derive(Debug)]
+/// Perform the stage while closure evaluates to true
+pub struct WhileStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    closure: CB,
+    stages: ST,
+    phantom: PhantomData<(E, EM, Z)>,
+}
+
+impl<CB, E, EM, ST, Z> UsesState for WhileStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    type State = E::State;
+}
+
+impl<CB, E, EM, ST, Z> Stage<E, EM, Z> for WhileStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    fn perform(
+        &mut self,
+        fuzzer: &mut Z,
+        executor: &mut E,
+        state: &mut E::State,
+        manager: &mut EM,
+        corpus_idx: CorpusId,
+    ) -> Result<(), Error> {
+        while (self.closure)(fuzzer, executor, state, manager, corpus_idx)? {
+            self.stages
+                .perform_all(fuzzer, executor, state, manager, corpus_idx)?;
+        }
+        Ok(())
+    }
+}
+
+impl<CB, E, EM, ST, Z> WhileStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    /// Constructor
+    pub fn new(closure: CB, stages: ST) -> Self {
+        Self {
+            closure,
+            stages,
+            phantom: PhantomData,
+        }
+    }
+}
+
 /// Perform the stage if closure evaluates to true
 #[derive(Debug)]
 pub struct IfElseStage<CB, E, EM, ST, Z>
