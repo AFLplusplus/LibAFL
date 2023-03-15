d7e9f82f50d2ae770aede14112a92b15ce5a4774
SpaceWhite <space_white@yahoo.com>
2023-03-16 02:24:51 +0900

    fix frida

--- fuzzers/frida_executable_libpng/src/lib.rs
+++ fuzzers/frida_executable_libpng/src/lib.rs
@@ -1,25 +1,20 @@
-use std::{process::exit, mem::transmute, ptr};
+use std::{mem::transmute, process::exit, ptr};
 
-use libc::{dlsym, RTLD_NEXT, c_void};
+use libc::{c_void, dlsym, RTLD_NEXT};
 
 mod fuzzer;
 
 type LibcStartMainFunc = fn(
-    //extern "C" fn(i32, *const *const u8, *const *const u8) -> i32,
-    *mut c_void,
+    unsafe extern "C" fn(i32, *const *const u8, *const *const u8) -> i32,
     i32,
     *const *const char,
-    extern "C" fn (i32, *const *const u8, *const *const u8) -> i32,
+    extern "C" fn(i32, *const *const u8, *const *const u8) -> i32,
     extern "C" fn(),
-    extern "C" fn (),
+    extern "C" fn(),
     *mut c_void,
 ) -> i32;
 
-type MainFunc = extern "C" fn(
-    i32,
-    *const *const u8,
-    *const *const u8,
-) -> i32;
+type MainFunc = extern "C" fn(i32, *const *const u8, *const *const u8) -> i32;
 
 extern "C" fn _dummy_main(_argc: i32, _argv: *const *const u8, _env: *const *const u8) -> i32 {
     0
@@ -28,45 +23,36 @@ extern "C" fn _dummy_main(_argc: i32, _argv: *const *const u8, _env: *const *con
 static mut ORIG_MAIN: MainFunc = _dummy_main;
 
 #[no_mangle]
+pub unsafe extern "C" fn main_hook(
+    _argc: i32,
+    _argv: *const *const u8,
+    _env: *const *const u8,
+) -> i32 {
+    fuzzer::lib(ORIG_MAIN);
+    0
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn __libc_start_main(
     main: extern "C" fn(i32, *const *const u8, *const *const u8) -> i32,
     argc: i32,
     argv: *const *const char,
-    init: extern "C" fn (i32, *const *const u8, *const *const u8) -> i32,
+    init: extern "C" fn(i32, *const *const u8, *const *const u8) -> i32,
     fini: extern "C" fn(),
-    rtld_fini: extern "C" fn (),
+    rtld_fini: extern "C" fn(),
     stack_end: *mut c_void,
-) -> u8 {
+) -> i32 {
     unsafe {
         ORIG_MAIN = main;
 
-        let orig_libc_start_main_addr: *mut c_void = dlsym(RTLD_NEXT, "__libc_start_main\0".as_ptr() as *const i8);
+        let orig_libc_start_main_addr: *mut c_void =
+            dlsym(RTLD_NEXT, "__libc_start_main\0".as_ptr() as *const i8);
 
         let orig_libc_start_main: LibcStartMainFunc = transmute(orig_libc_start_main_addr);
 
-        let mut main_hook = |_argc: i32, _argv: *const *const u8, _environment: *const *const u8| -> i32 {
-            return main(_argc, _argv, _environment);
-        };
+        let exit_code =
+            orig_libc_start_main(main_hook, argc, argv, init, fini, rtld_fini, stack_end);
 
-        let main_hook_addr_temp: *mut c_void = ptr::read(&mut main_hook as *mut _ as *const *mut c_void);
-        let main_hook_addr: *mut c_void = ptr::read(main_hook_addr_temp as *const *mut c_void);
-
-        let exit_code = orig_libc_start_main(
-            main_hook_addr,
-            argc,
-            argv,
-            init,
-            fini,
-            rtld_fini,
-            stack_end
-        );
-        
-        return exit_code as u8;
-
-        /*let exit_code = orig_libc_start_main(
-            &mut main_hook as *mut _ as *mut c_void,
-            argc,
-            argv,
-        );*/
+        return exit_code;
     }
 }
