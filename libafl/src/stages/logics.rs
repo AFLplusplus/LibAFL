e25fdbbec4654f87d62c6e8dabf2b3c9089e1795
tokatoka <tokazerkje@outlook.com>
2023-03-17 21:13:19 +0900

    IfStage

--- libafl/src/stages/logics.rs
+++ libafl/src/stages/logics.rs
@@ -86,6 +86,73 @@ where
     }
 }
 
+#[derive(Debug)]
+pub struct IfStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    closure: CB,
+    if_stages: ST,
+    phantom: PhantomData<(E, EM, Z)>,
+}
+
+impl<CB, E, EM, ST, Z> UsesState for IfStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    type State = E::State;
+}
+
+impl<CB, E, EM, ST, Z> Stage<E, EM, Z> for IfStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    fn perform(
+        &mut self,
+        fuzzer: &mut Z,
+        executor: &mut E,
+        state: &mut E::State,
+        manager: &mut EM,
+        corpus_idx: CorpusId,
+    ) -> Result<(), Error> {
+        if (self.closure)(fuzzer, executor, state, manager, corpus_idx)? {
+            self.if_stages
+                .perform_all(fuzzer, executor, state, manager, corpus_idx)?;
+        }
+        Ok(())
+    }
+}
+
+impl<CB, E, EM, ST, Z> IfStage<CB, E, EM, ST, Z>
+where
+    CB: FnMut(&mut Z, &mut E, &mut E::State, &mut EM, CorpusId) -> Result<bool, Error>,
+    E: UsesState,
+    EM: UsesState<State = E::State>,
+    ST: StagesTuple<E, EM, E::State, Z>,
+    Z: UsesState<State = E::State>,
+{
+    /// Constructor
+    pub fn new(closure: CB, if_stages: ST) -> Self {
+        Self {
+            closure,
+            if_stages,
+            phantom: PhantomData,
+        }
+    }
+}
+
 /// Perform the stage if closure evaluates to true
 #[derive(Debug)]
 pub struct IfElseStage<CB, E, EM, ST1, ST2, Z>
