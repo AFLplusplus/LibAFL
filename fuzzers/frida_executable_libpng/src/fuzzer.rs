35735eac23a09a4eac88c5b4c9b4a2f46dc9f730
SpaceWhite <space_white@yahoo.com>
2023-03-17 20:41:17 +0900

    fix metadata() to metadata_map()

--- fuzzers/frida_executable_libpng/src/fuzzer.rs
+++ fuzzers/frida_executable_libpng/src/fuzzer.rs
@@ -164,7 +164,7 @@ unsafe fn fuzz(
                 println!("We're a client, let's fuzz :)");
 
                 // Create a PNG dictionary if not existing
-                if state.metadata().get::<Tokens>().is_none() {
+                if state.metadata_map().get::<Tokens>().is_none() {
                     state.add_metadata(Tokens::from([
                         vec![137, 80, 78, 71, 13, 10, 26, 10], // PNG header
                         b"IHDR".to_vec(),
@@ -279,7 +279,7 @@ unsafe fn fuzz(
                 println!("We're a client, let's fuzz :)");
 
                 // Create a PNG dictionary if not existing
-                if state.metadata().get::<Tokens>().is_none() {
+                if state.metadata_map().get::<Tokens>().is_none() {
                     state.add_metadata(Tokens::from([
                         vec![137, 80, 78, 71, 13, 10, 26, 10], // PNG header
                         b"IHDR".to_vec(),
@@ -409,7 +409,7 @@ unsafe fn fuzz(
                 println!("We're a client, let's fuzz :)");
 
                 // Create a PNG dictionary if not existing
-                if state.metadata().get::<Tokens>().is_none() {
+                if state.metadata_map().get::<Tokens>().is_none() {
                     state.add_metadata(Tokens::from([
                         vec![137, 80, 78, 71, 13, 10, 26, 10], // PNG header
                         b"IHDR".to_vec(),
