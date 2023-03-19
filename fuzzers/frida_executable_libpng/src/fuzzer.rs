8345e24d587d8f224974262460f3e7e23e48ccaa
SpaceWhite <space_white@yahoo.com>
2023-03-19 18:27:28 +0900

    fix maxmapfeedback

--- fuzzers/frida_executable_libpng/src/fuzzer.rs
+++ fuzzers/frida_executable_libpng/src/fuzzer.rs
@@ -128,7 +128,7 @@ unsafe fn fuzz(
                 // This one is composed by two Feedbacks in OR
                 let mut feedback = feedback_or!(
                     // New maximization map feedback linked to the edges observer and the feedback state
-                    MaxMapFeedback::new_tracking(&edges_observer, true, false),
+                    MaxMapFeedback::tracking(&edges_observer, true, false),
                     // Time feedback, this one does not need a feedback state
                     TimeFeedback::with_observer(&time_observer)
                 );
@@ -245,7 +245,7 @@ unsafe fn fuzz(
                 // This one is composed by two Feedbacks in OR
                 let mut feedback = feedback_or!(
                     // New maximization map feedback linked to the edges observer and the feedback state
-                    MaxMapFeedback::new_tracking(&edges_observer, true, false),
+                    MaxMapFeedback::tracking(&edges_observer, true, false),
                     // Time feedback, this one does not need a feedback state
                     TimeFeedback::with_observer(&time_observer)
                 );
@@ -375,7 +375,7 @@ unsafe fn fuzz(
                 // This one is composed by two Feedbacks in OR
                 let mut feedback = feedback_or!(
                     // New maximization map feedback linked to the edges observer and the feedback state
-                    MaxMapFeedback::new_tracking(&edges_observer, true, false),
+                    MaxMapFeedback::tracking(&edges_observer, true, false),
                     // Time feedback, this one does not need a feedback state
                     TimeFeedback::with_observer(&time_observer)
                 );
