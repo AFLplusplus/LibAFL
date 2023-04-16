4d4287302e464bf8188388434bbdea50f1824570
Dominik Maier <dmnk@google.com>
2023-04-16 12:28:34 +0200

    Example fuzzers with even less UB

--- fuzzers/baby_fuzzer_tokens/src/main.rs
+++ fuzzers/baby_fuzzer_tokens/src/main.rs
@@ -1,6 +1,6 @@
 #[cfg(windows)]
 use std::ptr::write_volatile;
-use std::{fs, io::Read, path::PathBuf};
+use std::{fs, io::Read, path::PathBuf, ptr::write};
 
 use libafl::{
     bolts::{current_nanos, rands::StdRand, tuples::tuple_list},
@@ -20,10 +20,12 @@ use libafl::{
 
 /// Coverage map with explicit assignments due to the lack of instrumentation
 static mut SIGNALS: [u8; 16] = [0; 16];
+static mut SIGNALS_PTR: *mut u8 = unsafe { SIGNALS.as_mut_ptr() };
+
 /*
 /// Assign a signal to the signals map
 fn signals_set(idx: usize) {
-    unsafe { SIGNALS[idx] = 1 };
+    unsafe { write(SIGNALS_PTR.add(idx), 1) };
 }
 */
 
@@ -65,8 +67,7 @@ pub fn main() {
     };
 
     // Create an observation channel using the signals map
-    let observer =
-        unsafe { StdMapObserver::from_mut_ptr("signals", SIGNALS.as_mut_ptr(), SIGNALS.len()) };
+    let observer = unsafe { StdMapObserver::from_mut_ptr("signals", SIGNALS_PTR, SIGNALS.len()) };
 
     // Feedback to rate the interestingness of an input
     let mut feedback = MaxMapFeedback::new(&observer);
--- fuzzers/baby_fuzzer_with_forkexecutor/src/main.rs
+++ fuzzers/baby_fuzzer_with_forkexecutor/src/main.rs
@@ -1,6 +1,6 @@
-use std::path::PathBuf;
 #[cfg(windows)]
 use std::ptr::write_volatile;
+use std::{path::PathBuf, ptr::write};
 
 use libafl::{
     bolts::{
@@ -29,11 +29,11 @@ use libafl::{
 pub fn main() {
     let mut shmem_provider = unix_shmem::UnixShMemProvider::new().unwrap();
     let mut signals = shmem_provider.new_shmem(16).unwrap();
-    let mut signals_clone = signals.clone();
+    let signals_len = signals.as_slice().len();
+    let signals_ptr = signals.as_mut_slice().as_mut_ptr();
 
-    let mut signals_set = |idx: usize| {
-        let a = signals.as_mut_slice();
-        a[idx] = 1;
+    let signals_set = |idx: usize| {
+        unsafe { write(signals_ptr.add(idx), 1) };
     };
 
     // The closure that we want to fuzz
@@ -64,7 +64,7 @@ pub fn main() {
     };
 
     // Create an observation channel using the signals map
-    let observer = unsafe { StdMapObserver::new("signals", signals_clone.as_mut_slice()) };
+    let observer = unsafe { StdMapObserver::from_mut_ptr("signals", signals_ptr, signals_len) };
     // Create a stacktrace observer to add the observers tuple
 
     // Feedback to rate the interestingness of an input, obtained by ANDing the interestingness of both feedbacks
