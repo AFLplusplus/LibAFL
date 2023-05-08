af75d0bcb43f7df54685d5886d333f811994f1de
Dominik Maier <dmnk@google.com>
2023-05-08 14:26:49 +0200

    Fix latest clippy

--- libafl/src/lib.rs
+++ libafl/src/lib.rs
@@ -75,10 +75,11 @@ Welcome to `LibAFL`
 #[macro_use]
 extern crate std;
 #[macro_use]
-pub extern crate alloc;
+extern crate alloc;
 #[macro_use]
 extern crate static_assertions;
 #[cfg(feature = "ctor")]
+#[doc(hidden)]
 pub use ctor::ctor;
 
 // Re-export derive(SerdeAny)
