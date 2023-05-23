6bca078cc8a030e875918a1e6504c1ac585e037e
Dimitri Kokkonis <dimitri.kokkonis@cea.fr>
2023-05-23 14:30:01 +0200

    Clean up documentation on the baby fuzzer
    Since the baby fuzzer chapter of the documentation is done in a
    "tutorial", step-by-step fashion, it would be nice to be able to see
    where exactly new lines have to be placed in the existing code. To that
    end, the code used in the tutorial is moved to snippets (as is done in
    the Rust Book), as it allows for much more convenient maintenance of the
    snippets, as well as easy hiding of the non-important code on any given
    snippet.
    
    Furthermore, a few minor fixes are applied; a typo on a comment and a
    missing unsafe block.

---
+++ docs/listings/baby_fuzzer/listing-03/src/main.rs
@@ -0,0 +1,25 @@
+extern crate libafl;
+use libafl::{
+    bolts::AsSlice,
+    executors::ExitKind,
+    inputs::{BytesInput, HasTargetBytes},
+};
+
+fn main() {
+    let mut harness = |input: &BytesInput| {
+        let target = input.target_bytes();
+        let buf = target.as_slice();
+        if buf.len() > 0 && buf[0] == 'a' as u8 {
+            if buf.len() > 1 && buf[1] == 'b' as u8 {
+                if buf.len() > 2 && buf[2] == 'c' as u8 {
+                    panic!("=)");
+                }
+            }
+        }
+        ExitKind::Ok
+    };
+    // To test the panic:
+    let input = BytesInput::new(Vec::from("abc"));
+    #[cfg(feature = "panic")]
+    harness(&input);
+}
