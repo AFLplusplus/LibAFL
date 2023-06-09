a843109857a7405076578ea62aab692a0d8ebad9
Dominik Maier <dmnk@google.com>
2023-06-09 13:34:12 +0200

    added test, removed static_assertions

--- libafl/src/bolts/anymap.rs
+++ libafl/src/bolts/anymap.rs
@@ -44,6 +44,8 @@ macro_rules! impl_asany {
 ///
 /// # Note
 /// Probably not safe for future compilers, fine for now.
+/// The size changed in later rust versions, see <https://github.com/rust-lang/compiler-team/issues/608>
+#[inline]
 #[must_use]
 pub const fn pack_type_id(id: u128) -> TypeId {
     match size_of::<TypeId>() {
@@ -54,9 +56,7 @@ pub const fn pack_type_id(id: u128) -> TypeId {
         16 => unsafe { *(addr_of!(id) as *const TypeId) },
         _ => {
             // TypeId size of this size is not yet supported"
-            // The assert will complain at compiletime.
-            assert_eq_size!(TypeId, u64);
-            unreachable!();
+            panic!("Unsupported size for TypeId");
         }
     }
 }
@@ -66,6 +66,8 @@ pub const fn pack_type_id(id: u128) -> TypeId {
 ///
 /// # Note
 /// Probably not safe for future compilers, fine for now.
+/// The size changed in later rust versions, see <https://github.com/rust-lang/compiler-team/issues/608>
+#[inline]
 #[must_use]
 pub const fn unpack_type_id(id: TypeId) -> u128 {
     #[allow(clippy::cast_ptr_alignment)] // we never actually cast to u128 if the type is u64.
@@ -74,9 +76,7 @@ pub const fn unpack_type_id(id: TypeId) -> u128 {
         16 => unsafe { *(addr_of!(id) as *const u128) },
         _ => {
             // TypeId size of this size is not yet supported"
-            // The assert will complain at compiletime.
-            assert_eq_size!(TypeId, u64);
-            unreachable!();
+            panic!("Unsupported size for TypeId");
         }
     }
 }
@@ -451,5 +451,24 @@ macro_rules! create_anymap_for_trait {
                 }
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use core::any::TypeId;
+
+    use super::{pack_type_id, unpack_type_id};
+
+    #[test]
+    fn test_type_id() {
+        let type_id_u64 = unpack_type_id(TypeId::of::<u64>());
+        let type_id_u128 = unpack_type_id(TypeId::of::<u128>());
+
+        assert_eq!(pack_type_id(type_id_u64), TypeId::of::<u64>());
+        assert_eq!(pack_type_id(type_id_u128), TypeId::of::<u128>());
+
+        assert_ne!(pack_type_id(type_id_u64), TypeId::of::<u128>());
+        assert_ne!(pack_type_id(type_id_u128), TypeId::of::<u64>());
     }
 }
