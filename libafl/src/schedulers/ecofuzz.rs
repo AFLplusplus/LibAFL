2d7d1c578c656745c281d43c7ef4141309d4e5d7
Dominik Maier <dmnk@google.com>
2023-07-04 13:44:14 +0000

    More clippy

--- libafl/src/schedulers/ecofuzz.rs
+++ libafl/src/schedulers/ecofuzz.rs
@@ -150,7 +150,7 @@ where
             if !was_fuzzed {
                 let selection = Some(id);
                 state.metadata_mut::<EcoMetadata>()?.state = EcoState::Exploration;
-                #[allow(clippy::explicit_iter_loop)]
+                #[allow(clippy::unnecessary_literal_unwrap)] // false positive
                 return Ok(selection.expect("Error in the algorithm, this cannot be None"));
             }
         }
