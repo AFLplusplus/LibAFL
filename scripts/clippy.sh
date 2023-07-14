9d2589d358c25263de6a73e77238138b12e63a41
Addison Crump <addison.crump@cispa.de>
2023-07-14 19:05:17 +0200

    fixup clippy script

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -4,20 +4,20 @@ cd "$SCRIPT_DIR/.." || exit 1
 
 set -e
 
-#RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --release --tests --examples --benches -- -Z macro-backtrace \
-#   -D clippy::all \
-#   -D clippy::pedantic \
-#   -W clippy::similar_names \
-#   -A clippy::type_repetition_in_bounds \
-#   -A clippy::missing-errors-doc \
-#   -A clippy::cast-possible-truncation \
-#   -A clippy::used-underscore-binding \
-#   -A clippy::ptr-as-ptr \
-#   -A clippy::missing-panics-doc \
-#   -A clippy::missing-docs-in-private-items \
-#   -A clippy::unseparated-literal-suffix \
-#   -A clippy::module-name-repetitions \
-#   -A clippy::unreadable-literal
+RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --release --tests --examples --benches -- -Z macro-backtrace \
+   -D clippy::all \
+   -D clippy::pedantic \
+   -W clippy::similar_names \
+   -A clippy::type_repetition_in_bounds \
+   -A clippy::missing-errors-doc \
+   -A clippy::cast-possible-truncation \
+   -A clippy::used-underscore-binding \
+   -A clippy::ptr-as-ptr \
+   -A clippy::missing-panics-doc \
+   -A clippy::missing-docs-in-private-items \
+   -A clippy::unseparated-literal-suffix \
+   -A clippy::module-name-repetitions \
+   -A clippy::unreadable-literal
 
 cd libafl_libfuzzer/libafl_libfuzzer_runtime
 RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --release --tests --examples --benches -- -Z macro-backtrace \
