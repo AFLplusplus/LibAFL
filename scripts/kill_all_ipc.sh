44e10d8b21eb6302d78cb4374fb5660e16fc6257
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-08-08 15:08:31 +0200

    shellcheck

--- scripts/kill_all_ipc.sh
+++ scripts/kill_all_ipc.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 PATH="/bin:/usr/bin"
 
-IPCS_S=$(ipcs -s | egrep "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
-IPCS_M=$(ipcs -m | egrep "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
-IPCS_Q=$(ipcs -q | egrep "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
+IPCS_S=$(ipcs -s | grep -E "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
+IPCS_M=$(ipcs -m | grep -E "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
+IPCS_Q=$(ipcs -q | grep -E "0x[0-9a-f]+ [0-9]+" | grep "${USER}" | cut -f2 -d" ")
 
-for id in "$IPCS_M"; do
-  ipcrm -m $id;
+for id in $IPCS_M; do
+  ipcrm -m "$id";
 done
 
-for id in "$IPCS_S"; do
-  ipcrm -s $id;
+for id in $IPCS_S; do
+  ipcrm -s "$id";
 done
 
-for id in "$IPCS_Q"; do
-  ipcrm -q $id;
+for id in $IPCS_Q; do
+  ipcrm -q "$id";
 done
 
