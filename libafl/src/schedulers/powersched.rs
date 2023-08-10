4bee9a90399c157227ecbfe56d345c6564ec9786
lenawanel <115283664+lenawanel@users.noreply.github.com>
2023-08-10 14:23:19 +0200

    Update documentation of `PowerQueueScheduler::on_add` (#1409)
    this fixes https://github.com/AFLplusplus/LibAFL/issues/1373

--- libafl/src/schedulers/powersched.rs
+++ libafl/src/schedulers/powersched.rs
@@ -252,7 +252,7 @@ where
     S: HasCorpus + HasMetadata + HasTestcase,
     O: MapObserver,
 {
-    /// Add an entry to the corpus and return its index
+    /// Add an entry to the corpus
     fn on_add(&mut self, state: &mut Self::State, idx: CorpusId) -> Result<(), Error> {
         let current_idx = *state.corpus().current();
 
