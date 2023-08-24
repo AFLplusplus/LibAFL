74457fbc1d40ccecb2887669241ddbdad364a99c
lazymio <mio@lazym.io>
2023-08-24 13:28:48 +0800

    Revert back grimoire fix

--- fuzzers/baby_fuzzer_grimoire/src/main.rs
+++ fuzzers/baby_fuzzer_grimoire/src/main.rs
@@ -151,7 +151,7 @@ pub fn main() {
     let mut stages = tuple_list!(
         generalization,
         StdMutationalStage::new(mutator),
-        StdMutationalStage::new(grimoire_mutator)
+        StdMutationalStage::transforming(grimoire_mutator)
     );
 
     for input in initial_inputs {
