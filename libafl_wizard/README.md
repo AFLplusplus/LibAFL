5c4669f64cfa9c047f34240a8fa9532fe62e6766
matheusbaptistella <mbapts@gmail.com>
2023-08-27 20:02:45 -0300

    Added the UI and changed some logic

--- libafl_wizard/README.md
+++ libafl_wizard/README.md
@@ -1,1 +1,24 @@
-TODO
+# libafl_wizard
+
+<img align="right" src="./icons/libafl_wizard.png" alt="libafl_wizard logo" width="250" heigh="250">
+
+libafl_wizard is a tool to generate fuzzers using libafl's components. Simply by answering some questions, while learning more about how libafl is used, you can generate your own fuzzer to test programs and help with the development of more secure code!
+
+## Usage
+
+libafl_wizard has a UI, made with [Slint](https://slint.dev/releases/1.1.1/docs/slint/), to help during the process. You can choose between answers by simply clicking on them and visualize where these answers might take you through a flowchart!
+
+```
+cargo run --features ui
+```
+
+If you don't want or can't use libafl_wizard with the UI, it's possible to use the CLI version, but, as of now, this version doesn't contain a flowchart.
+
+```
+cargo run
+```
+
+## Contributing
+libafl_wizard uses the `questions.toml` TOML file to store and load the questions that will be asked during the generation process. Each question contains some fields, like the possible answers for that question and the Rust code associated to those answers. As libafl's components get updated or new ones introduced, the questions need to be updated as well.
+
+For changes on the UI, please check the [Slint](https://slint.dev/releases/1.1.1/docs/slint/) documentation page to learn more about the language!
--- libafl_wizard/src/lib.rs
+++ libafl_wizard/src/lib.rs
@@ -1,5 +1,5 @@
 use serde::Deserialize;
-use std::{error::Error, fs, fs::OpenOptions, io::Write, path::Path};
+use std::{fs, fs::OpenOptions, io::Write, path::Path, process::Command};
 use toml;
 
 #[derive(Deserialize)]
@@ -14,34 +14,35 @@ struct QuestionList {
     question: Vec<Question>,
 }
 
-// This represents a "node": the answer of a Question might lead to different Questions (different nodes).
 #[derive(Clone, Deserialize, PartialEq, Debug)]
 pub struct Question {
-    id: String,           // ID is resolved to local indexes.
-    title: String,        // The question that will be asked.
-    content: String,      // Description related to the question, to help the user.
-    answers: Vec<String>, // A vector containing all the possible answers for this question.
+    id: String, // ID is resolved to local indexes.
+    pub title: String,
+    pub content: String, // Description related to the question, to help the user.
+    pub answers: Vec<String>, // A vector containing all the possible answers for this question.
     next: Vec<String>, // A vector containing the next question for all the possible answers (for answer[0] the next question is next[0]...).
     code: Vec<String>, // Contains the Rust code associated to the question.
-    previous: usize, // The question that lead to the current one (possible UNDO functionality implementation).
-    skip: bool,      // Marks questions that should be skipped.
+    previous: usize,   // The question that led to the current one.
+    skip: bool,        // Marks questions that should be skipped.
 }
 
 impl Question {
     // Builds the diagram of questions from the csv file (this will probably be changed, the csv is used only for demonstrative purposes).
     // The diagram is a vector of Questions (vector of nodes): each Question, depending on the answer, will have the index of the next Question
     // that should be asked.
-    pub fn new() -> Result<Vec<Question>, Box<dyn Error>> {
-        let contents = fs::read_to_string("questions.toml")?;
+    pub fn new() -> Vec<Question> {
+        let contents =
+            fs::read_to_string("questions.toml").expect("Failed to read questions file.");
 
-        let q_list: QuestionList = toml::from_str(&contents)?;
+        let q_list: QuestionList =
+            toml::from_str(&contents).expect("Failed to parse toml questions.");
 
         // // Checks if each question is valid.
         // for q in q_list.question.iter() {
         //     q.validate_question()?;
         // }
 
-        Ok(q_list.question)
+        q_list.question
     }
 
     pub fn end(&self) -> bool {
@@ -52,17 +53,25 @@ impl Question {
         false
     }
 
-    pub fn print_question(&self) -> () {
-        print!("{esc}c", esc = 27 as char);
-        println!("=========================\nFuzzer Template Generator\n=========================");
-        println!("{}\n", self.title);
-        println!("{}\n", self.content);
+    pub fn print_question(&self) {
+        let mut output = String::new();
+
+        clear_terminal_screen();
+
+        // Construct the output string
+        output.push_str(&format!(
+            "=========================\nFuzzer Template Generator\n=========================\n"
+        ));
+        output.push_str(&format!("{}\n\n", self.title));
+        output.push_str(&format!("{}\n\n", self.content));
 
         for ans in &self.answers {
-            print!("\t{}", ans);
+            output.push_str(&format!("\t{}", ans));
         }
 
-        println!("\tUndo");
+        output.push_str("\tUndo\n");
+
+        print!("{}", output);
     }
 
     pub fn resolve_answer(
@@ -71,14 +80,14 @@ impl Question {
         input: &String,
     ) -> (usize, bool, usize) {
         // The "Undo" option makes the generator go back to the previous question, so if the user do something by mistake, they can correct it.
-        if validate_answer(&input, &String::from("Undo")) {
+        if validate_input(&input, &String::from("Undo")) {
             return (self.previous, true, 0);
         }
 
         // Checks if the user typed one of the acceptable answers. If so, returns the index of the next question associated to that answer.
         // For now we don't check for variants.
         for (i, ans) in self.answers.iter().enumerate() {
-            if validate_answer(&input, &ans) {
+            if validate_input(&input, &ans) {
                 let mut next_q = self.next_question(questions, i);
 
                 // If the question should be skipped, then the generator goes to next question.
@@ -118,7 +127,7 @@ impl Question {
         false
     }
 
-    pub fn add_code(&self, code_content: &mut Vec<String>, ans_i: usize) -> () {
+    pub fn add_code(&self, code_content: &mut Vec<String>, ans_i: usize) {
         if self.code[ans_i] != "" {
             code_content.push(self.code[ans_i].to_string());
         }
@@ -131,23 +140,23 @@ impl Question {
         next_q: usize,
         in_process_qs: &Vec<String>,
         forkserver_qs: &Vec<String>,
-    ) -> () {
+    ) {
         // If the user doesn't have the source code or can't provide a harness, then in process fuzzing in not possible and the questions
         // related to it are skipped.
         if ((self.id == "source code") || (self.id == "harness"))
-            && validate_answer(&input, &String::from("No"))
+            && validate_input(&input, &String::from("No"))
         {
             skip_questions(questions, &in_process_qs);
         }
         // If it is possible, then forkserver/llmp questions are skipped.
-        else if (self.id == "harness") && validate_answer(&input, &String::from("Yes")) {
+        else if (self.id == "harness") && validate_input(&input, &String::from("Yes")) {
             skip_questions(questions, &forkserver_qs);
         // If the user is undoing, then the generator resets the questions that were marked to be skipped.
         } else if ((self.id == "harness")
             || (self.id == "observers")
             || (self.id == "map size"
                 && (questions[next_q].id == "source code" || questions[next_q].id == "harness")))
-            && validate_answer(&input, &String::from("Undo"))
+            && validate_input(&input, &String::from("Undo"))
         {
             unskip_questions(questions, &in_process_qs);
             unskip_questions(questions, &forkserver_qs);
@@ -160,15 +169,15 @@ impl Question {
     }
 }
 
-pub fn read_sections() -> Result<(Vec<String>, Vec<String>), Box<dyn Error>> {
-    let contents = fs::read_to_string("sections.toml")?;
+pub fn read_sections() -> (Vec<String>, Vec<String>) {
+    let contents = fs::read_to_string("sections.toml").expect("Failed to read sections file.");
 
-    let sections: Sections = toml::from_str(&contents)?;
+    let sections: Sections = toml::from_str(&contents).expect("Failed to parse toml sections.");
 
-    Ok((
+    (
         sections.in_process_skip_questions,
         sections.forkserver_skip_questions,
-    ))
+    )
 }
 
 pub fn skip_questions(questions: &mut Vec<Question>, vec: &Vec<String>) -> () {
@@ -228,37 +237,102 @@ pub fn replace_code(code_content: &mut Vec<String>) -> () {
     }
 }
 
+// pub fn arrange_code(code_content: Vec<String>) -> Vec<String> {
+//     // List of libafl's components
+//     let components: Vec<String> = vec!["Observer", "Feedback", "State", "Monitor", "Event", "Scheduler", "Fuzzer", "Executor", "Generator", "Mutator", "Stage"];
+
+//     let code_line = String::new();
+
+//     let mut final_code: Vec<String> = Vec::new();
+
+//     // Each of the fields of 'code_content' contains a String, which contains a single or mroe lines of Rust code.
+//     // The idea here is to parse each String, looking for the Strings contained in 'components' as a substring, to find the components that
+//     // are going to be used.
+//     for s in code_content {
+//         for c in s.chars() {
+//             if c == ';' {
+//                 code_line.push(c);
+
+//                 // 'code_line' contains a line of Rust code, so we check to see if it's a component.
+//                 if let Some((i, _)) = components.iter().enumerate().find(|(_, comp)| code_line.contains(comp)) {
+//                     // If it's a component, we must write it to 'final_code' in the same order as the 'components' vector.
+//                     for (j, code) in final_code.iter().enumerate() {
+//                         if let Some(k) = components.iter().position(|&other_comp| code.contains(other_comp)) {
+//                             // If the current component in 'final_code' contains the same class of component, or if it contains a class that
+//                             // goes after the current one, we place it in this position.
+//                             if (other_comp == comp) || (i < k) {
+//                                 final_code.insert(j, code_line);
+//                                 break;
+//                             }
+//                         }
+
+//                         // If we've gone through the whole 'final_code' vector and didn't insert the code, this means we have a component
+//                         // that must be add to the end.
+//                         if j == final_code.len() {
+//                             final_code.push(code_line);
+//                             break;
+//                         }
+//                     }
+//                 }
+//                 else {
+//                     // If it's not a component, we simply add it to the end.
+//                     final_code.push(code_line);
+//                 }
+//             }
+//             else {
+//                 code_line.push(c);
+//             }
+//         }
+//     }
+
+//     final_code
+// }
+
 // Write Rust code in the file of the generated fuzzer.
-pub fn write_code(code_content: Vec<String>) -> Result<String, Box<dyn Error>> {
+pub fn write_code(code_content: Vec<String>) -> String {
     let mut counter = 0;
     let mut file_name = format!("fuzzer.rs");
 
+    let fuzzers_folder = "./fuzzers";
+    if !Path::new(fuzzers_folder).exists() {
+        fs::create_dir(fuzzers_folder).expect("Failed to create fuzzers directory.");
+    }
+
     // Creates "fuzzer.rs", "fuzzer_1.rs" files if the previous one already exists...
-    while Path::new(&file_name).exists() {
+    while Path::new(&format!("{}/{}", fuzzers_folder, file_name)).exists() {
         counter += 1;
         file_name = format!("fuzzer_{}.rs", counter);
     }
 
+    let file_path = format!("{}/{}", fuzzers_folder, file_name);
+
     let mut out_file = OpenOptions::new()
         .append(true)
         .create(true)
-        .open(&file_name)?;
+        .open(&file_path)
+        .expect("Failed to open the fuzzer file.");
 
     // While imports are not resolved, use this.
-    out_file.write_all("use libafl::prelude::*;\n\n".as_bytes())?;
+    out_file
+        .write_all("use libafl::prelude::*;\n\n".as_bytes())
+        .expect("Failed to write to the fuzzer file.");
 
     for (i, code) in code_content.iter().enumerate() {
-        out_file.write_all(code.as_bytes())?;
+        out_file
+            .write_all(code.as_bytes())
+            .expect("Failed to write to the fuzzer file.");
 
         if i < code_content.len() {
-            out_file.write_all(";\n\n".as_bytes())?;
+            out_file
+                .write_all(";\n\n".as_bytes())
+                .expect("Failed to write to the fuzzer file.");
         }
     }
 
-    Ok(file_name)
+    file_name
 }
 
-pub fn validate_answer(input: &String, ans: &String) -> bool {
+pub fn validate_input(input: &String, ans: &String) -> bool {
     let input_lower = input.to_lowercase();
     let ans_lower = ans.to_lowercase();
 
@@ -266,7 +340,7 @@ pub fn validate_answer(input: &String, ans: &String) -> bool {
     let mut ans_chars = ans_lower.chars();
 
     // Basically, an aswer is valid if it is an acceptable variant of that given answer. Acceptable variants are strings that contain the
-    // characters in the same order as the answer, so for the answer "Yes", acceptable variants are: "y", "Ye", "yes", "yES", but not
+    // characters in the same order as the answer, so for the answer "Yes", acceptable variants are: "y", "Ye", "yes", "YES", but not
     // "Yess", "yy", "Yhes", "yYess"...
     while let (Some(input_c), Some(ans_c)) = (input_chars.next(), ans_chars.next()) {
         if input_c != ans_c {
@@ -275,4 +349,21 @@ pub fn validate_answer(input: &String, ans: &String) -> bool {
     }
 
     true
+}
+
+pub fn clear_terminal_screen() {
+    if cfg!(target_os = "windows") {
+        Command::new("cmd")
+            .args(["/c", "cls"])
+            .spawn()
+            .expect("cls command failed to start")
+            .wait()
+            .expect("failed to wait");
+    } else {
+        Command::new("clear")
+            .spawn()
+            .expect("clear command failed to start")
+            .wait()
+            .expect("failed to wait");
+    };
 }
