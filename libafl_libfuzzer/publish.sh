912c3a3206ca9b6b003ba6d80473b888af861095
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-08-29 11:32:11 +0200

    allow dirty

--- libafl_libfuzzer/publish.sh
+++ libafl_libfuzzer/publish.sh
@@ -3,5 +3,5 @@
 set -x
 
 mv libafl_libfuzzer_runtime/Cargo.toml libafl_libfuzzer_runtime/Cargo.toml.orig
-cargo publish --no-verify "$@"
+cargo publish --allow-dirty --no-verify "$@"
 mv libafl_libfuzzer_runtime/Cargo.toml.orig libafl_libfuzzer_runtime/Cargo.toml
