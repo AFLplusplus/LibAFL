f3b1084423375fa470ca63dd40e198efd4ea6c25
Addison Crump <addison.crump@cispa.de>
2023-09-22 04:34:53 +0200

    modify API to pre-group

--- libafl/src/stages/string.rs
+++ libafl/src/stages/string.rs
@@ -1,5 +1,6 @@
-use alloc::{borrow::Cow, collections::BTreeSet, vec::Vec};
+use alloc::{collections::BTreeSet, vec::Vec};
 use core::{cmp::Ordering, marker::PhantomData};
+use std::ops::Range;
 
 use libafl_bolts::{impl_serdeany, Error};
 use serde::{Deserialize, Serialize};
@@ -13,30 +14,94 @@ use crate::{
     state::{HasCorpus, HasMetadata, UsesState},
 };
 
-mod unicode_properties {
+pub mod unicode_properties {
     include!(concat!(env!("OUT_DIR"), "/unicode_properties.rs"));
 }
 
+pub type PropertyRange = (Range<usize>, usize);
+pub type PropertyRanges = Vec<PropertyRange>;
+
 #[derive(Serialize, Deserialize, Clone, Debug)]
-pub enum StringClassificationMetadata {
+pub enum StringPropertiesMetadata {
     Unclassifiable,
-    Classifications(Vec<BTreeSet<Cow<'static, str>>>),
+    PropertyRanges(PropertyRanges),
 }
 
-impl_serdeany!(StringClassificationMetadata);
+impl_serdeany!(StringPropertiesMetadata);
 
-pub struct StringClassificationStage<S> {
+#[derive(Debug)]
+pub struct StringPropertiesStage<S> {
     phantom: PhantomData<S>,
 }
 
-impl<S> UsesState for StringClassificationStage<S>
+impl<S> StringPropertiesStage<S> {
+    fn group_by_properties(string: &str) -> PropertyRanges {
+        let mut char_properties = vec![BTreeSet::new(); string.chars().count()];
+        let mut all_properties = BTreeSet::new();
+        for (prop, &(_, ranges)) in unicode_properties::BY_NAME.iter().enumerate() {
+            // type inference help for IDEs
+            let prop: usize = prop;
+            let ranges: &'static [(u32, u32)] = ranges;
+
+            let min = ranges.first().unwrap().0;
+            let max = ranges.last().unwrap().1;
+
+            for (c, properties) in string.chars().zip(char_properties.iter_mut()) {
+                let value = c as u32;
+                if min <= value && value <= max {
+                    if ranges
+                        .binary_search_by(|&(min, max)| match min.cmp(&value) {
+                            Ordering::Less | Ordering::Equal => match value.cmp(&max) {
+                                Ordering::Less | Ordering::Equal => Ordering::Equal,
+                                Ordering::Greater => Ordering::Less,
+                            },
+                            Ordering::Greater => Ordering::Greater,
+                        })
+                        .is_ok()
+                    {
+                        properties.insert(prop);
+                        all_properties.insert(prop);
+                    }
+                }
+            }
+        }
+
+        fn top_is_property(props: &BTreeSet<usize>, prop: usize) -> bool {
+            props.first().map_or(false, |&i| i == prop)
+        }
+
+        let mut prop_ranges = Vec::new();
+        for curr_property in all_properties {
+            let mut prop_iter = string.char_indices().zip(char_properties.iter_mut());
+            loop {
+                let mut prop_iter = (&mut prop_iter)
+                    .skip_while(|(_, props)| !top_is_property(props, curr_property))
+                    .take_while(|(_, props)| top_is_property(props, curr_property))
+                    .map(|((i, c), props)| {
+                        props.pop_first();
+                        (i, c)
+                    });
+                if let Some((min, min_c)) = prop_iter.next() {
+                    let (max, max_c) = prop_iter.last().unwrap_or((min, min_c));
+                    prop_ranges.push((min..(max + max_c.len_utf8()), curr_property));
+                } else {
+                    break;
+                }
+            }
+        }
+
+        prop_ranges
+    }
+}
+
+impl<S> UsesState for StringPropertiesStage<S>
 where
     S: UsesInput,
 {
     type State = S;
 }
 
-impl<S, E, EM, Z> Stage<E, EM, Z> for StringClassificationStage<S>
+impl<S, E, EM, Z> Stage<E, EM, Z> for StringPropertiesStage<S>
 where
     S: UsesInput<Input = BytesInput> + HasCorpus + HasTestcase,
     E: UsesState<State = S>,
@@ -53,7 +118,7 @@ where
     ) -> Result<(), Error> {
         start_timer!(state);
         let mut testcase = state.testcase_mut(corpus_idx)?;
-        if testcase.has_metadata::<StringClassificationMetadata>() {
+        if testcase.has_metadata::<StringPropertiesMetadata>() {
             return Ok(()); // already classified
         }
 
@@ -62,35 +127,33 @@ where
 
         let bytes = input.bytes();
         let metadata = if let Ok(string) = core::str::from_utf8(bytes) {
-            let mut char_properties = vec![BTreeSet::new(); string.chars().count()];
-            for &(name, ranges) in unicode_properties::BY_NAME {
-                let ranges: &'static [(u32, u32)] = ranges; // type inference help for IDEs
-                let min = ranges.first().unwrap().0;
-                let max = ranges.last().unwrap().1;
-
-                for (c, properties) in string.chars().zip(char_properties.iter_mut()) {
-                    let value = c as u32;
-                    if min <= value && value <= max {
-                        if ranges
-                            .binary_search_by(|&(min, max)| match min.cmp(&value) {
-                                Ordering::Less | Ordering::Equal => match value.cmp(&max) {
-                                    Ordering::Less | Ordering::Equal => Ordering::Equal,
-                                    Ordering::Greater => Ordering::Less,
-                                },
-                                Ordering::Greater => Ordering::Greater,
-                            })
-                            .is_ok()
-                        {
-                            properties.insert(Cow::from(name));
-                        }
-                    }
-                }
-            }
-            StringClassificationMetadata::Classifications(char_properties)
+            StringPropertiesMetadata::PropertyRanges(Self::group_by_properties(string))
         } else {
-            StringClassificationMetadata::Unclassifiable
+            StringPropertiesMetadata::Unclassifiable
         };
         testcase.add_metadata(metadata);
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::state::NopState;
+
+    // a not-so-useful test for this
+    #[test]
+    fn check_hex() {
+        let hex = "0123456789abcdef0123456789abcdef";
+        let property_ranges =
+            StringPropertiesStage::<NopState<BytesInput>>::group_by_properties(hex);
+
+        for (range, prop) in property_ranges {
+            let prop = unicode_properties::BY_NAME[prop].0;
+            println!(
+                "{prop}: {} ({range:?})",
+                core::str::from_utf8(&hex.as_bytes()[range.clone()]).unwrap()
+            );
+        }
     }
 }
