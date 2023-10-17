46fb9083ec3aa855fbaeba064dcfc3ae5bba2a7a
toka <tokazerkje@outlook.com>
2023-10-17 16:03:06 +0200

    aaaaaaaaaaaa

--- libafl_cc/src/no-link-rt.c
+++ libafl_cc/src/no-link-rt.c
@@ -149,7 +149,7 @@ void __cmplog_ins_hookN_extended_ctx(uint32_t ctx, uint128_t arg1,
   (void)arg1;
   (void)arg2;
 }
-void __cmplog_ins_hookN(uint32_t ctx, uint128_t arg1, uint128_t arg2,
+void __cmplog_ins_hookN_ctx(uint32_t ctx, uint128_t arg1, uint128_t arg2,
                         uint8_t size) {
   (void)ctx;
   (void)arg1;
