395bd70860e6ac8ce227c0d86883b8d0582d6a2a
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-10-19 17:04:13 +0200

    enlarge redzone

--- libafl_qemu/libqasan/malloc.c
+++ libafl_qemu/libqasan/malloc.c
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <assert.h>
 #include <pthread.h>
 
-#define REDZONE_SIZE 64
+#define REDZONE_SIZE 128
 // 50 mb quarantine
 #define QUARANTINE_MAX_BYTES 52428800
 
