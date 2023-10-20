f156751d68a7018432e4a4994a8ea4da707965c2
toka <tokazerkje@outlook.com>
2023-10-20 17:07:54 +0200

    concolic upd

--- libafl_concolic/symcc_libafl/src/lib.rs
+++ libafl_concolic/symcc_libafl/src/lib.rs
@@ -5,7 +5,7 @@
 /// The URL of the `LibAFL` `SymCC` fork.
 pub const SYMCC_REPO_URL: &str = "https://github.com/AFLplusplus/symcc.git";
 /// The commit of the `LibAFL` `SymCC` fork.
-pub const SYMCC_REPO_COMMIT: &str = "2a3229da6101596af220f20fef5085e59537abcb";
+pub const SYMCC_REPO_COMMIT: &str = "6e1a05550c5129c1e22364b9ed8cf4c10c14eaa8";
 
 #[cfg(feature = "clone")]
 mod clone {
