00c864627112dd9029b44b986afa17bcab09d4ea
Dominik Maier <dmnk@google.com>
2023-11-10 11:54:33 +0100

    What?

--- libafl/src/monitors/mod.rs
+++ libafl/src/monitors/mod.rs
@@ -21,9 +21,7 @@ use core::{fmt, fmt::Write, time::Duration};
 #[cfg(feature = "std")]
 pub use disk::{OnDiskJSONMonitor, OnDiskTOMLMonitor};
 use hashbrown::HashMap;
-#[cfg(not(feature = "afl_exec_sec"))]
-use libafl_bolts::{current_time, format_duration_hms, ClientId};
-use libafl_bolts::Error;
+use libafl_bolts::{current_time, format_duration_hms, ClientId, Error};
 use serde::{Deserialize, Serialize};
 
 #[cfg(feature = "afl_exec_sec")]
