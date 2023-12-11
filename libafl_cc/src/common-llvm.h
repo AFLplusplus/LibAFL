002656b076d70da9855111df5fc0feb1c44ba099
Andrea Fioraldi <andreafioraldi@gmail.com>
2023-12-11 15:26:53 +0100

    Fix #1721 (#1722)

--- libafl_cc/src/common-llvm.h
+++ libafl_cc/src/common-llvm.h
@@ -59,4 +59,64 @@ static uint64_t PowerOf2Ceil(unsigned in) {
 }
 #endif
 
+/* Function that we never instrument or analyze */
+/* Note: this ignore check is also called in isInInstrumentList() */
+static inline bool isIgnoreFunction(const llvm::Function *F) {
+  // Starting from "LLVMFuzzer" these are functions used in libfuzzer based
+  // fuzzing campaign installations, e.g. oss-fuzz
+
+  static constexpr const char *ignoreList[] = {
+
+      "asan.",
+      "llvm.",
+      "sancov.",
+      "__ubsan",
+      "ign.",
+      "__afl",
+      "_fini",
+      "__libc_",
+      "__asan",
+      "__msan",
+      "__cmplog",
+      "__sancov",
+      "__san",
+      "__cxx_",
+      "__decide_deferred",
+      "_GLOBAL",
+      "_ZZN6__asan",
+      "_ZZN6__lsan",
+      "msan.",
+      "LLVMFuzzerM",
+      "LLVMFuzzerC",
+      "LLVMFuzzerI",
+      "maybe_duplicate_stderr",
+      "discard_output",
+      "close_stdout",
+      "dup_and_close_stderr",
+      "maybe_close_fd_mask",
+      "ExecuteFilesOnyByOne"
+
+  };
+
+  for (auto const &ignoreListFunc : ignoreList) {
+    if (F->getName().startswith(ignoreListFunc)) { return true; }
+  }
+  static constexpr const char *ignoreSubstringList[] = {
+
+      "__asan",     "__msan",     "__ubsan",   "__lsan",
+      "__san",      "__sanitize", "_GLOBAL__", "DebugCounter",
+      "DwarfDebug", "DebugLoc"
+
+  };
+
+  for (auto const &ignoreListFunc : ignoreSubstringList) {
+    // hexcoder: F->getName().contains() not avaiilable in llvm 3.8.0
+    if (llvm::StringRef::npos != F->getName().find(ignoreListFunc)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 #endif  // LIBAFL_COMMON_LLVM_H
