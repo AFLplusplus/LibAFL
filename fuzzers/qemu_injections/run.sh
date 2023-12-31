ae778f66aed9ba83a881063f4c5a04034d2e2372
vanhauser-thc <vh@thc.org>
2023-12-31 11:48:21 +0100

    remove qemu_injections

--- fuzzers/qemu_injections/run.sh
+++
@@ -1,1 +0,0 @@
-clear;cargo run -- -y injections.yaml -i in -o out -v  -- ./static 2>&1|grep -i injection
--- fuzzers/qemu_injections/runshared.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=`pwd`
-make && { clear;cargo run -- -y injections.yaml -i in -o out -v  -- ./sqltest 2>&1|tee foo; }
