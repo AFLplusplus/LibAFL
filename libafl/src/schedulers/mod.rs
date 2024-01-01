271701860170c3fb8d4c83f0cf94ffbfb6a8a0d6
Dongjia "toka" Zhang <tokazerkje@outlook.com>
2024-01-01 18:22:03 +0100

    Dedup common code in scheduler (#1702)
    * dedup common code in scheduler
    
    * del eco
    
    * fixing
    
    * fix

--- libafl/src/schedulers/mod.rs
+++ libafl/src/schedulers/mod.rs
@@ -1,6 +1,9 @@
 //! Schedule the access to the Corpus.
 
-use alloc::borrow::ToOwned;
+use alloc::{
+    borrow::ToOwned,
+    string::{String, ToString},
+};
 use core::marker::PhantomData;
 
 pub mod testcase_score;
@@ -15,7 +18,7 @@ pub use minimizer::{
 };
 
 pub mod powersched;
-pub use powersched::PowerQueueScheduler;
+pub use powersched::{PowerQueueScheduler, SchedulerMetadata};
 
 pub mod probabilistic_sampling;
 pub use probabilistic_sampling::ProbabilitySamplingScheduler;
@@ -26,23 +29,20 @@ pub use accounting::CoverageAccountingScheduler;
 pub mod weighted;
 pub use weighted::{StdWeightedScheduler, WeightedScheduler};
 
-pub mod ecofuzz;
-pub use ecofuzz::{EcoMetadata, EcoScheduler, EcoState, EcoTestcaseMetadata, EcoTestcaseScore};
-
 pub mod tuneable;
 use libafl_bolts::rands::Rand;
 pub use tuneable::*;
 
 use crate::{
-    corpus::{Corpus, CorpusId, HasTestcase, Testcase},
+    corpus::{Corpus, CorpusId, HasTestcase, SchedulerTestcaseMetadata, Testcase},
     inputs::UsesInput,
-    observers::ObserversTuple,
+    observers::{MapObserver, ObserversTuple},
     random_corpus_id,
-    state::{HasCorpus, HasRand, State, UsesState},
+    state::{HasCorpus, HasMetadata, HasRand, State, UsesState},
     Error,
 };
 
-/// The scheduler also implemnts `on_remove` and `on_replace` if it implements this stage.
+/// The scheduler also implements `on_remove` and `on_replace` if it implements this stage.
 pub trait RemovableScheduler: Scheduler
 where
     Self::State: HasCorpus,
@@ -64,6 +64,171 @@ where
         _idx: CorpusId,
         _prev: &Testcase<<Self::State as UsesInput>::Input>,
     ) -> Result<(), Error> {
+        Ok(())
+    }
+}
+
+/// Define the metadata operations when removing testcase from AFL-style scheduler
+pub trait HasAFLRemovableScheduler: RemovableScheduler
+where
+    Self::State: HasCorpus + HasMetadata + HasTestcase,
+{
+    #[allow(clippy::cast_precision_loss)]
+    #[allow(clippy::cast_precision_loss)]
+    /// Adjusting metadata when removing the testcase
+    fn on_remove_metadata(
+        &mut self,
+        state: &mut Self::State,
+        _idx: CorpusId,
+        prev: &Option<Testcase<<Self::State as UsesInput>::Input>>,
+    ) -> Result<(), Error> {
+        let prev = prev.as_ref().ok_or_else(|| {
+            Error::illegal_argument(
+                "Power schedulers must be aware of the removed corpus entry for reweighting.",
+            )
+        })?;
+
+        let prev_meta = prev.metadata::<SchedulerTestcaseMetadata>()?;
+
+        // Use these to adjust `SchedulerMetadata`
+        let (prev_total_time, prev_cycles) = prev_meta.cycle_and_time();
+        let prev_bitmap_size = prev_meta.bitmap_size();
+        let prev_bitmap_size_log = libm::log2(prev_bitmap_size as f64);
+
+        let psmeta = state.metadata_mut::<SchedulerMetadata>()?;
+
+        psmeta.set_exec_time(psmeta.exec_time() - prev_total_time);
+        psmeta.set_cycles(psmeta.cycles() - (prev_cycles as u64));
+        psmeta.set_bitmap_size(psmeta.bitmap_size() - prev_bitmap_size);
+        psmeta.set_bitmap_size_log(psmeta.bitmap_size_log() - prev_bitmap_size_log);
+        psmeta.set_bitmap_entries(psmeta.bitmap_entries() - 1);
+
+        Ok(())
+    }
+
+    #[allow(clippy::cast_precision_loss)]
+    /// Adjusting metadata when replacing the corpus
+    fn on_replace_metadata(
+        &mut self,
+        state: &mut Self::State,
+        idx: CorpusId,
+        prev: &Testcase<<Self::State as UsesInput>::Input>,
+    ) -> Result<(), Error> {
+        let prev_meta = prev.metadata::<SchedulerTestcaseMetadata>()?;
+
+        // Next depth is + 1
+        let prev_depth = prev_meta.depth() + 1;
+
+        // Use these to adjust `SchedulerMetadata`
+        let (prev_total_time, prev_cycles) = prev_meta.cycle_and_time();
+        let prev_bitmap_size = prev_meta.bitmap_size();
+        let prev_bitmap_size_log = libm::log2(prev_bitmap_size as f64);
+
+        let psmeta = state.metadata_mut::<SchedulerMetadata>()?;
+
+        // We won't add new one because it'll get added when it gets executed in calirbation next time.
+        psmeta.set_exec_time(psmeta.exec_time() - prev_total_time);
+        psmeta.set_cycles(psmeta.cycles() - (prev_cycles as u64));
+        psmeta.set_bitmap_size(psmeta.bitmap_size() - prev_bitmap_size);
+        psmeta.set_bitmap_size_log(psmeta.bitmap_size_log() - prev_bitmap_size_log);
+        psmeta.set_bitmap_entries(psmeta.bitmap_entries() - 1);
+
+        state
+            .corpus()
+            .get(idx)?
+            .borrow_mut()
+            .add_metadata(SchedulerTestcaseMetadata::new(prev_depth));
+        Ok(())
+    }
+}
+
+/// Defines the common metadata operations for the AFL-style schedulers
+pub trait HasAFLSchedulerMetadata<O, S>: Scheduler
+where
+    Self::State: HasCorpus + HasMetadata + HasTestcase,
+    O: MapObserver,
+{
+    /// Return the last hash
+    fn last_hash(&self) -> usize;
+
+    /// Set the last hash
+    fn set_last_hash(&mut self, value: usize);
+
+    /// Get the observer map observer name
+    fn map_observer_name(&self) -> &String;
+
+    /// Called when a [`Testcase`] is added to the corpus
+    fn on_add_metadata(&self, state: &mut Self::State, idx: CorpusId) -> Result<(), Error> {
+        let current_idx = *state.corpus().current();
+
+        let mut depth = match current_idx {
+            Some(parent_idx) => state
+                .testcase(parent_idx)?
+                .metadata::<SchedulerTestcaseMetadata>()?
+                .depth(),
+            None => 0,
+        };
+
+        // TODO increase perf_score when finding new things like in AFL
+        // https://github.com/google/AFL/blob/master/afl-fuzz.c#L6547
+
+        // Attach a `SchedulerTestcaseMetadata` to the queue entry.
+        depth += 1;
+        let mut testcase = state.testcase_mut(idx)?;
+        testcase.add_metadata(SchedulerTestcaseMetadata::with_n_fuzz_entry(
+            depth,
+            self.last_hash(),
+        ));
+        testcase.set_parent_id_optional(current_idx);
+        Ok(())
+    }
+
+    /// Called when a [`Testcase`] is evaluated
+    fn on_evaluation_metadata<OT>(
+        &mut self,
+        state: &mut Self::State,
+        _input: &<Self::State as UsesInput>::Input,
+        observers: &OT,
+    ) -> Result<(), Error>
+    where
+        OT: ObserversTuple<Self::State>,
+    {
+        let observer = observers
+            .match_name::<O>(self.map_observer_name())
+            .ok_or_else(|| Error::key_not_found("MapObserver not found".to_string()))?;
+
+        let mut hash = observer.hash() as usize;
+
+        let psmeta = state.metadata_mut::<SchedulerMetadata>()?;
+
+        hash %= psmeta.n_fuzz().len();
+        // Update the path frequency
+        psmeta.n_fuzz_mut()[hash] = psmeta.n_fuzz()[hash].saturating_add(1);
+
+        self.set_last_hash(hash);
+
+        Ok(())
+    }
+
+    /// Called when choosing the next [`Testcase`]
+    fn on_next_metadata(
+        &mut self,
+        state: &mut Self::State,
+        _next_idx: Option<CorpusId>,
+    ) -> Result<(), Error> {
+        let current_idx = *state.corpus().current();
+
+        if let Some(idx) = current_idx {
+            let mut testcase = state.testcase_mut(idx)?;
+            let tcmeta = testcase.metadata_mut::<SchedulerTestcaseMetadata>()?;
+
+            if tcmeta.handicap() >= 4 {
+                tcmeta.set_handicap(tcmeta.handicap() - 4);
+            } else if tcmeta.handicap() > 0 {
+                tcmeta.set_handicap(tcmeta.handicap() - 1);
+            }
+        }
+
         Ok(())
     }
 }
