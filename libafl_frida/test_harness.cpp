10bf1e6419054c6c085715d1ad1cf925bf1e6306
Moshe Kravchik <mkravchik@hotmail.com>
2024-01-09 15:47:43 +0200

    clang-format

--- libafl_frida/test_harness.cpp
+++ libafl_frida/test_harness.cpp
@@ -30,28 +30,28 @@ extern "C" int heap_oob_write(const uint8_t *_data, size_t _size) {
   return 0;
 }
 extern "C" int malloc_heap_uaf_read(const uint8_t *_data, size_t _size) {
-  int *array = (int *)malloc(100 * sizeof(int));
+  int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   free(array);
   fprintf(stdout, "%d\n", array[5]);
   return 0;
 }
 
 extern "C" int malloc_heap_uaf_write(const uint8_t *_data, size_t _size) {
-  int *array = (int *)malloc(100 * sizeof(int));
+  int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   free(array);
   array[5] = 1;
   return 0;
 }
 
 extern "C" int malloc_heap_oob_read(const uint8_t *_data, size_t _size) {
-  int *array = (int *)malloc(100 * sizeof(int));
+  int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   fprintf(stdout, "%d\n", array[100]);
   free(array);
   return 0;
 }
 
 extern "C" int malloc_heap_oob_write(const uint8_t *_data, size_t _size) {
-  int *array = (int *)malloc(100 * sizeof(int));
+  int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   array[100] = 1;
   free(array);
   return 0;
