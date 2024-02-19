6976af0644bc8209423e0409956635061c62bb58
toka <tokazerkje@outlook.com>
2024-02-19 15:16:01 +0100

    ctx

--- libafl_cc/src/clang.rs
+++ libafl_cc/src/clang.rs
@@ -33,8 +33,6 @@ pub enum LLVMPasses {
     //CmpLogIns,
     /// The CmpLog pass
     CmpLogRtn,
-    /// The AFL coverage pass
-    AFLCoverage,
     /// The Autotoken pass
     AutoTokens,
     /// The Coverage Accouting (BB metric) pass
@@ -44,6 +42,8 @@ pub enum LLVMPasses {
     #[cfg(unix)]
     /// The CmpLog Instruction pass
     CmpLogInstructions,
+    /// Instrument caller for sancov coverage
+    Ctx,
 }
 
 impl LLVMPasses {
@@ -53,8 +53,6 @@ impl LLVMPasses {
         match self {
             LLVMPasses::CmpLogRtn => PathBuf::from(env!("OUT_DIR"))
                 .join(format!("cmplog-routines-pass.{}", dll_extension())),
-            LLVMPasses::AFLCoverage => PathBuf::from(env!("OUT_DIR"))
-                .join(format!("afl-coverage-pass.{}", dll_extension())),
             LLVMPasses::AutoTokens => {
                 PathBuf::from(env!("OUT_DIR")).join(format!("autotokens-pass.{}", dll_extension()))
             }
@@ -66,6 +64,8 @@ impl LLVMPasses {
             #[cfg(unix)]
             LLVMPasses::CmpLogInstructions => PathBuf::from(env!("OUT_DIR"))
                 .join(format!("cmplog-instructions-pass.{}", dll_extension())),
+            LLVMPasses::Ctx => PathBuf::from(env!("OUT_DIR"))
+                .join(format!("ctx-pass.{}", dll_extension())),
         }
     }
 }
