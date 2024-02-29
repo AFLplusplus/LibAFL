6b94389c9494390bd2371923c6b4850822654d3a
Dominik Maier <dmnk@google.com>
2024-02-29 03:39:25 +0100

    fix no_std

--- libafl_bolts/src/tuples.rs
+++ libafl_bolts/src/tuples.rs
@@ -9,7 +9,6 @@ use core::{
     ptr::{addr_of, addr_of_mut},
 };
 
-use alloc::vec::Vec;
 pub use tuple_list::{tuple_list, tuple_list_type, TupleList};
 
 #[cfg(any(feature = "xxh3", feature = "alloc"))]
