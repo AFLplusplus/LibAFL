606356dd6fb5d6647b5c5f660fb83bf2c48fb23a
Dominik Maier <dmnk@google.com>
2024-02-29 03:33:10 +0100

    Move into_vec to extra trait

--- libafl_bolts/src/tuples.rs
+++ libafl_bolts/src/tuples.rs
@@ -9,6 +9,7 @@ use core::{
     ptr::{addr_of, addr_of_mut},
 };
 
+use alloc::vec::Vec;
 pub use tuple_list::{tuple_list, tuple_list_type, TupleList};
 
 #[cfg(any(feature = "xxh3", feature = "alloc"))]
