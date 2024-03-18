a0143d02a61c728cf2d2b30cf30fe045418f0458
Dominik Maier <domenukk@gmail.com>
2024-03-18 08:46:48 +0100

    Rename OSError -> OsError and merge with redundant Error::File (#1944)
    * OSError -> OsError
    
    * Move File errors to io Errors
    
    * Removing File errors
    
    * fixes :)
    
    * More format
    
    * fix libfuzzer runtime

--- libafl_libfuzzer/libafl_libfuzzer_runtime/src/corpus.rs
+++ libafl_libfuzzer/libafl_libfuzzer_runtime/src/corpus.rs
@@ -114,7 +114,7 @@ where
                 let path = self.corpus_dir.join(&name);
 
                 match input.to_file(&path) {
-                    Err(Error::File(e, _)) if e.kind() == ErrorKind::AlreadyExists => {
+                    Err(Error::OsError(e, ..)) if e.kind() == ErrorKind::AlreadyExists => {
                         // we do not care if the file already exists; in this case, we assume it is equal
                     }
                     res => res?,
@@ -192,7 +192,7 @@ where
             Error::empty("The testcase, when being saved, must have a file path!")
         })?;
         match input.to_file(path) {
-            Err(Error::File(e, _)) if e.kind() == ErrorKind::AlreadyExists => {
+            Err(Error::OsError(e, ..)) if e.kind() == ErrorKind::AlreadyExists => {
                 // we do not care if the file already exists; in this case, we assume it is equal
                 Ok(())
             }
@@ -250,7 +250,7 @@ where
             Error::illegal_state("Should have set the path in the LibfuzzerCrashCauseFeedback.")
         })?;
         match input.to_file(path) {
-            Err(Error::File(e, _)) if e.kind() == ErrorKind::AlreadyExists => {
+            Err(Error::OsError(e, ..)) if e.kind() == ErrorKind::AlreadyExists => {
                 // we do not care if the file already exists; in this case, we assume it is equal
             }
             res => res?,
