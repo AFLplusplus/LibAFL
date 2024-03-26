6e237551ea2f897eda841fde98955a758f016506
Toka <tokazerkje@outlook.com>
2024-03-26 19:30:15 +0100

    not ture

--- fuzzers/libfuzzer_libpng_accounting/src/lib.rs
+++ fuzzers/libfuzzer_libpng_accounting/src/lib.rs
@@ -75,7 +75,7 @@ struct Opt {
     #[arg(short = 'a', long, help = "Specify a remote broker", name = "REMOTE")]
     remote_broker_addr: Option<SocketAddr>,
 
-    #[arg(short, long, help = "Set an initial corpus directory", name = "INPUT", required = ture)]
+    #[arg(short, long, help = "Set an initial corpus directory", name = "INPUT", required = true)]
     input: Vec<PathBuf>,
 
     #[arg(
