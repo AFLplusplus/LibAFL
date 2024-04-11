8f97f2a9180626190579ab3072ae061a9d45ee1f
l4yton <l4yton0@protonmail.com>
2024-04-11 12:35:04 +0200

    update example fuzzers

--- fuzzers/nyx_libxml2_standalone/src/main.rs
+++ fuzzers/nyx_libxml2_standalone/src/main.rs
@@ -21,7 +21,7 @@ use libafl_nyx::{executor::NyxExecutor, helper::NyxHelper, settings::NyxSettings
 
 fn main() {
     // nyx stuff
-    let settings = NyxSettings::builder().cpu_id(0).parent_cpu_id(None).build();
+    let settings = NyxSettings::builder().cpu_id(0).build();
     let helper = NyxHelper::new("/tmp/nyx_libxml2/", settings).unwrap();
     let observer =
         unsafe { StdMapObserver::from_mut_ptr("trace", helper.bitmap_buffer, helper.bitmap_size) };
