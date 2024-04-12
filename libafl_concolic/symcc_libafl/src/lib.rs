f9fb9160b3c3aec5e27f8335685612f420c3eac0
Dongjia "toka" Zhang <tokazerkje@outlook.com>
2024-04-12 23:39:29 +0200

    symcc upd

--- libafl_concolic/symcc_libafl/src/lib.rs
+++ libafl_concolic/symcc_libafl/src/lib.rs
@@ -5,7 +5,7 @@
 /// The URL of the `LibAFL` `SymCC` fork.
 pub const SYMCC_REPO_URL: &str = "https://github.com/AFLplusplus/symcc.git";
 /// The commit of the `LibAFL` `SymCC` fork.
-pub const SYMCC_REPO_COMMIT: &str = "6010402596f02da6de1c2dc88794f339d7c4dfe7";
+pub const SYMCC_REPO_COMMIT: &str = "f33f6790400f5e081682966d1d6ff5ac4f8ae49e";
 
 #[cfg(feature = "clone")]
 mod clone {
