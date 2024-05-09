211ef34ca3eed7a258c3313b72c16508803b142a
s1341 <github@shmarya.net>
2024-05-09 17:57:30 +0300

    fmt

--- libafl_frida/src/asan/hook_funcs.rs
+++ libafl_frida/src/asan/hook_funcs.rs
@@ -815,11 +815,18 @@ impl AsanRuntime {
         _handle: *const c_void,
         _desired_access: u32,
         _file_offset_high: u32,
-        _file_offset_low: u32,       
+        _file_offset_low: u32,
         size: usize,
     ) -> *const c_void {
-        let original: extern "C" fn(*const c_void, u32, u32, u32, usize) -> *const c_void = unsafe { std::mem::transmute(self.hooks.get(&"MapViewOfFile".to_string()).unwrap().0)};
-        let ret = (original)(_handle, _desired_access, _file_offset_high, _file_offset_low, size);
+        let original: extern "C" fn(*const c_void, u32, u32, u32, usize) -> *const c_void =
+            unsafe { std::mem::transmute(self.hooks.get(&"MapViewOfFile".to_string()).unwrap().0) };
+        let ret = (original)(
+            _handle,
+            _desired_access,
+            _file_offset_high,
+            _file_offset_low,
+            size,
+        );
         self.unpoison(ret as usize, size);
         ret
     }
