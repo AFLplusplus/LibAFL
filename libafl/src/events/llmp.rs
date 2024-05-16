759540a011776b02aadf3d1b69bcae00aa01a903
Toka <tokazerkje@outlook.com>
2024-05-16 13:40:58 +0200

    fix

--- libafl/src/events/llmp.rs
+++ libafl/src/events/llmp.rs
@@ -1556,6 +1556,7 @@ where
                             handle.status()
                         }
                         ForkResult::Child => {
+                            println!("AAA!");
                             self.shmem_provider.post_fork(true)?;
                             break (staterestorer, self.shmem_provider.clone(), core_id);
                         }
@@ -1607,16 +1608,6 @@ where
                 ctr = ctr.wrapping_add(1);
             }
         } else {
-            // At this point we are the fuzzer *NOT* the restarter.
-            // We setup signal handlers to clean up shmem segments used by state restorer
-            #[cfg(all(unix, not(miri)))]
-            if let Err(_e) =
-                unsafe { setup_signal_handler(addr_of_mut!(EVENTMGR_SIGHANDLER_STATE)) }
-            {
-                // We can live without a proper ctrl+c signal handler. Print and ignore.
-                log::error!("Failed to setup signal handlers: {_e}");
-            }
-
             // We are the newly started fuzzing instance (i.e. on Windows), first, connect to our own restore map.
             // We get here *only on Windows*, if we were started by a restarting fuzzer.
             // A staterestorer and a receiver for single communication
@@ -1626,6 +1617,16 @@ where
                 None,
             )
         };
+
+        // At this point we are the fuzzer *NOT* the restarter.
+        // We setup signal handlers to clean up shmem segments used by state restorer
+        #[cfg(all(unix, not(miri)))]
+        if let Err(_e) =
+            unsafe { setup_signal_handler }
+        {
+            // We can live without a proper ctrl+c signal handler. Print and ignore.
+            log::error!("Failed to setup signal handlers: {_e}");
+        }
 
         if let Some(core_id) = core_id {
             let core_id: CoreId = core_id;
