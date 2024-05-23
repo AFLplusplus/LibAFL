c2b8921e56f8994d7f53f4e69ca440f0e45ea2cc
Dominik Maier <dmnk@google.com>
2024-05-23 13:00:08 +0200

    Rename peek_next_free_id to peek_free_id (that it's the next is obvious)

--- libafl/src/corpus/ondisk.rs
+++ libafl/src/corpus/ondisk.rs
@@ -108,8 +108,8 @@ where
 
     /// Peek the next free corpus id
     #[inline]
-    fn peek_next_free_id(&self) -> CorpusId {
-        self.inner.peek_next_free_id()
+    fn peek_free_id(&self) -> CorpusId {
+        self.inner.peek_free_id()
     }
 
     /// Removes an entry from the corpus, returning it if it was present.
