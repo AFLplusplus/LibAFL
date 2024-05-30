e912216a37b36266fba5ed60f16a52669ff3bb8f
Dongjia "toka" Zhang <tokazerkje@outlook.com>
2024-05-30 11:53:32 +0200

    Add remove_from_all method to `Corpus` trait (#2259)
    * add remove_from_all
    
    * a
    
    * a
    
    * aaaaaaa
    
    * make remove_from_all to remove

--- libafl/src/corpus/inmemory_ondisk.rs
+++ libafl/src/corpus/inmemory_ondisk.rs
@@ -112,7 +112,7 @@ where
         Ok(entry)
     }
 
-    /// Removes an entry from the corpus, returning it if it was present.
+    /// Removes an entry from the corpus, returning it if it was present; considers both enabled and disabled corpus
     #[inline]
     fn remove(&mut self, idx: CorpusId) -> Result<Testcase<I>, Error> {
         let entry = self.inner.remove(idx)?;
