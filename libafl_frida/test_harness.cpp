03d8d2eb08b5112d5476949a06b90b13849d738e
mkravchik <mkravchik@hotmail.com>
2024-06-11 04:22:46 -0700

    Fixing Frida ASAN tests on Windows (#2299)
    * libafl_frida unit tests passing with ASAN
    
    * Clippy+fmt
    
    * Clippy
    
    * Setup VS environment before building

--- libafl_frida/test_harness.cpp
+++ libafl_frida/test_harness.cpp
@@ -7,16 +7,21 @@
 
 BOOL APIENTRY DllMain(HANDLE hModule, DWORD ul_reason_for_call,
                       LPVOID lpReserved) {
+  (void)hModule;
+  (void)lpReserved;
+  (void)ul_reason_for_call;
   return TRUE;
 }
 
-  #define EXTERN __declspec(dllexport) extern "C"
+  #define EXTERN extern "C" __declspec(dllexport)  
 #else
   #define EXTERN
 extern "C" {
 #endif
 
 EXTERN int heap_uaf_read(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = new int[100];
   delete[] array;
   fprintf(stdout, "%d\n", array[5]);
@@ -24,13 +29,26 @@ EXTERN int heap_uaf_read(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int heap_uaf_write(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = new int[100];
   delete[] array;
   array[5] = 1;
   return 0;
 }
 
+#include <winnt.h>
+#include <winternl.h>
+
+static volatile bool stop = false;
+
 EXTERN int heap_oob_read(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
+
+  // while(!stop);
+
+  // OutputDebugStringA("heap_oob_read\n");
   int *array = new int[100];
   fprintf(stdout, "%d\n", array[100]);
   delete[] array;
@@ -38,12 +56,16 @@ EXTERN int heap_oob_read(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int heap_oob_write(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = new int[100];
   array[100] = 1;
   delete[] array;
   return 0;
 }
 EXTERN int malloc_heap_uaf_read(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   free(array);
   fprintf(stdout, "%d\n", array[5]);
@@ -51,6 +73,8 @@ EXTERN int malloc_heap_uaf_read(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_uaf_write(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   free(array);
   array[5] = 1;
@@ -58,6 +82,8 @@ EXTERN int malloc_heap_uaf_write(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_oob_read(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   fprintf(stdout, "%d\n", array[100]);
   free(array);
@@ -65,6 +91,8 @@ EXTERN int malloc_heap_oob_read(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_oob_write(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   int *array = static_cast<int *>(malloc(100 * sizeof(int)));
   array[100] = 1;
   free(array);
@@ -72,6 +100,8 @@ EXTERN int malloc_heap_oob_write(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_oob_write_0x12(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x12));
   array[0x12] = 1;
   free(array);
@@ -79,6 +109,8 @@ EXTERN int malloc_heap_oob_write_0x12(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_oob_write_0x14(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x14));
   array[0x14] = 1;
   free(array);
@@ -86,6 +118,8 @@ EXTERN int malloc_heap_oob_write_0x14(const uint8_t *_data, size_t _size) {
 }
 
 EXTERN int malloc_heap_oob_write_0x17(const uint8_t *_data, size_t _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x17));
   array[0x17] = 1;
   free(array);
@@ -94,6 +128,8 @@ EXTERN int malloc_heap_oob_write_0x17(const uint8_t *_data, size_t _size) {
 
 EXTERN int malloc_heap_oob_write_0x17_int_at_0x16(const uint8_t *_data,
                                                   size_t         _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x17));
   *(int *)(&array[0x16]) = 1;
   free(array);
@@ -102,6 +138,8 @@ EXTERN int malloc_heap_oob_write_0x17_int_at_0x16(const uint8_t *_data,
 
 EXTERN int malloc_heap_oob_write_0x17_int_at_0x15(const uint8_t *_data,
                                                   size_t         _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x17));
   *(int *)(&array[0x15]) = 1;
   free(array);
@@ -109,6 +147,8 @@ EXTERN int malloc_heap_oob_write_0x17_int_at_0x15(const uint8_t *_data,
 }
 EXTERN int malloc_heap_oob_write_0x17_int_at_0x14(const uint8_t *_data,
                                                   size_t         _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x17));
   *(int *)(&array[0x14]) = 1;
   free(array);
@@ -117,6 +157,8 @@ EXTERN int malloc_heap_oob_write_0x17_int_at_0x14(const uint8_t *_data,
 
 EXTERN int malloc_heap_oob_write_0x17_int_at_0x13(const uint8_t *_data,
                                                   size_t         _size) {
+  (void)_data;
+  (void)_size;
   char *array = static_cast<char *>(malloc(0x17));
   *(int *)(&array[0x13]) = 1;
   free(array);
@@ -125,6 +167,8 @@ EXTERN int malloc_heap_oob_write_0x17_int_at_0x13(const uint8_t *_data,
 
 EXTERN int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
   // abort();
+  (void)data;
+  (void)size;
   return 0;
 }
 
