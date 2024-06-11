8f7ef17791c06019c937ba411c65310529b0a29b
Toka <tokazerkje@outlook.com>
2024-06-11 17:26:48 +0200

    bb

--- libafl_bolts/src/shmem.rs
+++ libafl_bolts/src/shmem.rs
@@ -645,7 +645,7 @@ pub mod unix_shmem {
     #[cfg(all(unix, feature = "std", not(target_os = "android")))]
     mod default {
         #[cfg(target_vendor = "apple")]
-        use alloc::str::ToString;
+        use alloc::string::ToString;
         use core::{
             ffi::CStr,
             ops::{Deref, DerefMut},
