9a70ef5321c3f1c0bed8d9ddc273da8a06010cf5
Dominik Maier <dmnk@google.com>
2024-07-12 15:14:59 +0200

    Rename all-caps TOML and JSON to Toml and Json in monitors

--- libafl/src/monitors/mod.rs
+++ libafl/src/monitors/mod.rs
@@ -19,7 +19,7 @@ use alloc::{borrow::Cow, fmt::Debug, string::String, vec::Vec};
 use core::{fmt, fmt::Write, time::Duration};
 
 #[cfg(feature = "std")]
-pub use disk::{OnDiskJSONMonitor, OnDiskTOMLMonitor};
+pub use disk::{OnDiskJSONMonitor, OnDiskTomlMonitor};
 use hashbrown::HashMap;
 use libafl_bolts::{current_time, format_duration_hms, ClientId};
 use serde::{Deserialize, Serialize};
