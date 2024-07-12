79cbc16800af2ac87fd205eac3676339f3d45f48
Dominik Maier <domenukk@gmail.com>
2024-07-12 17:42:07 +0200

    Replace uses std HashMap with hashbrown for consistency (#2387)
    * Replace uses std HashMap with hashbrown for consistency
    
    * fix
    
    * more fix
    
    * Hashbrownify libfuzzer_runtime
    
    * more

--- libafl_qemu/src/helpers/asan.rs
+++ libafl_qemu/src/helpers/asan.rs
@@ -1,13 +1,8 @@
 #![allow(clippy::cast_possible_wrap)]
 
-use std::{
-    borrow::Cow,
-    collections::{HashMap, HashSet},
-    env, fs,
-    path::PathBuf,
-    sync::Mutex,
-};
+use std::{borrow::Cow, env, fs, path::PathBuf, sync::Mutex};
 
+use hashbrown::{HashMap, HashSet};
 use libafl::{executors::ExitKind, inputs::UsesInput, observers::ObserversTuple, HasMetadata};
 use libc::{
     c_void, MAP_ANON, MAP_FAILED, MAP_FIXED, MAP_NORESERVE, MAP_PRIVATE, PROT_READ, PROT_WRITE,
