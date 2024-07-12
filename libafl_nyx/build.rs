1b3192444691a1651109369944e1c69ac4fa5c92
Toka <tokazerkje@outlook.com>
2024-07-12 15:46:23 +0200

    f

--- libafl_nyx/build.rs
+++ libafl_nyx/build.rs
@@ -7,10 +7,8 @@ fn main() {
         let status = Command::new("./build_nyx_support.sh")
             .status()
             .expect("can't run ./build_nyx_support.sh");
-        if status.success() {
-            println!("success to run ./build_nyx_support.sh");
-        } else {
-            panic!("fail to run ./build_nyx_support.sh");
+        if !status.success() {
+            println!("cargo:warning=fail to run ./build_nyx_support.sh");
         }
     } else {
         println!("cargo:warning=NYX node is only avaliable on x64 Linux");
