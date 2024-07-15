bacb124bdd144b6f19264f645299672428659fe2
Addison Crump <addison.crump@cispa.de>
2024-07-15 16:35:43 +0200

    template out libafl_libfuzzer

--- libafl_libfuzzer/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-mv libafl_libfuzzer_runtime/Cargo.toml libafl_libfuzzer_runtime/Cargo.toml.orig
-cargo publish --allow-dirty --no-verify "$@"
-mv libafl_libfuzzer_runtime/Cargo.toml.orig libafl_libfuzzer_runtime/Cargo.toml
