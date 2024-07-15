191771488304af78ad3301c75d8a540405c386c0
Toka <tokazerkje@outlook.com>
2024-07-15 17:14:35 +0200

    aaa

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,69 +1,79 @@
 #!/bin/bash
 
-# Check if an argument is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <comma-separated list of directories>"
-    exit 1
-fi
-
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 cd "$SCRIPT_DIR/.." || exit 1
 
 # Function to run Clippy on a single directory
 run_clippy() {
-    local dir="$1"
-    echo "Running Clippy on $dir"
-    pushd "$dir" || return 1
+   local dir="$1"
+   echo "Running Clippy on $dir"
+   pushd "$dir" || return 1
 
-    RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests --examples --benches -- -Z macro-backtrace \
-       -D clippy::all \
-       -D clippy::pedantic \
-       -W clippy::similar_names \
-       -A clippy::type_repetition_in_bounds \
-       -A clippy::missing-errors-doc \
-       -A clippy::cast-possible-truncation \
-       -A clippy::used-underscore-binding \
-       -A clippy::ptr-as-ptr \
-       -A clippy::missing-panics-doc \
-       -A clippy::missing-docs-in-private-items \
-       -A clippy::unseparated-literal-suffix \
-       -A clippy::module-name-repetitions \
-       -A clippy::unreadable-literal
+   RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests --examples --benches -- -Z macro-backtrace \
+      -D clippy::all \
+      -D clippy::pedantic \
+      -W clippy::similar_names \
+      -A clippy::type_repetition_in_bounds \
+      -A clippy::missing-errors-doc \
+      -A clippy::cast-possible-truncation \
+      -A clippy::used-underscore-binding \
+      -A clippy::ptr-as-ptr \
+      -A clippy::missing-panics-doc \
+      -A clippy::missing-docs-in-private-items \
+      -A clippy::unseparated-literal-suffix \
+      -A clippy::module-name-repetitions \
+      -A clippy::unreadable-literal
 
-    # Check if we're on Linux and if libafl_libfuzzer_runtime exists
-    if [[ "$OSTYPE" == "linux-gnu"* ]] && [ -d "libafl_libfuzzer/libafl_libfuzzer_runtime" ]; then
-        echo "Running Clippy on libafl_libfuzzer_runtime"
-        cd libafl_libfuzzer/libafl_libfuzzer_runtime || return 1
-        RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests --examples --benches -- -Z macro-backtrace \
-           -D clippy::all \
-           -D clippy::pedantic \
-           -W clippy::similar_names \
-           -A clippy::type_repetition_in_bounds \
-           -A clippy::missing-errors-doc \
-           -A clippy::cast-possible-truncation \
-           -A clippy::used-underscore-binding \
-           -A clippy::ptr-as-ptr \
-           -A clippy::missing-panics-doc \
-           -A clippy::missing-docs-in-private-items \
-           -A clippy::unseparated-literal-suffix \
-           -A clippy::module-name-repetitions \
-           -A clippy::unreadable-literal
-    fi
-
-    popd || return 1
+   popd || return 1
 }
 
-# Split the input string into an array
-IFS=',' read -ra PROJECTS <<< "$1"
+# Define projects based on the operating system
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+   ALL_PROJECTS=(
+      "libafl_concolic/symcc_runtime"
+      "libafl_concolic/symcc_libafl"
+      "libafl_frida"
+      "libafl_libfuzzer"
+      "libafl_nyx"
+      "libafl_qemu"
+      "libafl_tinyinst"
+      "libafl_qemu/libafl_qemu_build"
+      "libafl_qemu/libafl_qemu_sys"
+   )
+fi
+
+if [ "$#" -eq 0 ]; then
+   # No arguments provided, run on all projects
+   PROJECTS=("${ALL_PROJECTS[@]}")
+else
+   # Arguments provided, split the input string into an array
+   IFS=',' read -ra PROJECTS <<<"$1"
+fi
+
+# First run it on all
+RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests --examples --benches -- -Z macro-backtrace \
+   -D clippy::all \
+   -D clippy::pedantic \
+   -W clippy::similar_names \
+   -A clippy::type_repetition_in_bounds \
+   -A clippy::missing-errors-doc \
+   -A clippy::cast-possible-truncation \
+   -A clippy::used-underscore-binding \
+   -A clippy::ptr-as-ptr \
+   -A clippy::missing-panics-doc \
+   -A clippy::missing-docs-in-private-items \
+   -A clippy::unseparated-literal-suffix \
+   -A clippy::module-name-repetitions \
+   -A clippy::unreadable-literal
 
 # Loop through each project and run Clippy
 for project in "${PROJECTS[@]}"; do
    # Trim leading and trailing whitespace
    project=$(echo "$project" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
    if [ -d "$project" ]; then
-       run_clippy "$project"
+      run_clippy "$project"
    else
-       echo "Warning: Directory $project does not exist. Skipping."
+      echo "Warning: Directory $project does not exist. Skipping."
    fi
 done
 
