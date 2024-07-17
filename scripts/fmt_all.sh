2966a5052fe7f408c60b3d1492757034ddecb3b1
Romain Malmain <romain.malmain@pm.me>
2024-07-17 15:11:57 +0200

    detect fmt errors in CI

--- scripts/fmt_all.sh
+++ scripts/fmt_all.sh
@@ -4,9 +4,9 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LIBAFL_DIR=$(realpath "$SCRIPT_DIR/..")
 
 if [ "$1" = "check" ]; then
-  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- -c --verbose
+  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- -c --verbose || exit 1
 else
-  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- --verbose
+  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- --verbose || exit 1
 fi
 
 if command -v black > /dev/null; then
