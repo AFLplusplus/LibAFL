1763d21ac8372eed4f8d1cd38a5d74e568314f93
Dominik Maier <dmnk@google.com>
2024-07-25 16:29:34 +0000

    Cmplog bytes without alloc

--- libafl_targets/src/cmps/mod.rs
+++ libafl_targets/src/cmps/mod.rs
@@ -13,7 +13,7 @@ use core::{
 };
 
 use libafl::{
-    observers::{cmp::AFLppCmpLogHeader, CmpMap, CmpValues},
+    observers::{cmp::AFLppCmpLogHeader, CmpMap, CmpValues, CmplogBytes},
     Error,
 };
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
@@ -567,8 +567,8 @@ impl CmpMap for AFLppCmpLogMap {
                 let v0_len = self.vals.fn_operands[idx][execution].v0_len & (0x80 - 1);
                 let v1_len = self.vals.fn_operands[idx][execution].v1_len & (0x80 - 1);
                 Some(CmpValues::Bytes((
-                    self.vals.fn_operands[idx][execution].v0[..(v0_len as usize)].to_vec(),
-                    self.vals.fn_operands[idx][execution].v1[..(v1_len as usize)].to_vec(),
+                    CmplogBytes::from_buf_and_len(self.vals.fn_operands[idx][execution].v0, v0_len),
+                    CmplogBytes::from_buf_and_len(self.vals.fn_operands[idx][execution].v1, v1_len),
                 )))
             }
         }
