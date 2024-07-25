f3d2a7e0a2523d450b05ca00a641a48d11e90da0
Addison Crump <addison.crump@cispa.de>
2024-07-25 16:03:38 +0200

    simplify combined

--- libafl/src/executors/combined.rs
+++ libafl/src/executors/combined.rs
@@ -1,14 +1,9 @@
 //! A `CombinedExecutor` wraps a primary executor and a secondary one
 //! In comparison to the [`crate::executors::DiffExecutor`] it does not run the secondary executor in `run_target`.
 
-use core::fmt::Debug;
-
-use libafl_bolts::tuples::RefIndexable;
-
-use crate::{
-    executors::{Executor, ExitKind, HasObservers},
-    state::HasExecutions,
-    Error,
+use core::{
+    fmt::Debug,
+    ops::{Deref, DerefMut},
 };
 
 /// A [`CombinedExecutor`] wraps a primary executor, forwarding its methods, and a secondary one
@@ -20,71 +15,41 @@ pub struct CombinedExecutor<A, B> {
 
 impl<A, B> CombinedExecutor<A, B> {
     /// Create a new `CombinedExecutor`, wrapping the given `executor`s.
-    pub fn new<EM, Z>(primary: A, secondary: B) -> Self
-    where
-        A: Executor<EM, Z>,
-        B: Executor<EM, Z, State = <Self as UsesState>::State>,
-        EM: UsesState<State = <Self as UsesState>::State>,
-        Z: UsesState<State = <Self as UsesState>::State>,
-    {
+    pub fn new(primary: A, secondary: B) -> Self {
         Self { primary, secondary }
     }
 
     /// Retrieve the primary `Executor` that is wrapped by this `CombinedExecutor`.
-    pub fn primary(&mut self) -> &mut A {
-        &mut self.primary
+    pub fn primary(&self) -> &A {
+        &self.primary
     }
 
     /// Retrieve the secondary `Executor` that is wrapped by this `CombinedExecutor`.
-    pub fn secondary(&mut self) -> &mut B {
+    pub fn secondary(&self) -> &B {
+        &self.secondary
+    }
+
+    /// Retrieve, mutably, the primary `Executor` that is wrapped by this `CombinedExecutor`.
+    pub fn primary_mut(&mut self) -> &mut A {
+        &mut self.primary
+    }
+
+    /// Retrieve, mutably, the secondary `Executor` that is wrapped by this `CombinedExecutor`.
+    pub fn secondary_mut(&mut self) -> &mut B {
         &mut self.secondary
     }
 }
 
-impl<A, B, EM, Z> Executor<EM, Z> for CombinedExecutor<A, B>
-where
-    A: Executor<EM, Z>,
-    B: Executor<EM, Z, State = <Self as UsesState>::State>,
-    Self::State: HasExecutions,
-    EM: UsesState<State = <Self as UsesState>::State>,
-    Z: UsesState<State = <Self as UsesState>::State>,
-{
-    fn run_target(
-        &mut self,
-        fuzzer: &mut Z,
-        state: &mut Self::State,
-        mgr: &mut EM,
-        input: &Self::Input,
-    ) -> Result<ExitKind, Error> {
-        self.primary.run_target(fuzzer, state, mgr, input)
+impl<A, B> Deref for CombinedExecutor<A, B> {
+    type Target = A;
+
+    fn deref(&self) -> &Self::Target {
+        self.primary()
     }
 }
 
-impl<A, B> UsesState for CombinedExecutor<A, B>
-where
-    A: UsesState,
-{
-    type State = A::State;
-}
-
-impl<A, B> UsesObservers for CombinedExecutor<A, B>
-where
-    A: UsesObservers,
-{
-    type Observers = A::Observers;
-}
-
-impl<A, B> HasObservers for CombinedExecutor<A, B>
-where
-    A: HasObservers,
-{
-    #[inline]
-    fn observers(&self) -> RefIndexable<&Self::Observers, Self::Observers> {
-        self.primary.observers()
-    }
-
-    #[inline]
-    fn observers_mut(&mut self) -> RefIndexable<&mut Self::Observers, Self::Observers> {
-        self.primary.observers_mut()
+impl<A, B> DerefMut for CombinedExecutor<A, B> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.primary_mut()
     }
 }
