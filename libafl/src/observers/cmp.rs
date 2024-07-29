19f73ed11523d927292c47a2e1a2a22e82bd4e84
Dominik Maier <dmnk@google.com>
2024-07-29 15:54:07 +0000

    clippy

--- libafl/src/observers/cmp.rs
+++ libafl/src/observers/cmp.rs
@@ -45,6 +45,7 @@ pub struct CmplogBytes {
 impl CmplogBytes {
     /// Creates a new [`CmplogBytes`] object from the provided buf and length.
     /// Lengths above 32 are illegal but will be ignored.
+    #[must_use]
     pub fn from_buf_and_len(buf: [u8; 32], len: u8) -> Self {
         debug_assert!(len <= 32, "Len too big: {len}, max: 32");
         CmplogBytes { buf, len: len % 32 }
