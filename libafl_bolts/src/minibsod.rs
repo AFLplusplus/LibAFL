291fdeb85ed43be7ed0806f29f7bfb2029dc98a1
David CARLIER <devnexen@gmail.com>
2024-08-01 07:14:09 +0100

    bolts: fix openbsd build (#2467)
    Co-authored-by: dcarlier <dcarlier@openbsdvbox.chello.ie>

--- libafl_bolts/src/minibsod.rs
+++ libafl_bolts/src/minibsod.rs
@@ -1,7 +1,7 @@
 //! Implements a mini-bsod generator.
 //! It dumps all important registers and prints a stacktrace.
 
-#[cfg(target_vendor = "apple")]
+#[cfg(any(target_vendor = "apple", target_os = "openbsd"))]
 use core::mem::size_of;
 use std::io::{BufWriter, Write};
 #[cfg(any(target_os = "solaris", target_os = "illumos"))]
