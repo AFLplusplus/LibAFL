0dbb0127ca1e020582872f0fc91fa7fc186971f5
Marcondiro <cavenati.marco@gmail.com>
2024-08-05 12:41:55 +0000

    use perf-event-open-sys instead of bindgen

--- libafl_qemu/src/qemu/systemmode/intel_pt.rs
+++ libafl_qemu/src/qemu/systemmode/intel_pt.rs
@@ -1,63 +1,34 @@
-mod perf_events {
-    #[allow(non_upper_case_globals)]
-    #[allow(non_camel_case_types)]
-    #[allow(non_snake_case)]
-    #[allow(unused)]
-    pub mod bindings {
-        include!("perf_event_bindings.rs");
-    }
-    use std::fs;
-
-    pub use bindings::*;
-    use bitflags::bitflags;
-
-    const PT_EVENT_TYPE_PATH: &str = "/sys/bus/event_source/devices/intel_pt/type";
-
-    bitflags! {
-        /// IA32_RTIT_CTL MSR flags
-        #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-        struct PtConfig: u64 {
-            /// Disable call return address compression.
-            /// AKA DisRETC in Intel SDM
-            const NORETCOMP = 0b1 << 11;
-        }
-    }
-
-    impl perf_event_attr {
-        pub fn new_intel_pt() -> Result<Self, ()> {
-            let mut attr: perf_event_attr;
-            unsafe {
-                attr = core::mem::zeroed();
-            }
-            attr.size = core::mem::size_of::<perf_event_attr>() as u32;
-            attr.type_ = intel_pt_perf_type()?;
-            attr.set_disabled(1);
-            attr.config |= PtConfig::NORETCOMP.bits();
-
-            Ok(attr)
-        }
-    }
-
-    fn intel_pt_perf_type() -> Result<u32, ()> {
-        let s = fs::read_to_string(PT_EVENT_TYPE_PATH).map_err(|_| ())?;
-        s.parse::<u32>().map_err(|_| ())
-        // TODO better Err()
-    }
-}
-
-use std::os::{
-    fd::{AsRawFd, FromRawFd, OwnedFd},
-    raw::c_void,
+use std::{
+    fs,
+    os::{
+        fd::{AsRawFd, FromRawFd, OwnedFd},
+        raw::c_void,
+    },
 };
 
-use libc::{ioctl, syscall};
-use perf_events::{perf_event_attr, perf_event_mmap_page, PERF_FLAG_FD_CLOEXEC};
-use syscall_numbers::x86_64::SYS_perf_event_open;
+use bitflags::bitflags;
+use libc::ioctl;
+use perf_event_open_sys::{
+    bindings::{perf_event_attr, perf_event_mmap_page, PERF_FLAG_FD_CLOEXEC},
+    perf_event_open,
+};
 
 const PAGE_SIZE: usize = 4096;
 const PERF_BUFFER_SIZE: usize = ((1 + (1 << 7)) * PAGE_SIZE) as usize;
 const PERF_AUX_BUFFER_SIZE: usize = 64 * 1024 * 1024;
+const PT_EVENT_TYPE_PATH: &str = "/sys/bus/event_source/devices/intel_pt/type";
 
+bitflags! {
+    /// IA32_RTIT_CTL MSR flags
+    #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+    struct PtConfig: u64 {
+        /// Disable call return address compression.
+        /// AKA DisRETC in Intel SDM
+        const NORETCOMP = 0b1 << 11;
+    }
+}
+
+// TODO consider moving to static assert crate
 const _: () = assert!(
     ((PERF_BUFFER_SIZE - PAGE_SIZE) & (PERF_BUFFER_SIZE - PAGE_SIZE - 1)) == 0,
     "PERF_BUFFER_SIZE should be 1+2^n pages"
@@ -79,7 +50,6 @@ const _: () = assert!(
 // TODO use libaflerr instead of () for Result
 
 pub struct IntelPT {
-    perf_event_attr: perf_event_attr,
     fd: OwnedFd,
     // TODO use proper types
     perf_buffer: *mut c_void,
@@ -89,8 +59,17 @@ pub struct IntelPT {
 impl IntelPT {
     pub fn new() -> Self {
         // TODO change the unwrap
-        let perf_event_attr = perf_event_attr::new_intel_pt().unwrap();
-        let fd = perf_event_open(&perf_event_attr, 0).unwrap();
+        let mut perf_event_attr = new_perf_event_attr_intel_pt().unwrap();
+        let pid = 0; // TODO pid
+        let fd = unsafe {
+            OwnedFd::from_raw_fd(perf_event_open(
+                &mut perf_event_attr as *mut _,
+                pid,
+                -1,
+                -1,
+                PERF_FLAG_FD_CLOEXEC as u64,
+            ))
+        };
         let perf_buffer = setup_perf_buffer(&fd).unwrap();
 
         // the first page is a metadata page
@@ -103,7 +82,6 @@ impl IntelPT {
         let perf_aux_buffer = setup_perf_aux_buffer(&fd, buff_metadata).unwrap();
 
         Self {
-            perf_event_attr,
             fd,
             perf_buffer,
             perf_aux_buffer,
@@ -129,26 +107,6 @@ impl IntelPT {
 #[inline]
 const fn next_page_aligned_addr(address: usize) -> usize {
     (address + PAGE_SIZE - 1) & !(PAGE_SIZE - 1)
-}
-
-#[inline]
-fn perf_event_open(attr: &perf_event_attr, pid: u32) -> Result<OwnedFd, &'static str> {
-    unsafe {
-        let res = syscall(
-            SYS_perf_event_open,
-            attr as *const _,
-            pid,
-            -1,
-            -1,
-            PERF_FLAG_FD_CLOEXEC,
-        );
-        if res == -1 {
-            Err("IntelPT: Failed to open perf event")
-            // TODO read errno and better Err()
-        } else {
-            Ok(OwnedFd::from_raw_fd(res as i32))
-        }
-    }
 }
 
 fn setup_perf_buffer(fd: &OwnedFd) -> Result<*mut c_void, &'static str> {
@@ -189,4 +147,23 @@ fn setup_perf_aux_buffer(
     } else {
         Ok(mmap_addr)
     }
+}
+
+pub fn new_perf_event_attr_intel_pt() -> Result<perf_event_attr, ()> {
+    let mut attr: perf_event_attr;
+    unsafe {
+        attr = core::mem::zeroed();
+    }
+    attr.size = core::mem::size_of::<perf_event_attr>() as u32;
+    attr.type_ = intel_pt_perf_type()?;
+    attr.set_disabled(1);
+    attr.config |= PtConfig::NORETCOMP.bits();
+
+    Ok(attr)
+}
+
+fn intel_pt_perf_type() -> Result<u32, ()> {
+    let s = fs::read_to_string(PT_EVENT_TYPE_PATH).map_err(|_| ())?;
+    s.parse::<u32>().map_err(|_| ())
+    // TODO better Err()
 }
