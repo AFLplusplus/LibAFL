799c634fef047d3e98355fe1ad17c5226c901a57
Niklas Gögge <n.goeggi@gmail.com>
2024-08-09 11:24:46 +0100

    Implement FeedbackFactory for {Const,Not}Feedback (#2478)
    * libafl: Impl FeedbackFactory for NotFeedback
    
    * libafl: Impl FeedbackFactory for ConstFeedback

--- libafl/src/feedbacks/mod.rs
+++ libafl/src/feedbacks/mod.rs
@@ -818,6 +818,16 @@ where
     }
 }
 
+impl<A, S, T> FeedbackFactory<NotFeedback<A, S>, T> for NotFeedback<A, S>
+where
+    A: Feedback<S> + FeedbackFactory<A, T>,
+    S: State,
+{
+    fn create_feedback(&self, ctx: &T) -> NotFeedback<A, S> {
+        NotFeedback::new(self.first.create_feedback(ctx))
+    }
+}
+
 impl<A, S> NotFeedback<A, S>
 where
     A: Feedback<S>,
@@ -1276,6 +1286,12 @@ impl From<ConstFeedback> for bool {
             ConstFeedback::True => true,
             ConstFeedback::False => false,
         }
+    }
+}
+
+impl<T> FeedbackFactory<ConstFeedback, T> for ConstFeedback {
+    fn create_feedback(&self, _ctx: &T) -> ConstFeedback {
+        *self
     }
 }
 
