7c78eb7aa72e700de68f36c6097885c72a0a5049
sharad <sharad@mineo333.dev>
2024-09-05 15:28:17 -0400

    Fix libafl_cc

--- libafl_cc/build.rs
+++ libafl_cc/build.rs
@@ -213,9 +213,9 @@ fn build_pass(
                 }
                 Err(err) => {
                     if required {
-                        panic!("Failed to compile required compiler pass src/{src_file} - Exit status: {s}");
+                        panic!("Failed to compile required compiler pass src/{src_file} - Exit status: {err}");
                     } else {
-                        println!("cargo:warning=Skipping non-required compiler pass src/{src_file} - Reason: Exit status {s}. You can ignore this error.");
+                        println!("cargo:warning=Skipping non-required compiler pass src/{src_file} - Reason: Exit status {err}. You can ignore this error.");
                     }
                 }
             }
