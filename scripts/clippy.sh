0e3c7e48642ab3838cc0a15fe0df602720ef0236
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2024-09-09 18:00:06 +0200

    Do not use --all-features on libafl_qemu

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -7,10 +7,12 @@ set -e
 # Function to run Clippy on a single directory
 run_clippy() {
    local dir="$1"
+   local all_features="$2"
+   echo "All features: $all_features"
    echo "Running Clippy on $dir"
    pushd "$dir" || return 1
 
-   RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests --examples --benches -- -Z macro-backtrace \
+   RUST_BACKTRACE=full cargo +nightly clippy --all ${all_features:+"$all_features"} --no-deps --tests --examples --benches -- -Z macro-backtrace \
       -D clippy::all \
       -D clippy::pedantic \
       -W clippy::similar_names \
@@ -43,6 +45,11 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
    )
 fi
 
+# Do not use --all-features for the following projects
+NO_ALL_FEATURES=(
+   "libafl_qemu"
+)
+
 if [ "$#" -eq 0 ]; then
    # No arguments provided, run on all projects
    PROJECTS=("${ALL_PROJECTS[@]}")
@@ -72,8 +79,12 @@ RUST_BACKTRACE=full cargo +nightly clippy --all --all-features --no-deps --tests
 for project in "${PROJECTS[@]}"; do
    # Trim leading and trailing whitespace
    project=$(echo "$project" | sed 's/^[[:space:]]*//;s/[[:space:]]*$//')
+   all_features="--all-features"
+   if [[ " ${NO_ALL_FEATURES[*]} " =~ ${project} ]]; then
+      all_features=""
+   fi
    if [ -d "$project" ]; then
-      run_clippy "$project"
+      run_clippy "$project" $all_features
    else
       echo "Warning: Directory $project does not exist. Skipping."
    fi
