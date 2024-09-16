c4f8497896e7b64ef509ef5faa3d77e36f5957cf
Nereuxofficial <37740907+Nereuxofficial@users.noreply.github.com>
2024-09-16 16:08:22 +0200

    fix: shell script lints

--- scripts/createAliases.sh
+++ scripts/createAliases.sh
@@ -3,8 +3,8 @@
 # creates a symbolic link from bin-x.x to bin
 # This just strips off last 3 characters when creating a link
 
-LLVMFILES=/usr/bin/llvm*
-CLANGFILES=/usr/bin/clang*
+LLVMFILES="/usr/bin/llvm*"
+CLANGFILES="/usr/bin/clang*"
 LLC=/usr/bin/llc-$1
 OPT=/usr/bin/opt-$1
 LLD=/usr/bin/lld-$1
@@ -12,7 +12,7 @@ LLD=/usr/bin/lld-$1
 for f in $LLVMFILES $CLANGFILES $LLC $OPT $LLD
 do
 	link=${f::-3}
-	echo "linking" $f "to" $link
+	echo "linking" "$f" "to" "$link"
 	ln -s $f $link
 	if [ -e $f ]
 	  then cp $link /usr/local/bin/
