e621ecb710b91cf0bb940646d34e943efd243ee8
Romain Malmain <romain.malmain@pm.me>
2024-09-21 17:15:26 +0200

    add taplo to fmt_all.sh

--- libafl/src/lib.rs
+++ libafl/src/lib.rs
@@ -1,4 +1,3 @@
-#![feature(once_cell_get_mut)]
 /*!
 Welcome to `LibAFL`
 */
--- scripts/fmt_all.sh
+++ scripts/fmt_all.sh
@@ -3,10 +3,12 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LIBAFL_DIR=$(realpath "$SCRIPT_DIR/..")
 
+cd "${LIBAFL_DIR}" || exit 1
+
 if [ "$1" = "check" ]; then
   cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- -c --verbose || exit 1
 else
-  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- --verbose || exit 1
+  cargo run --manifest-path "$LIBAFL_DIR/utils/libafl_fmt/Cargo.toml" --release -- --verbose
 fi
 
 if command -v black > /dev/null; then
@@ -19,6 +21,11 @@ if command -v black > /dev/null; then
 
 else
   echo "Warning: python black not found. Formatting skipped for python."
+fi
+
+if command -v taplo > /dev/null; then
+  echo "[*] Formatting TOML files"
+  taplo format
 fi
 
 echo "[*] Done :)"
