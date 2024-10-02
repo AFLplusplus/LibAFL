390a1a06d288f32e762e9abf65fef8af8d9c0ec7
Toka <tokazerkje@outlook.com>
2024-10-02 12:37:42 +0200

    libafl_fuzz

--- libafl_qemu/src/executor.rs
+++ libafl_qemu/src/executor.rs
@@ -139,9 +139,11 @@ where
     where
         ED: EmulatorDriver<CM, ET, S, SM>,
         EM: EventFirer<State = S> + EventRestarter<State = S>,
-        OF: Feedback<S>,
+        OF: Feedback<EM, S::Input, OT, S>,
         S: Unpin + State + HasExecutions + HasCorpus + HasSolutions,
-        Z: HasObjective<Objective = OF, State = S> + HasScheduler<State = S> + ExecutionProcessor,
+        Z: HasObjective<Objective = OF, State = S>
+            + HasScheduler<State = S>
+            + ExecutionProcessor<EM, OT>,
         S::Solutions: Corpus<Input = S::Input>, //delete me
         <S::Corpus as Corpus>::Input: Clone,    //delete me
     {
@@ -328,6 +330,7 @@ where
     S: State + HasSolutions,
     SP: ShMemProvider,
     Z: HasObjective<State = S>,
+    Z::Objective: Feedback<EM, S::Input, OT, S>,
 {
     pub fn new(
         emulator: Emulator<CM, ED, ET, S, SM>,
@@ -383,7 +386,7 @@ where
     OT: ObserversTuple<S::Input, S> + Debug,
     ET: EmulatorModuleTuple<S>,
     SP: ShMemProvider,
-    OF: Feedback<S>,
+    OF: Feedback<EM, S::Input, OT, S>,
     Z: HasObjective<Objective = OF, State = S>,
 {
     fn run_target(
