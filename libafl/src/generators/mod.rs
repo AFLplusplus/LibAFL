547128ebf4b0e7ed0ffbff9ad0914dd90d8a43e8
Dongjia "toka" Zhang <tokazerkje@outlook.com>
2024-10-21 14:56:13 +0200

    no min but max

--- libafl/src/generators/mod.rs
+++ libafl/src/generators/mod.rs
@@ -83,7 +83,7 @@ where
 {
     fn generate(&mut self, state: &mut S) -> Result<BytesInput, Error> {
         let mut size = state.rand_mut().below(self.max_size);
-        size = min(size, 1);
+        size = max(size, 1);
         let random_bytes: Vec<u8> = (0..size)
             .map(|_| state.rand_mut().below(nonzero!(256)) as u8)
             .collect();
@@ -111,7 +111,7 @@ where
 {
     fn generate(&mut self, state: &mut S) -> Result<BytesInput, Error> {
         let mut size = state.rand_mut().below(self.max_size);
-        size = min(size, 1);
+        size = max(size, 1);
         let printables = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz \t\n!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~".as_bytes();
         let random_bytes: Vec<u8> = (0..size)
             .map(|_| *state.rand_mut().choose(printables).unwrap())
