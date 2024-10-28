56fa095b339886d2e058558a5de07848467f6a46
Romain Malmain <romain.malmain@pm.me>
2024-10-28 17:44:11 +0100

    update qemu to v9.1.1

--- libafl_qemu/libafl_qemu_build/src/build.rs
+++ libafl_qemu/libafl_qemu_build/src/build.rs
@@ -11,7 +11,7 @@ use crate::cargo_add_rpath;
 
 pub const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 pub const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-pub const QEMU_REVISION: &str = "805b14ffc44999952562e8f219d81c21a4fa50b9";
+pub const QEMU_REVISION: &str = "6bc81c2b3ae28fc80c6f938081066ac28a361c69";
 
 #[allow(clippy::module_name_repetitions)]
 pub struct BuildResult {
