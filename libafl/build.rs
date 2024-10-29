83db743cd5153a50568bd36169e4ab66b2f3f00d
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2024-10-29 16:30:36 +0100

    Small cleanings

--- libafl/build.rs
+++ libafl/build.rs
@@ -9,14 +9,6 @@ fn nightly() {
 fn nightly() {}
 
 fn main() {
-    let target_os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
-    if cfg!(feature = "intel_pt") && target_os != "linux" {
-        println!(
-            "cargo:warning=The 'intel_pt' feature is enabled, it is compatible only with linux but \
-            you are building for ({target_os}). The feature will not work."
-        );
-    }
-
     println!("cargo:rustc-check-cfg=cfg(nightly)");
     println!("cargo:rerun-if-changed=build.rs");
     nightly();
