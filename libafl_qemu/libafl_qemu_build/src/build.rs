b7b9f145070473951bb3bbbfce269b2d6285fdaa
Romain Malmain <romain.malmain@pm.me>
2024-10-31 14:18:39 +0100

    fix for new qemu gen_callN and x86 decoder

--- libafl_qemu/libafl_qemu_build/src/build.rs
+++ libafl_qemu/libafl_qemu_build/src/build.rs
@@ -11,7 +11,7 @@ use crate::cargo_add_rpath;
 
 pub const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 pub const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-pub const QEMU_REVISION: &str = "513bd84b400a29544b3c2f63d1ec6f515859ace4";
+pub const QEMU_REVISION: &str = "940d21f35bc5e8b599075cc6519fdaa08cc69d2e";
 
 #[allow(clippy::module_name_repetitions)]
 pub struct BuildResult {
