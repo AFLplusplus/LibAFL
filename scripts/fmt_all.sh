1b5ae7e67af4cf2a93938c236086d44c896e8958
Valentin Huber <git@valentinhuber.me>
2024-11-07 23:16:15 +0100

    Enforcing python formatting in CI

--- scripts/fmt_all.sh
+++ scripts/fmt_all.sh
@@ -13,14 +13,13 @@ fi
 
 if command -v black > /dev/null; then
   echo "[*] Formatting python files"
-  if ! black "$SCRIPT_DIR"
-  then
-    echo "Python format failed."
-    exit 1
+  if [ "$1" = "check" ]; then
+    black --check "$SCRIPT_DIR" || exit 1
+  else
+    black "$SCRIPT_DIR" || exit 1
   fi
-
 else
-  echo "Warning: python black not found. Formatting skipped for python."
+  echo -e "\n\033[1;33mWarning\033[0m: python black not found. Formatting skipped for python.\n"
 fi
 
 if [ "$1" != "check" ]; then
