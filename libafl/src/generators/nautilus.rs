36bbdb7539cc485e9382b7c844b4d3cf94b2e372
jejuisland87654 <jejuisland87654@gmail.com>
2024-11-13 20:45:18 +0100

    Fix NautilusContext::from_file for python files (#2690)

--- libafl/src/generators/nautilus.rs
+++ libafl/src/generators/nautilus.rs
@@ -89,9 +89,10 @@ impl NautilusContext {
         let grammar_file = grammar_file.as_ref();
         if grammar_file.extension().unwrap_or_default() == "py" {
             log::debug!("Creating NautilusContext from python grammar");
-            let ctx = python_grammar_loader::load_python_grammar(
+            let mut ctx = python_grammar_loader::load_python_grammar(
                 fs::read_to_string(grammar_file)?.as_str(),
             );
+            ctx.initialize(tree_depth);
             return Ok(Self { ctx });
         }
         log::debug!("Creating NautilusContext from json grammar");
