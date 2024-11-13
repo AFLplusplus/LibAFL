996f073f618f315249c01ae2ae6cb4bbb9b68f1f
aarnav <aarnav@srlabs.de>
2024-11-13 11:39:08 +0100

    fix unicorn CI?

--- fuzzers/forkserver/libafl-fuzz/src/fuzzer.rs
+++ fuzzers/forkserver/libafl-fuzz/src/fuzzer.rs
@@ -568,7 +568,7 @@ fn base_forkserver_builder<'a>(
     if let Some(target_env) = &opt.target_env {
         executor = executor.envs(target_env);
     }
-    if opt.frida_mode {
+    if opt.frida_mode || opt.unicorn_mode || opt.qemu_mode {
         executor = executor.kill_signal(nix::sys::signal::Signal::SIGKILL);
     }
     if let Some(kill_signal) = opt.kill_signal {
