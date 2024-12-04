570030f14cf641dd5839dc0ca827c40839a4f822
Marcondiro <46560192+Marcondiro@users.noreply.github.com>
2024-12-04 17:35:29 +0100

    Best bootloader ever

--- fuzzers/full_system/qemu_intel_pt_bootloader/src/boot.s
+++ fuzzers/full_system/qemu_intel_pt_bootloader/src/boot.s
@@ -1,52 +1,46 @@
 [bits 16] ; use 16 bits 
-[org 0x7c00] ; sets the start address 
+[org 0x7c00] ; sets the start address
 
-_start:
-    mov dx, [0x041e]    ; BIOS Data Area (BDA) keyboard buffer
-    mov cx, [0x0420]
+%macro print_string 1   ; %1: Pointer to the string (null-terminated)
+    mov si, %1          ; Load the pointer to the string
+.print_char:
+    lodsb               ; Load the next byte from [SI] into AL
+    or al, al           ; Check if it's the null terminator
+    jz .done            ; If zero, we are done
+    mov ah, 0x0E        ; BIOS teletype function
+    int 0x10            ; Call BIOS interrupt
+    jmp .print_char     ; Repeat for the next character
+.done:
+    mov al, 0x0d        ; CR
+    int 0x10
+    mov al, 0x0a        ; LF
+    int 0x10
+%endmacro
 
-    mov ah, 0x0E
-    mov al, dh
-    int 0x10
-    mov al, dl
-    int 0x10
-    mov al, ch
-    int 0x10
-    mov al, cl
-    int 0x10
-    mov al, 0x0d    ; CR
-    int 0x10
-    mov al, 0x0a    ; LF
-    int 0x10
+start:
+    mov ah, 0xc0
+    int 0x15            ; ask for the system configuration parameters
+    jc fail             ; carry must be 0
+    cmp ah, 0           ; ah must be 0
+    jne fail
 
-    cmp dh, 'a'
-    jne sleep_forever
-    cmp dl, 'b'
-    jne sleep_forever
-    cmp ch, 'c'
-    jne sleep_forever
+    mov ax, [es:bx]     ; byte count
+    cmp ax, 8
+    jl fail
 
-loop:
-    add cx, 1
-    cmp cx, 'a'
-    jne loop
-    add cx, 1
+    mov ch, [es:bx + 2] ; Model
+    mov cl, [es:bx + 3] ; Submodel
+    mov dh, [es:bx + 4] ; BIOS revision
+
+    cmp ch, 'a'
+    jne fail
+    cmp cl, 'b'
+    jne fail
+    cmp dh, 'c'
+    jne fail
 
 shutdown:
-    ; say bye to user
-    mov ah, 0x0E
-    mov al, 'B'
-    int 0x10
-    mov al, 'y'
-    int 0x10
-    mov al, 'e'
-    int 0x10
-    mov al, '!'
-    int 0x10
-    mov al, 0x0d    ; CR
-    int 0x10
-    mov al, 0x0a    ; LF
-    int 0x10
+    print_string bye
 
     ; sleep a bit to make sure output is printed
     xor cx, cx
@@ -63,6 +57,8 @@ shutdown:
     mov cx, 0x0003
     int 0x15
 
+fail:
+    print_string fail_msg
 sleep_forever:
     mov cx, 0xffff
     mov dx, 0xffff
@@ -70,6 +66,9 @@ sleep_forever:
     int 0x15
     jmp sleep_forever
 
-times 510-($-$$) db 0 ; fill the output file with zeroes until 510 bytes are full 
+fail_msg db "I don't like your BIOS. :(", 0
+bye db "Amazing <3 Bye!", 0
 
-dw 0xaa55 ; magic bytes that tell BIOS that this is bootable
+times 510-($-$$) db 0   ; fill the output file with zeroes until 510 bytes are full
+
+dw 0xaa55               ; magic bytes that tell BIOS that this is bootable
