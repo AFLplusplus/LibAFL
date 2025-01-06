0c6da525898f5319b20f63f23bb1485b2ba0adc6
Romain Malmain <romain.malmain@pm.me>
2025-01-06 15:40:12 +0100

    fix qemu launcher

--- fuzzers/binary_only/qemu_launcher/src/instance.rs
+++ fuzzers/binary_only/qemu_launcher/src/instance.rs
@@ -39,8 +39,10 @@ use libafl_bolts::{
 use libafl_qemu::{
     elf::EasyElf,
     modules::{
-        cmplog::CmpLogObserver, utils::filters::StdAddressFilter, EmulatorModuleTuple,
-        StdEdgeCoverageModule,
+        cmplog::CmpLogObserver,
+        edges::EdgeCoverageFullVariant,
+        utils::filters::{NopPageFilter, StdAddressFilter},
+        EdgeCoverageModule, EmulatorModule, EmulatorModuleTuple, StdEdgeCoverageModule,
     },
     Emulator, GuestAddr, Qemu, QemuExecutor,
 };
