9eed04b0f0e4253bead29c7bfd2d4d575d1c7f06
Toka <tokazerkje@outlook.com>
2025-01-11 14:34:39 +0100

    13

--- libafl_targets/src/call.rs
+++ libafl_targets/src/call.rs
@@ -1,9 +1,7 @@
 use core::marker::PhantomData;
 
 use hashbrown::HashMap;
-use libafl::{
-    executors::hooks::ExecutorHook,
-};
+use libafl::executors::hooks::ExecutorHook;
 use once_cell::sync::Lazy;
 /// The list of functions that this execution has observed
 pub static mut FUNCTION_LIST: Lazy<HashMap<usize, usize>> = Lazy::new(HashMap::new);
