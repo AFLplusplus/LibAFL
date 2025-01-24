43e3d879f321b29bf081f8a3b97fc130d735e60c
Romain Malmain <romain.malmain@pm.me>
2025-01-24 10:59:14 +0100

    clippy

--- libafl_concolic/symcc_runtime/src/tracing.rs
+++ libafl_concolic/symcc_runtime/src/tracing.rs
@@ -1,4 +1,5 @@
 //! Tracing of expressions in a serialized form.
+#![allow(no_mangle_generic_items)]
 
 pub use libafl::observers::concolic::serialization_format::StdShMemMessageFileWriter;
 use libafl::observers::concolic::SymExpr;
@@ -43,12 +44,14 @@ where
 macro_rules! expression_builder {
     ($method_name:ident ( $($param_name:ident : $param_type:ty ),+ ) => $message:ident) => {
         // #[expect(clippy::missing_safety_doc)]
+        #[no_mangle]
         fn $method_name(&mut self, $( $param_name : $param_type, )+ ) -> Option<RSymExpr> {
             self.write_message(SymExpr::$message { $($param_name,)+ })
         }
     };
     ($method_name:ident () => $message:ident) => {
         // #[expect(clippy::missing_safety_doc)]
+        #[no_mangle]
         fn $method_name(&mut self) -> Option<RSymExpr> {
             self.write_message(SymExpr::$message)
         }
@@ -71,6 +74,7 @@ impl<SHM> Runtime for TracingRuntime<SHM>
 where
     SHM: ShMem,
 {
+    #[no_mangle]
     fn build_integer_from_buffer(
         &mut self,
         _buffer: *mut core::ffi::c_void,
