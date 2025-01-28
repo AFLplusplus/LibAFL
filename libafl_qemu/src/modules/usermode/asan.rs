c6608579cc53158c1053a5751f9422ede85f0064
Toka <tokazerkje@outlook.com>
2025-01-28 14:09:21 +0100

    correct pc

--- libafl_qemu/src/modules/usermode/asan.rs
+++ libafl_qemu/src/modules/usermode/asan.rs
@@ -1089,7 +1089,7 @@ pub fn trace_write_asan<ET, I, S, const N: usize>(
     emulator_modules: &mut EmulatorModules<ET, I, S>,
     state: Option<&mut S>,
     id: u64,
-    _pc: GuestAddr,
+    pc: GuestAddr,
     addr: GuestAddr,
 ) where
     ET: EmulatorModuleTuple<I, S>,
@@ -1099,38 +1099,35 @@ pub fn trace_write_asan<ET, I, S, const N: usize>(
     let h: &mut AsanModule = emulator_modules.get_mut::<AsanModule>().unwrap();
     // todo review this shit
     let value_after_write = unsafe {qemu.read_mem_val::<u32>(addr).unwrap() };
-    let rip = qemu.read_reg(Regs::Rip).unwrap(); // why getting rip would fail :P
-    println!("Value written is {} {} {}", value_after_write, rip, addr);
     if h.use_rca() {
         let state = state.expect("state missing for rca");
         let predicates = state
             .metadata_mut::<Tracer>()
             .expect("Predicates missing for rca");
-        let rip = qemu.read_reg(Regs::Rip).unwrap(); // why getting rip would fail :P
         match N {
             1 => {
                 let value = unsafe { qemu.read_mem_val::<u8>(addr) };
                 if let Ok(value) = value {
-                    predicates.update_max_min(rip, u64::from(value));
+                    predicates.update_max_min(pc, u64::from(value));
                 }
             }
             2 => {
                 let value: Result<u16, crate::QemuRWError> =
                     unsafe { qemu.read_mem_val::<u16>(addr) };
                 if let Ok(value) = value {
-                    predicates.update_max_min(rip, u64::from(value));
+                    predicates.update_max_min(pc, u64::from(value));
                 }
             }
             4 => {
                 let value = unsafe { qemu.read_mem_val::<u32>(addr) };
                 if let Ok(value) = value {
-                    predicates.update_max_min(rip, u64::from(value));
+                    predicates.update_max_min(pc, u64::from(value));
                 }
             }
             8 => {
                 let value = unsafe { qemu.read_mem_val::<u64>(addr) };
                 if let Ok(value) = value {
-                    predicates.update_max_min(rip, value);
+                    predicates.update_max_min(pc, value);
                 }
             }
             _ => {
