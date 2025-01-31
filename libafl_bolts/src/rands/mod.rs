ace2a76ece18c895b7d073d6c2c55f41f2bcef8c
Dongjia "toka" Zhang <tokazerkje@outlook.com>
2025-01-31 19:26:32 +0100

    Update rand_core  (#2919)
    * a
    
    * upd lain

--- libafl_bolts/src/rands/mod.rs
+++ libafl_bolts/src/rands/mod.rs
@@ -263,10 +263,6 @@ macro_rules! impl_rng_core {
             fn fill_bytes(&mut self, dest: &mut [u8]) {
                 rand_core::impls::fill_bytes_via_next(self, dest)
             }
-
-            fn try_fill_bytes(&mut self, dest: &mut [u8]) -> Result<(), rand_core::Error> {
-                Ok(self.fill_bytes(dest))
-            }
         }
     };
 }
