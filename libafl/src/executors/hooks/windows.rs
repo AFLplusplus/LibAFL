b5d167ffea759943d50833aac561bab1072c98ca
Toka <tokazerkje@outlook.com>
2025-02-10 10:18:19 +0100

    fmt

--- libafl/src/executors/hooks/windows.rs
+++ libafl/src/executors/hooks/windows.rs
@@ -5,7 +5,7 @@ pub mod windows_asan_handler {
     use core::sync::atomic::{compiler_fence, Ordering};
 
     use windows::Win32::System::Threading::{
-        EnterCriticalSection, LeaveCriticalSection, CRITICAL_SECTION, ExitProcess
+        EnterCriticalSection, ExitProcess, LeaveCriticalSection, CRITICAL_SECTION,
     };
 
     use crate::{
