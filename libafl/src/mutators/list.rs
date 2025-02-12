238ea8b4e4185841d1683e9c06f82b9f70f2b733
Valentin Huber <git@valentinhuber.me>
2025-02-12 16:02:30 +0100

    Add remove mutators for ListInput

--- libafl/src/mutators/list.rs
+++ libafl/src/mutators/list.rs
@@ -1,12 +1,14 @@
 //! Mutators for [`ListInput`]
 use alloc::borrow::Cow;
+use core::num::NonZero;
 
-use libafl_bolts::{Error, Named};
+use libafl_bolts::{rands::Rand as _, Error, Named};
 
 use crate::{
     generators::Generator,
     inputs::ListInput,
     mutators::{MutationResult, Mutator},
+    state::HasRand,
 };
 
 /// Mutator that generates a new input and appends it to the list.
@@ -37,5 +39,62 @@ where
 impl<G> Named for GenerateToAppendMutator<G> {
     fn name(&self) -> &Cow<'static, str> {
         &Cow::Borrowed("GenerateToAppendMutator")
+    }
+}
+
+/// Mutator that removes the last entry from a [`ListInput`].
+///
+/// Returns [`MutationResult::Skipped`] if the input is empty.
+#[derive(Debug)]
+pub struct RemoveLastEntryMutator;
+
+impl<I, S> Mutator<ListInput<I>, S> for RemoveLastEntryMutator {
+    fn mutate(
+        &mut self,
+        _state: &mut S,
+        input: &mut ListInput<I>,
+    ) -> Result<MutationResult, Error> {
+        match input.parts_mut().pop() {
+            Some(_) => Ok(MutationResult::Mutated),
+            None => Ok(MutationResult::Skipped),
+        }
+    }
+}
+
+impl Named for RemoveLastEntryMutator {
+    fn name(&self) -> &Cow<'static, str> {
+        &Cow::Borrowed("RemoveLastEntryMutator")
+    }
+}
+
+/// Mutator that removes a random entry from a [`ListInput`].
+///
+/// Returns [`MutationResult::Skipped`] if the input is empty.
+#[derive(Debug)]
+pub struct RemoveRandomEntryMutator;
+
+impl<I, S> Mutator<ListInput<I>, S> for RemoveRandomEntryMutator
+where
+    S: HasRand,
+{
+    fn mutate(&mut self, state: &mut S, input: &mut ListInput<I>) -> Result<MutationResult, Error> {
+        let parts = input.parts_mut();
+        match parts.len() {
+            0 => Ok(MutationResult::Skipped),
+            len => {
+                // Safety: null checks are done above
+                let index = state
+                    .rand_mut()
+                    .below(unsafe { NonZero::new_unchecked(len) });
+                parts.remove(index);
+                Ok(MutationResult::Mutated)
+            }
+        }
+    }
+}
+
+impl Named for RemoveRandomEntryMutator {
+    fn name(&self) -> &Cow<'static, str> {
+        &Cow::Borrowed("RemoveRandomEntryMutator")
     }
 }
