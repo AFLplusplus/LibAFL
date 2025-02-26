09d81f74dbc239c5345cb7e7127466d4db64ae5a
Dominik Maier <dmnk@google.com>
2025-02-26 23:04:00 +0100

    less edition

--- libafl_qemu/build_linux.rs
+++ libafl_qemu/build_linux.rs
@@ -183,7 +183,6 @@ pub fn build() {
         .expect("Could not write bindings.");
 
     bindgen::Builder::default()
-        .rust_edition(2024)
         .derive_debug(true)
         .derive_default(true)
         .impl_debug(true)
