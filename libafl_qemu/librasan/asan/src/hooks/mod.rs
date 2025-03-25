294a5e9fb003f696a1c286292691b31f336265c2
Your Name <you@example.com>
2025-03-25 10:05:43 +0000

    Add wcsnlen

--- libafl_qemu/librasan/asan/src/hooks/mod.rs
+++ libafl_qemu/librasan/asan/src/hooks/mod.rs
@@ -57,6 +57,7 @@ pub mod wcscmp;
 pub mod wcscpy;
 pub mod wcslen;
 pub mod wcsncmp;
+pub mod wcsnlen;
 pub mod write;
 
 #[cfg(feature = "libc")]
@@ -229,6 +230,10 @@ impl PatchedHook {
             PatchedHook::new::<unsafe extern "C" fn (*const wchar_t, *const wchar_t, size_t) -> c_int>(
                 c"wcsncmp",
                 hooks::wcsncmp::wcsncmp,
+            ),
+            PatchedHook::new::<unsafe extern "C" fn ( *const wchar_t,  size_t) -> size_t>(
+                c"wcsnlen",
+                hooks::wcsnlen::wcsnlen,
             ),
         ]
         .to_vec()
