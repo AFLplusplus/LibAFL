184b69be8e4d00b7b898ed57965937900dd4b64a
Romain Malmain <romain.malmain@pm.me>
2025-03-31 15:51:01 +0200

    Update QEMU syshook ret value (#3092)
    * update syshook ret value

--- libafl_qemu/libafl_qemu_build/src/build.rs
+++ libafl_qemu/libafl_qemu_build/src/build.rs
@@ -11,7 +11,7 @@ use crate::cargo_add_rpath;
 
 pub const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 pub const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-pub const QEMU_REVISION: &str = "2a676d9cd8c474b5c0db1d77d2769e56e2ed8524";
+pub const QEMU_REVISION: &str = "97bef506eed24ee8d0eda4a07c4419c55dae4acb";
 
 pub struct BuildResult {
     pub qemu_path: PathBuf,
