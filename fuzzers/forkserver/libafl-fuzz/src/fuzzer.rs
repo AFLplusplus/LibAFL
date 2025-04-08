5f1bfa4fd8033f0b6aca18bba9131fa8e847416c
mio <mio@lazym.io>
2025-04-08 10:03:34 +0800

    leaveout

--- fuzzers/forkserver/libafl-fuzz/src/fuzzer.rs
+++ fuzzers/forkserver/libafl-fuzz/src/fuzzer.rs
@@ -51,7 +51,7 @@ use libafl_bolts::{
     rands::StdRand,
     shmem::{ShMem, ShMemProvider, UnixShMemProvider},
     tuples::{tuple_list, Handled, Merge},
-    AsSliceMut,
+    AflTargetArgs, AsSliceMut,
 };
 #[cfg(feature = "nyx")]
 use libafl_nyx::{executor::NyxExecutor, helper::NyxHelper, settings::NyxSettings};
