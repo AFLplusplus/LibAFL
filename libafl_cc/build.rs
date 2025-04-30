767f9e692eb062b02788d447f0ec43b0fcaee818
Dongjia Zhang <tokazerkje@outlook.com>
2025-04-30 16:04:14 +0200

    nothing works

--- libafl_cc/build.rs
+++ libafl_cc/build.rs
@@ -258,6 +258,12 @@ fn build_pass(
 
 #[expect(clippy::too_many_lines)]
 fn main() {
+    if cfg!(target_os = "ios"){
+        println!("cargo:warning=ios not supported");
+        return;
+    }
+    println!("cargo:warning=building libafl_cc ...");
+
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let out_dir = Path::new(&out_dir);
     let src_dir = Path::new("src");
