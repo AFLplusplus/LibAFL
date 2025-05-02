806a42d7d054f977cb2c0da0a82c68188c43ba3d
Romain Malmain <romain.malmain@pm.me>
2025-05-02 22:21:52 +0200

    update qemu

--- libafl_qemu/libafl_qemu_build/src/build.rs
+++ libafl_qemu/libafl_qemu_build/src/build.rs
@@ -11,7 +11,7 @@ use crate::cargo_add_rpath;
 
 pub const QEMU_URL: &str = "https://github.com/AFLplusplus/qemu-libafl-bridge";
 pub const QEMU_DIRNAME: &str = "qemu-libafl-bridge";
-pub const QEMU_REVISION: &str = "13757ee0e957747b3d891e961e754d2885ee6432";
+pub const QEMU_REVISION: &str = "d20fb07d99ffccd481b439e104ae3624ef5c75da";
 
 pub struct BuildResult {
     pub qemu_path: PathBuf,
