64ddded1dde8fe5b6b4a540f28d2487cc134acf4
aarnav <aarnav@srlabs.de>
2025-05-05 15:21:19 +0200

    fmt

--- libafl/src/stages/sync.rs
+++ libafl/src/stages/sync.rs
@@ -8,16 +8,16 @@ use core::{marker::PhantomData, time::Duration};
 use std::path::{Path, PathBuf};
 
 use libafl_bolts::{
-    current_time,
+    Named, current_time,
     fs::find_new_files_rec,
     shmem::{ShMem, ShMemProvider},
-    Named,
 };
 use serde::{Deserialize, Serialize};
 
 use crate::{
+    Error, HasMetadata, HasNamedMetadata,
     corpus::{Corpus, CorpusId, HasCurrentCorpusId},
-    events::{llmp::LlmpEventConverter, Event, EventConfig, EventFirer, EventWithStats},
+    events::{Event, EventConfig, EventFirer, EventWithStats, llmp::LlmpEventConverter},
     executors::{Executor, ExitKind, HasObservers},
     fuzzer::{Evaluator, EvaluatorObservers, ExecutionProcessor, HasObjective},
     inputs::{Input, InputConverter},
@@ -26,7 +26,6 @@ use crate::{
         HasCorpus, HasCurrentTestcase, HasExecutions, HasRand, HasSolutions,
         MaybeHasClientPerfMonitor, Stoppable,
     },
-    Error, HasMetadata, HasNamedMetadata,
 };
 
 /// Default name for `SyncFromDiskStage`; derived from AFL++
