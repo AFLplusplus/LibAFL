fe107a00a08c9a4a354f436c6a5d9c8839488e68
mio <mio@lazym.io>
2025-05-13 02:05:52 +0800

    fix

--- fuzzers/forkserver/forkserver_simple/src/main.rs
+++ fuzzers/forkserver/forkserver_simple/src/main.rs
@@ -6,7 +6,7 @@ use libafl::{
     HasMetadata,
     corpus::{Corpus, InMemoryCorpus, OnDiskCorpus},
     events::SimpleEventManager,
-    executors::{HasObservers, forkserver::ForkserverExecutor},
+    executors::{ChildArgs, HasObservers, forkserver::ForkserverExecutor},
     feedback_and_fast, feedback_or,
     feedbacks::{CrashFeedback, MaxMapFeedback, TimeFeedback},
     fuzzer::{Fuzzer, StdFuzzer},
