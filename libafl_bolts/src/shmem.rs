88c550f36f5a9057f65d62ab31ef255004dde02e
Evian-Zhang <evianzhang1999@163.com>
2025-05-18 19:56:43 +0800

    Fix clippy

--- libafl_bolts/src/shmem.rs
+++ libafl_bolts/src/shmem.rs
@@ -4,10 +4,10 @@
 #[cfg(feature = "alloc")]
 use alloc::{rc::Rc, string::ToString, vec::Vec};
 #[cfg(feature = "alloc")]
-use core::{cell::RefCell, fmt, fmt::Display, mem::ManuallyDrop};
+use core::{cell::RefCell, fmt, fmt::Display};
 use core::{
     fmt::Debug,
-    mem::size_of,
+    mem::{ManuallyDrop, size_of},
     ops::{Deref, DerefMut},
 };
 #[cfg(feature = "std")]
