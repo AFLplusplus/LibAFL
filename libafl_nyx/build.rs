2b8c2f184ff297b6d58fd62d795e500cb5d72ea9
Dominik Maier <dmnk@google.com>
2025-05-30 13:46:17 +0200

    Skip nyx checkout in doc mode

--- libafl_nyx/build.rs
+++ libafl_nyx/build.rs
@@ -1,7 +1,7 @@
 use std::process::Command;
 
 fn main() {
-    if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") {
+    if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") && !cfg!(doc) {
         println!("cargo:rerun-if-changed=build.rs");
         // let output = Command::new("./build_nyx_support.sh").output().expect("can't run ./build_nyx_support.sh");
         let status = Command::new("./build_nyx_support.sh")
@@ -13,6 +13,6 @@ fn main() {
             panic!("fail to run ./build_nyx_support.sh");
         }
     } else {
-        println!("cargo:warning=NYX node is only avaliable on x64 Linux");
+        println!("cargo:warning=NYX node is only available on x64 Linux");
     }
 }
