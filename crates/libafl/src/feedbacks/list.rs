833d8f81035bd8a4fe84d566c7a876cc62b41ab0
grayfox <grayfox@outerhaven.de>
2025-08-18 21:03:37 +0200

    ListFeedback: move functionality of ListDumpFeedback to ListFeedback
    Adds a second constructor `with_coverage_dump` to ListFeedback. This will
    create an Option<File> from a path parameter. `has_interesting_list_observer_feedback` will
    than write any new observed coverage to the file. For the already existing `new`
    constructor, this Option<File> is set to `None`.
    
    Removes the `Clone` trait from ListFeedback and adds necessary traits
    `ToString` and `LowerHex` to `T`.

--- crates/libafl/src/feedbacks/list.rs
+++ crates/libafl/src/feedbacks/list.rs
@@ -13,9 +13,11 @@ use crate::{
     executors::ExitKind,
     feedbacks::{Feedback, StateInitializer},
     observers::ListObserver,
+    std::fmt::LowerHex,
     std::fs::File,
     std::io::Write,
     std::path::Path,
+    std::string::ToString,
 };
 
 /// The metadata to remember past observed value
@@ -62,10 +64,11 @@ impl<T> HasRefCnt for ListFeedbackMetadata<T> {
 }
 
 /// Consider interesting a testcase if the list in `ListObserver` is not empty.
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub struct ListFeedback<T> {
     observer_handle: Handle<ListObserver<T>>,
     novelty: HashSet<T>,
+    file: Option<File>,
 }
 
 libafl_bolts::impl_serdeany!(
@@ -75,7 +78,15 @@ libafl_bolts::impl_serdeany!(
 
 impl<T> ListFeedback<T>
 where
-    T: Debug + Eq + Hash + for<'a> Deserialize<'a> + Serialize + 'static + Copy,
+    T: Debug
+        + Eq
+        + Hash
+        + for<'a> Deserialize<'a>
+        + Serialize
+        + 'static
+        + Copy
+        + ToString
+        + LowerHex,
 {
     fn has_interesting_list_observer_feedback<OT, S>(
         &mut self,
@@ -99,7 +110,15 @@ where
                 self.novelty.insert(*v);
             }
         }
-        !self.novelty.is_empty()
+        if !self.novelty.is_empty() {
+            if let Some(mut file) = self.file.as_ref() {
+                for line in &self.novelty {
+                    file.write_all(format!("0x{line:x}\n").as_bytes()).unwrap();
+                }
+            }
+            return true;
+        }
+        false
     }
 
     fn append_list_observer_metadata<S: HasNamedMetadata>(&mut self, state: &mut S) {
@@ -129,7 +148,16 @@ impl<EM, I, OT, S, T> Feedback<EM, I, OT, S> for ListFeedback<T>
 where
     OT: MatchName,
     S: HasNamedMetadata,
-    T: Debug + Eq + Hash + for<'a> Deserialize<'a> + Serialize + Default + Copy + 'static,
+    T: Debug
+        + Eq
+        + Hash
+        + for<'a> Deserialize<'a>
+        + Serialize
+        + Default
+        + Copy
+        + 'static
+        + ToString
+        + LowerHex,
 {
     fn is_interesting(
         &mut self,
@@ -173,123 +201,14 @@ impl<T> ListFeedback<T> {
         Self {
             observer_handle: observer.handle(),
             novelty: HashSet::new(),
+            file: None,
         }
     }
-}
 
-/// Consider interesting a testcase if the list in `ListObserver` is not empty.
-/// Modified version of [[`ListFeedback`]] that expects a [[`ListObserver`]]
-/// containing addresses and will write any newly observed address to
-/// a file (see [[`ListDumpFeedback::new`]])
-#[derive(Debug)]
-pub struct ListDumpFeedback {
-    observer_handle: Handle<ListObserver<u64>>,
-    novelty: HashSet<u64>,
-    file: File,
-}
-
-impl ListDumpFeedback {
-    fn has_interesting_list_observer_feedback<OT, S>(
-        &mut self,
-        state: &mut S,
-        observers: &OT,
-    ) -> bool
-    where
-        OT: MatchName,
-        S: HasNamedMetadata,
-    {
-        let observer = observers.get(&self.observer_handle).unwrap();
-        // TODO register the list content in a testcase metadata
-        self.novelty.clear();
-        // can't fail
-        let history_set = state
-            .named_metadata_map_mut()
-            .get_mut::<ListFeedbackMetadata<u64>>(self.name())
-            .unwrap();
-        for v in observer.list() {
-            if !history_set.set.contains(v) {
-                self.novelty.insert(*v);
-            }
-        }
-        if !self.novelty.is_empty() {
-            for line in &self.novelty {
-                self.file
-                    .write_all(format!("0x{line:x}\n").as_bytes())
-                    .unwrap();
-            }
-            return true;
-        }
-        false
-    }
-
-    fn append_list_observer_metadata<S: HasNamedMetadata>(&mut self, state: &mut S) {
-        let history_set = state
-            .named_metadata_map_mut()
-            .get_mut::<ListFeedbackMetadata<u64>>(self.name())
-            .unwrap();
-
-        for v in &self.novelty {
-            history_set.set.insert(*v);
-        }
-    }
-}
-
-impl<S> StateInitializer<S> for ListDumpFeedback
-where
-    S: HasNamedMetadata,
-{
-    fn init_state(&mut self, state: &mut S) -> Result<(), Error> {
-        state.add_named_metadata_checked(self.name(), ListFeedbackMetadata::<u64>::default())?;
-        Ok(())
-    }
-}
-
-impl<EM, I, OT, S> Feedback<EM, I, OT, S> for ListDumpFeedback
-where
-    OT: MatchName,
-    S: HasNamedMetadata,
-{
-    fn is_interesting(
-        &mut self,
-        state: &mut S,
-        _manager: &mut EM,
-        _input: &I,
-        observers: &OT,
-        _exit_kind: &ExitKind,
-    ) -> Result<bool, Error> {
-        Ok(self.has_interesting_list_observer_feedback(state, observers))
-    }
-
-    #[cfg(feature = "track_hit_feedbacks")]
-    fn last_result(&self) -> Result<bool, Error> {
-        Ok(!self.novelty.is_empty())
-    }
-
-    fn append_metadata(
-        &mut self,
-        state: &mut S,
-        _manager: &mut EM,
-        _observers: &OT,
-        _testcase: &mut crate::corpus::Testcase<I>,
-    ) -> Result<(), Error> {
-        self.append_list_observer_metadata(state);
-        Ok(())
-    }
-}
-
-impl Named for ListDumpFeedback {
-    #[inline]
-    fn name(&self) -> &Cow<'static, str> {
-        self.observer_handle.name()
-    }
-}
-
-impl ListDumpFeedback {
-    /// Creates a new [`ListDumpFeedback`], deciding if the given [`ListObserver`] value of a run is interesting.
-    /// Dump newly observed addresses to `path`
-    #[must_use]
-    pub fn new<P: AsRef<Path>>(observer: &ListObserver<u64>, path: P) -> Self {
-        let file = File::create(path).unwrap();
+    /// Creates a new [`ListFeedback`], deciding if the given [`ListObserver`] value of a run is interesting.
+    /// Will dump newly observed addresses to `path`. If `path` exists, the file will be truncated.
+    pub fn with_coverage_dump<P: AsRef<Path>>(observer: &ListObserver<T>, path: P) -> Self {
+        let file = Some(File::create(path).unwrap());
 
         Self {
             observer_handle: observer.handle(),
