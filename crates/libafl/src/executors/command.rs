063131e8579b86fed11349718d9910e9924a7457
Dominik Maier <dmnk@google.com>
2025-11-05 07:53:09 +0100

    more fix

--- crates/libafl/src/executors/command.rs
+++ crates/libafl/src/executors/command.rs
@@ -215,7 +215,7 @@ impl CommandConfigurator<Child> for StdCommandConfigurator {
 
 /// Linux specific [`CommandConfigurator`] that leverages `ptrace`
 ///
-/// This configurator was primarly developed to be used in conjunction with
+/// This configurator was primarily developed to be used in conjunction with
 /// [`crate::executors::hooks::intel_pt::IntelPTHook`]
 #[cfg(all(feature = "intel_pt", target_os = "linux"))]
 #[derive(Debug, Clone, PartialEq, Eq, TypedBuilder)]
@@ -305,7 +305,7 @@ impl CommandConfigurator<Pid> for PTraceCommandConfigurator {
         Duration::from_secs(u64::from(self.timeout))
     }
 
-    /// Use [`PTraceCommandConfigurator::builder().timeout`] instead
+    /// Use [`PTraceCommandConfigurator::builder().timeout`](PTraceCommandConfigurator::timeout) instead
     fn exec_timeout_mut(&mut self) -> &mut Duration {
         unimplemented!("Use [`PTraceCommandConfigurator::builder().timeout`] instead")
     }
