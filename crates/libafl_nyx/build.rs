289af2b5862a3fccf62920edb05b0ba2376f3d2e
toka <tokazerkje@outlook.jp>
2025-11-20 16:33:35 +0100

    build libafl_nyx in target

--- crates/libafl_nyx/build.rs
+++ crates/libafl_nyx/build.rs
@@ -1,10 +1,15 @@
-use std::process::Command;
-
+use std::{env, path::PathBuf, process::Command};
 fn main() {
     if cfg!(target_os = "linux") && cfg!(target_arch = "x86_64") && !cfg!(doc) {
+        let target_dir = PathBuf::from(env::var("OUT_DIR").unwrap())
+            .ancestors()
+            .nth(3)
+            .unwrap()
+            .to_path_buf();
         println!("cargo:rerun-if-changed=build.rs");
         // let output = Command::new("./build_nyx_support.sh").output().expect("can't run ./build_nyx_support.sh");
         let status = Command::new("./build_nyx_support.sh")
+            .arg(target_dir)
             .status()
             .expect("can't run ./build_nyx_support.sh");
         if status.success() {
