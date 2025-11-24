003b852eb37860dd0d06d6bf388a3771fa1cc699
Romain Malmain <romain.malmain@pm.me>
2025-11-24 17:49:25 +0100

    fmt

--- crates/libafl_qemu/libvharness_sys/build.rs
+++ crates/libafl_qemu/libvharness_sys/build.rs
@@ -37,7 +37,7 @@ fn main() {
             let vharness_rev = vharness_dir.join("QEMU_REVISION");
             if !vharness_rev.exists()
                 || fs::read_to_string(&vharness_rev).expect("Failed to read QEMU_REVISION")
-                != LIBVHARNESS_COMMIT
+                    != LIBVHARNESS_COMMIT
             {
                 drop(fs::remove_dir_all(&vharness_dir));
             }
