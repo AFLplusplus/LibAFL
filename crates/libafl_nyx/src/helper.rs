88a67589589473e2904fc92f032a5654b9dd4ea4
toka <tokazerkje@outlook.jp>
2025-11-25 15:56:28 +0100

    fix

--- crates/libafl_nyx/src/helper.rs
+++ crates/libafl_nyx/src/helper.rs
@@ -46,7 +46,7 @@ impl NyxHelper {
         })?;
         nyx_config.set_input_buffer_size(settings.input_buffer_size);
         if !nyx_config.set_aux_buffer_size(settings.aux_buffer_size) {
-            return Err(Err::illegal_argument(format!(
+            return Err(Error::illegal_argument(format!(
                 "aux_buffer_size must be multiple of 4KB"
             )));
         }
