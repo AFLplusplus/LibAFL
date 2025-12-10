94689c764ccacd90ef80ebee34e78cb3025774ac
Dominik Maier <dmnk@google.com>
2025-12-10 18:47:45 +0000

    fix?

--- crates/libafl_qemu/src/modules/utils/filters.rs
+++ crates/libafl_qemu/src/modules/utils/filters.rs
@@ -388,7 +388,10 @@ impl PageFilter for NopPageFilter {
 #[cfg(any(feature = "injections", not(cpu_target = "hexagon")))]
 pub(crate) static mut NOP_ADDRESS_FILTER: UnsafeCell<NopAddressFilter> =
     UnsafeCell::new(NopAddressFilter);
-#[cfg(all(feature = "systemmode", not(cpu_target = "hexagon")))]
+#[cfg(any(
+    feature = "systemmode",
+    not(any(cpu_target = "mips", cpu_target = "hexagon"))
+))]
 pub(crate) static mut NOP_PAGE_FILTER: UnsafeCell<NopPageFilter> = UnsafeCell::new(NopPageFilter);
 
 #[cfg(all(feature = "systemmode", test))]
