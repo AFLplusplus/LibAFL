1e96943ae21d5e7d19e0aa37e17b85672aced888
Dominik Maier <dmnk@google.com>
2025-12-13 04:36:24 +0100

    fmt

--- fuzzers/inprocess/libfuzzer_libpng_launcher/src/lib.rs
+++ fuzzers/inprocess/libfuzzer_libpng_launcher/src/lib.rs
@@ -437,7 +437,7 @@ pub extern "C" fn libafl_main() {
         .overcommit(opt.overcommit)
         .broker_port(broker_port)
         .remote_broker_addr(opt.remote_broker_addr);
-        //.stdout_file(Some("/dev/null"));
+    //.stdout_file(Some("/dev/null"));
 
     #[cfg(unix)]
     let builder = builder.fork(opt.fork);
