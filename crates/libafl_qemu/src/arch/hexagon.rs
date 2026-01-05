1dca46cf9e3276c4181abe69dccb8926d0c27e1f
Romain Malmain <romain.malmain@pm.me>
2026-01-05 14:47:47 +0100

    hexagon

--- crates/libafl_qemu/src/arch/hexagon.rs
+++ crates/libafl_qemu/src/arch/hexagon.rs
@@ -97,15 +97,16 @@ impl Regs {
 pub type GuestReg = u32;
 
 impl crate::ArchExtras for crate::CPU {
-    fn read_return_address(&self) -> Result<GuestReg, QemuRWError> {
-        self.read_reg(Regs::Lr)
+    fn read_return_address(&self) -> Result<GuestAddr, QemuRWError> {
+        self.read_reg(Regs::Lr) as GuestAddr
     }
 
     fn write_return_address<T>(&self, val: T) -> Result<(), QemuRWError>
     where
-        T: Into<GuestReg>,
+        T: Into<GuestAddr>,
     {
-        self.write_reg(Regs::Lr, val)
+        let addr: GuestAddr = val.into();
+        self.write_reg(Regs::Lr, val as GuestReg)
     }
 
     fn read_function_argument_with_cc(
