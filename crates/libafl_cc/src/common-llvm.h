891720316b961f8337d612658f41dc31f6509c0d
R. Elliott Childre <elliottchildre329@gmail.com>
2026-05-05 16:07:57 -0400

    [libafl_cc] Fix for changes to ArrayRef construct
    Constructing an ArrayRef from a nullopt was deprecated in LLVM commit:
    
    `2529de5c935a ([ADT] Deprecate ArrayRef(std::nullopt) (#146011), 2025-06-27)`
    
    (First tagged in LLVM 21)
    
    Then removed in LLVM commit:
    
    `cfbb4cc31215 ([ADT] Remove ArrayRef(std::nullopt_t) (#165831), 2025-10-31)`
    
    (First tagged in LLVM 22)
    
    The LLVM authors recommend switching to the `{}` C++ syntax, so
    introduce that conditionally if and only if the LLVM version is
    greater than or equal to 21 and fall back to the previous nullopt
    workaround, otherwise

--- crates/libafl_cc/src/common-llvm.h
+++ crates/libafl_cc/src/common-llvm.h
@@ -10,11 +10,6 @@
 #define HAVE_VECTOR_INTRINSICS 1
 
 #include <optional>
-#if LLVM_VERSION_MAJOR >= 16
-// None constant being deprecated for LLVM-16, it is recommended
-// to use the std::nullopt_t type instead. (#1010)
-constexpr std::nullopt_t None = std::nullopt;
-#endif
 
 // all llvm includes and friends
 #include "llvm/Support/CommandLine.h"
@@ -32,6 +27,23 @@ constexpr std::nullopt_t None = std::nullopt;
 #endif
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/PassManager.h"
+
+#if LLVM_VERSION_MAJOR >= 21
+  // None is only used in libafl_cc as the second parameter of MDNode::get()
+  // that param is an ArrayRef, which can no longer be constructed with a
+  // nullopt since LLVM 21+
+  //
+  // Keep these around to keep IWYU quiet
+  #include "llvm/ADT/ArrayRef.h"
+namespace llvm {
+class Metadata;
+}
+inline constexpr llvm::ArrayRef<llvm::Metadata *> None{};
+#elif LLVM_VERSION_MAJOR >= 16
+// None constant being deprecated for LLVM-16, it is recommended
+// to use the std::nullopt_t type instead. (#1010)
+constexpr std::nullopt_t None = std::nullopt;
+#endif
 
 #define FATAL(...)                          \
   do {                                      \
