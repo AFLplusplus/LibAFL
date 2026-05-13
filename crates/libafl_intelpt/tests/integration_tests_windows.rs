a9ca1dff6c17479a8be37808fbd666cf676c99bc
Marcondiro <Marco.Cavenati@eurecom.fr>
2026-05-13 19:29:25 +0200

    WIP win

--- crates/libafl_intelpt/tests/integration_tests_windows.rs
+++ crates/libafl_intelpt/tests/integration_tests_windows.rs
@@ -1,66 +1,96 @@
 #![cfg(feature = "std")]
 #![cfg(target_os = "windows")]
 
-use std::{ffi::c_void, fs::File, io::Write};
-
-use libafl_intelpt::{IntelPT, availability};
-use windows::Win32::{
-    Foundation::{CloseHandle, WAIT_OBJECT_0},
-    System::Threading::{
-        CreateThread, GetThreadId, INFINITE, Sleep, THREAD_CREATION_FLAGS, WaitForSingleObject,
+use std::{
+    arch::asm,
+    slice,
+    sync::{
+        Arc, Barrier,
+        mpsc::{Sender, channel},
     },
+    thread,
 };
 
-extern "system" fn worker_main(_lp_parameter: *mut c_void) -> u32 {
-    unsafe { Sleep(2000) };
-    0
+use libafl_intelpt::{IntelPT, availability};
+use log::LevelFilter;
+use proc_maps::get_process_maps;
+use ptcov::PtImage;
+use windows::Win32::System::Threading::{GetCurrentProcessId, GetCurrentThreadId};
+
+fn worker_main(thread_id_sender: Sender<u32>, barrier: Arc<Barrier>) -> u32 {
+    let tid = unsafe { GetCurrentThreadId() };
+    thread_id_sender.send(tid).unwrap();
+
+    barrier.wait();
+
+    let mut count = 0;
+    unsafe {
+        asm!(
+        "2:",
+        "add {0:r}, 1",
+        "cmp {0:r}, 255",
+        "jle 2b",
+        inout(reg) count,
+        options(nostack)
+        );
+    }
+
+    barrier.wait();
+    count
 }
 
 #[test]
 fn intel_pt_trace_thread() {
+    let _ = env_logger::builder()
+        .is_test(true)
+        .filter_level(LevelFilter::Trace)
+        .try_init();
+
     if let Err(reason) = availability() {
         println!("Intel PT is not available, skipping test. Reasons:");
         println!("{reason}");
         return;
     }
 
-    let thread_handle = unsafe {
-        CreateThread(
-            None,
-            0,
-            Some(worker_main),
-            Some(std::ptr::null_mut()),
-            THREAD_CREATION_FLAGS(0),
-            None,
-        )
-    }
-    .expect("Failed to create worker thread");
+    let pid = unsafe { GetCurrentProcessId() };
 
-    let thread_id = unsafe { GetThreadId(thread_handle) };
-    assert!(
-        thread_id != 0,
-        "Failed to read worker thread ID from parent"
-    );
+    let maps = get_process_maps(pid).expect("failed to get process maps");
+    let images = maps
+        .iter()
+        .filter(|map| map.is_exec())// && map.filename().is_some())
+        .map(|pm| {
+            println!("map: {:?}", pm);
+            let data = unsafe { slice::from_raw_parts(pm.start() as *const u8, pm.size()) };
+            PtImage::new(data, pm.start() as u64)
+        })
+        .collect::<Vec<_>>();
 
     let mut pt = IntelPT::builder()
-        .thread_id(thread_id)
-        .images(&[])
+        .images(&images)
         .build()
         .expect("Failed to create IntelPT for worker thread");
     pt.enable_tracing().expect("Failed to enable tracing");
 
-    let wait_result = unsafe { WaitForSingleObject(thread_handle, INFINITE) };
-    assert_eq!(
-        wait_result, WAIT_OBJECT_0,
-        "Worker thread did not terminate cleanly"
+    let (thread_id_sender, thread_id_receiver) = channel();
+    let barrier = Arc::new(Barrier::new(2));
+    let worker_barrier = barrier.clone();
+    let worker = thread::spawn(|| worker_main(thread_id_sender, worker_barrier));
+    let worker_tid = thread_id_receiver.recv().unwrap();
+    println!("Intel PT worker thread id: {}", worker_tid);
+    pt.set_tid(Some(worker_tid));
+    barrier.wait();
+
+    let mut map = vec![0u16; 0x10_00];
+    pt.decode_traces_into_map(map.as_mut_ptr(), map.len())
+        .unwrap();
+    pt.dump_last_trace_to_file().unwrap();
+
+    let assembly_jump_id = map.iter().position(|count| *count >= 254);
+    assert!(
+        assembly_jump_id.is_some(),
+        "Assembly jumps not found in traces"
     );
 
-    let trace = pt
-        .get_raw_trace()
-        .expect("Failed to get raw trace from raw trace");
-
-    let mut file = File::create("output.bin").unwrap();
-    file.write_all(&trace).unwrap();
-
-    let _ = unsafe { CloseHandle(thread_handle) };
+    barrier.wait();
+    worker.join().expect("Worker thread panicked");
 }
