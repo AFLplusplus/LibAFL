236fb29abf8cf38a9beb82229793e40358fc5e99
Dominik Maier <dmnk@google.com>
2026-05-17 20:02:39 +0200

    undo

--- utils/ci_runner/src/main.rs
+++ utils/ci_runner/src/main.rs
@@ -43,8 +43,7 @@ fn run() -> Result<(), Box<dyn core::error::Error>> {
     for profile in &["DEV", "RELEASE"] {
         unsafe {
             env::set_var(format!("CARGO_PROFILE_{profile}_OPT_LEVEL"), "z");
-            env::set_var(format!("CARGO_PROFILE_{profile}_INCREMENTAL"), "false");
-            env::set_var(format!("CARGO_PROFILE_{profile}_DEBUG"), "1");
+            env::set_var(format!("CARGO_PROFILE_{profile}_INCREMENTAL"), "true");
         }
     }
 
