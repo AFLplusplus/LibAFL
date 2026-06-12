d213287cf3e74df3dd21f45f5cd25c96a18b78ed
Marcondiro <Marco.Cavenati@eurecom.fr>
2026-06-12 10:17:34 +0200

    Simpler integration test, lints

--- crates/libafl_intelpt/tests/integration_tests_windows.rs
+++ crates/libafl_intelpt/tests/integration_tests_windows.rs
@@ -1,15 +1,10 @@
 #![cfg(feature = "std")]
 #![cfg(target_os = "windows")]
 
-use std::{
-    arch::asm,
-    process, slice,
-    sync::{
-        Arc, Barrier,
-        mpsc::{Sender, channel},
-    },
-    thread,
-};
+extern crate alloc;
+
+use std::{arch::asm, process};
+use alloc::slice;
 
 use libafl_intelpt::{IntelPT, availability};
 use log::LevelFilter;
@@ -17,30 +12,8 @@ use proc_maps::get_process_maps;
 use ptcov::PtImage;
 use windows::Win32::System::Threading::GetCurrentThreadId;
 
-fn worker_main(thread_id_sender: Sender<u32>, barrier: Arc<Barrier>) -> u32 {
-    let tid = unsafe { GetCurrentThreadId() };
-    thread_id_sender.send(tid).unwrap();
-
-    barrier.wait();
-
-    let mut count = 0;
-    unsafe {
-        asm!(
-        "2:",
-        "add {0:r}, 1",
-        "cmp {0:r}, 255",
-        "jle 2b",
-        inout(reg) count,
-        options(nostack)
-        );
-    }
-
-    barrier.wait();
-    count
-}
-
 #[test]
-fn intel_pt_trace_thread() {
+fn intel_pt_trace_loop() {
     let _ = env_logger::builder()
         .is_test(true)
         .filter_level(LevelFilter::Trace)
@@ -68,15 +41,24 @@ fn intel_pt_trace_thread() {
         .images(&images)
         .build()
         .expect("Failed to create IntelPT for worker thread");
-
-    let (thread_id_sender, thread_id_receiver) = channel();
-    let barrier = Arc::new(Barrier::new(2));
-    let worker_barrier = barrier.clone();
-    let worker = thread::spawn(|| worker_main(thread_id_sender, worker_barrier));
-    let worker_tid = thread_id_receiver.recv().unwrap();
-    pt.set_thread_id(Some(worker_tid));
+    let tid = unsafe { GetCurrentThreadId() };
+    pt.set_thread_id(Some(tid));
     pt.enable_tracing().unwrap();
-    barrier.wait();
+
+    let mut count = 0;
+    unsafe {
+        asm!(
+        "2:",
+        "add {0:r}, 1",
+        "cmp {0:r}, 255",
+        "jle 2b",
+        inout(reg) count,
+        options(nostack)
+        );
+    }
+    let _ = count;
+
+    pt.disable_tracing().unwrap();
 
     let mut map = vec![0u16; 0x10_00];
     pt.decode_traces_into_map(map.as_mut_ptr(), map.len())
@@ -87,7 +69,4 @@ fn intel_pt_trace_thread() {
         assembly_jump_id.is_some(),
         "Assembly jumps not found in traces"
     );
-
-    barrier.wait();
-    worker.join().expect("Worker thread panicked");
 }
