829eff563340807e36347bd312d8caffb2aa0997
Dominik Maier <dmnk@google.com>
2026-08-11 14:12:08 +0200

    removed stale link

--- crates/libafl/src/stages/tracing.rs
+++ crates/libafl/src/stages/tracing.rs
@@ -41,10 +41,8 @@ where
         + HasCurrentTestcase<I>
         + MaybeHasClientPerfMonitor,
 {
-    /// Perform tracing on the given `CorpusId`. Useful for if wrapping [`TracingStage`] with your
-    /// own stage and you need to manage [`super::NestedStageRetryCountRestartHelper`] differently
-    /// see [`super::ConcolicTracingStage`]'s implementation as an example of usage.
-    #[allow(rustdoc::broken_intra_doc_links)]
+    /// Perform tracing on the current testcase. Useful if wrapping [`TracingStage`] with your
+    /// own stage and you need to manage [`super::NestedStageRetryCountRestartHelper`] differently.
     pub fn trace(&mut self, fuzzer: &mut Z, state: &mut S, manager: &mut EM) -> Result<(), Error> {
         start_timer!(state);
         let input = state.current_input_cloned()?;
